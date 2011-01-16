66bd49b01b7f3dc7fa6702d9b6de83e6bfcaeccf
Ryan Riley <ryan.riley@panesofglass.org>
2011-01-15 16:16:58 -0800

    Updated Parser and solution.

--- src/Parser.fs
+++ src/Parser.fs
@@ -9,6 +9,8 @@ module Parser =
   let start = Async.Start
   let continueWith = Async.StartWithContinuations
 
+  let zero : Parser<'r,'s> = fun input -> async { return None }
+
   let puree v : Parser<'r,'s> = fun input -> async { return Some(v, input) }
     
   let bind (p: Parser<'a,'s>) (f: 'a -> Parser<'b,'s>) : Parser<'b,'s> =
@@ -19,8 +21,6 @@ module Parser =
               | None -> async { return None } }
   let (>>=) = bind
 
-  let zero : Parser<'r,'s> = fun input -> async { return None }
-  
   let choice (p: Parser<'a,'s>) (q: Parser<'a,'s>) : Parser<'a,'s> =
     fun input -> async {
       let! comp = p input
@@ -31,28 +31,40 @@ module Parser =
 
   type ParserBuilder() =
     member x.Delay(f) = fun input -> f () input
+
     member x.Zero() = zero
+
     member x.Return(v) = puree v
+
     member x.ReturnFrom(p) : Parser<'r,'s> = p
+
     member x.Bind(p, f) = p >>= f
+
     member x.Combine(p, q) = p +++ q
+
     member this.TryWith(p:Parser<'r,'s>, h) : Parser<'r,'s> =
       fun input -> async {
         try return! p input
         with e -> return! h e input }
+
     member this.TryFinally(p:Parser<'r,'s>, compensation) : Parser<'r,'s> =
       fun input -> async {
         try return! p input
         finally compensation() }
+
     member this.Using(res:#IDisposable, body) =
       this.TryFinally(body res, (fun () -> match res with null -> () | disp -> disp.Dispose()))
+
     member this.While(guard, m) =
-      if not(guard()) then zero
-      else m >>= (fun () -> this.While(guard, m))
+      if not(guard()) then zero else
+        m >>= (fun () -> this.While(guard, m))
+
     member this.For(sequence:seq<_>, body) =
       this.Using(sequence.GetEnumerator(),
                  (fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current))))
+
     member x.Yield(v) = puree v
+
     member x.YieldFrom(p) : Parser<'r,'s> = p
   
   let parse = ParserBuilder()
