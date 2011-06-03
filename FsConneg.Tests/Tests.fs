ffb1316ef0822f6b0fc54022651424423ac9c6c0
mausch <mauricioscheffer@gmail.com>
2011-06-03 11:27:07 -0300

    initial commit

---
+++ FsConneg.Tests/Tests.fs
@@ -0,0 +1,14 @@
+﻿module Tests
+
+open Xunit
+open FsConneg
+
+[<Fact>]
+let ``Parse AcceptLanguage``() =
+    let acceptLang = "en-US; q=0.6, de-de;q=0.4,de;q=0.2,en-ca,en;q=0.8"
+    let langs = parseLang acceptLang
+    match parseLang acceptLang with
+    | ["en-ca"; "en"; "en-us"; "de-de"; "de"] -> ()
+    | x -> failwithf "wrong parsing: %A" x
+    ()
+
---
+++ FsConneg/FsConneg.fs
@@ -0,0 +1,39 @@
+﻿module FsConneg
+
+open System
+open System.Globalization
+
+type Either<'a,'b> = Left of 'a | Right of 'b
+
+let inline internal split (sep: char) (s: string) =
+    s.Split [|sep|]
+
+type Double with
+    static member internal tryParse a =
+        match Double.TryParse(a, NumberStyles.Number, CultureInfo.InvariantCulture) with
+        | false, _ -> None
+        | _, v -> Some v
+
+let inline internal nth i arr = Array.get arr i
+
+let internal (|Q|_|) (x: string) =
+    if x = null 
+        then None
+        else
+            let x = x.Trim().ToLowerInvariant()
+            if not (x.StartsWith "q=") 
+                then None
+                else x |> split '=' |> nth 1 |> Double.tryParse
+
+let parseLang langs =
+    split ',' langs
+    |> Seq.map (split ';')
+    |> Seq.map (fun s ->
+                    match s with
+                    | [|x|] -> x.Trim().ToLowerInvariant(), 1.
+                    | [|x; Q q|] -> x.Trim().ToLowerInvariant(), q
+                    | _ -> failwith "%A" s)
+    |> Seq.sortBy (snd >> ((*)-1.))
+    |> Seq.map fst
+    |> Seq.toList
+
