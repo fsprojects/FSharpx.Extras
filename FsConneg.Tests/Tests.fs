f5ca36dfdec84d8e235acc5aa27262dba72adf15
mausch <mauricioscheffer@gmail.com>
2011-06-03 15:02:44 -0300

    tests

--- FsConneg.Tests/Tests.fs
+++ FsConneg.Tests/Tests.fs
@@ -24,3 +24,16 @@ let ``Best content type``() =
     | None -> failwith "no suitable content type found"
     | Some contentType -> Assert.Equal("text/plain", contentType)
 
+[<Fact>]
+let ``Best content type, none found``() =
+    let accept = "text/html; q=0.8; level=2, text/html; q=0.2; level=1, text/plain, image/jpeg"
+    Assert.Equal(None, bestContent "application" accept)
+
+[<Fact>]
+let ``Content type matching``() =
+    let accept = "text/html; q=0.8; level=2, text/html; q=0.2; level=1, image/png, image/jpeg;q=0.8"
+    let images = parseAccept accept |> List.filter (fun t -> t.StartsWith "image/")
+    match images with
+    | "image/jpeg"::_ -> failwith "should have been image/png"
+    | "image/png"::_ -> ()
+    | x -> failwithf "can't handle any of these content types: %A, failing with 406" x
