6150aa4a609f106370e427e1e4205747287c1175
Ryan Riley <ryan.riley@logos.com>
2011-07-06 08:26:14 -0700

    Update the continuation monad to use the type-based approach from Matthew Podwysocki.

--- src/FSharp.Monad/Continuation.fs
+++ src/FSharp.Monad/Continuation.fs
@@ -2,36 +2,55 @@
 
 open System
 
-type Cont<'a,'r> = ('a -> 'r) -> 'r
-
 /// The continuation monad.
 /// The algorithm is from Wes Dyer http://blogs.msdn.com/b/wesdyer/archive/2008/01/11/the-marvels-of-monads.aspx.
 /// The builder approach is from Matthew Podwysocki's excellent Creating Extended Builders series http://codebetter.com/blogs/matthew.podwysocki/archive/2010/01/18/much-ado-about-monads-creating-extended-builders.aspx.
-type ContinuationBuilder() =
-  member this.Return(x) : Cont<'a,'r> = fun c -> c x
-  member this.ReturnFrom(m: Cont<'a,'r>) = m
-  member this.Bind(m:Cont<'a,'r>, f:'a -> Cont<'b,'r>) : Cont<'b,'r> =
-    fun k -> m (fun x -> (f x) k)
-  member this.Zero() = this.Return()
-  member this.TryWith(m:Cont<'a,'r>, h:exn -> Cont<'a,'r>) : Cont<'a,'r> =
-    fun k -> try m k
-             with e -> (h e) k
-  member this.TryFinally(m:Cont<'a,'r>, compensation) : Cont<'a,'r> =
-    fun k -> try m k
-             finally compensation()
-  member this.Using(res:#IDisposable, body) =
-    this.TryFinally(body res, fun () -> match res with null -> () | disp -> disp.Dispose())
-  member this.Combine(comp1, comp2) = this.Bind(comp1, fun () -> comp2)
-  member this.Delay(f) = this.Bind(this.Return(), f)
-  member this.While(guard, m) =
-    if not(guard()) then this.Zero() else
-      this.Bind(m, fun () -> this.While(guard, m))
-  member this.For(sequence:seq<_>, body) =
-    this.Using(sequence.GetEnumerator(),
-               fun enum -> this.While(enum.MoveNext, this.Delay(fun () -> body enum.Current)))
-let cont = ContinuationBuilder()
+/// Current implementation from Matt's gist at https://gist.github.com/628956
+type Cont<'a,'r> =
+  abstract Run : ('a -> 'r) * (exn -> 'r) -> 'r
 
-let callCC f = fun k -> (f (fun a -> (fun _ -> k a))) k
+let private protect f x cont econt =
+  let res = try Choice1Of2 (f x) with err -> Choice2Of2 err
+  match res with
+  | Choice1Of2 v -> cont v
+  | Choice2Of2 v -> econt v
+
+let runCont (c:Cont<_,_>) cont econt = c.Run(cont, econt)
+let throw exn = { new Cont<_,_> with member x.Run (cont,econt) = econt exn }
+let callCC f =
+  { new Cont<_,_> with
+      member x.Run(cont, econt) =
+        runCont (f (fun a -> { new Cont<_,_> with member x.Run(_,_) = cont a })) cont econt }
+ 
+type ContinuationBuilder() =
+  member this.Return(a) = 
+    { new Cont<_,_> with member x.Run(cont, econt) = cont a }
+  member this.ReturnFrom(comp:Cont<_,_>) = comp
+  member this.Bind(comp1, f) = 
+    { new Cont<_,_> with 
+        member x.Run (cont, econt) = 
+          runCont comp1 (fun a -> protect f a (fun comp2 -> runCont comp2 cont econt) econt) econt }
+  member this.Catch(comp:Cont<_,_>) =
+    { new Cont<Choice<_, exn>,_> with 
+        member x.Run (cont, econt) = 
+          runCont comp (fun v -> cont (Choice1Of2 v)) (fun err -> cont (Choice2Of2 err)) }
+  member this.Zero() =
+    this.Return ()
+  member this.TryWith(tryBlock, catchBlock) =
+    this.Bind(this.Catch tryBlock, (function Choice1Of2 v -> this.Return v 
+                                           | Choice2Of2 exn -> catchBlock exn))
+  member this.TryFinally(tryBlock, finallyBlock) =
+    this.Bind(this.Catch tryBlock, (function Choice1Of2 v -> finallyBlock(); this.Return v 
+                                           | Choice2Of2 exn -> finallyBlock(); throw exn))
+  member this.Using(res:#IDisposable, body) =
+    this.TryFinally(body res, (fun () -> match res with null -> () | disp -> disp.Dispose()))
+  member this.Combine(comp1, comp2) = this.Bind(comp1, (fun () -> comp2))
+  member this.Delay(f) = this.Bind(this.Return (), f)
+  member this.While(pred, body) =
+    if pred() then this.Bind(body, (fun () -> this.While(pred,body))) else this.Return ()
+  member this.For(items:seq<_>, body) =
+    this.Using(items.GetEnumerator(), (fun enum -> this.While((fun () -> enum.MoveNext()), this.Delay(fun () -> body enum.Current))))
+let cont = ContinuationBuilder()
 
 module Operators =
   open FSharp.Monad.Operators
--- tests/FSharp.Monad.Tests/ContinuationTest.fs
+++ tests/FSharp.Monad.Tests/ContinuationTest.fs
@@ -1,6 +1,7 @@
 ﻿module FSharp.Monad.Tests.ContinuationTest
 
 open System
+open FSharp.Monad
 open FSharp.Monad.Continuation
 open NUnit.Framework
 open FsUnit
@@ -13,8 +14,12 @@ let addSomeNumbers = cont {
 
 [<Test>]
 let ``When adding 6 to 7 and applying a continuation to convert to string and replace 1 with a, it should return a3``() =
-  let run f m = m f 
-  addSomeNumbers (fun x -> x.ToString().Replace('1', 'a')) |> should equal "a3"
+  runCont addSomeNumbers (fun x -> x.ToString().Replace('1', 'a')) (sprintf "%A") |> should equal "a3"
+
+[<Test>]
+let ``When throwing an exception, it should catch the exception``() =
+  let message = "failed"
+  runCont (Continuation.throw (Exception(message))) (fun _ -> "didn't handle exception") (fun (e:exn) -> e.Message) |> should equal message
 
 (* Test callCC *)
 let sum l =
@@ -26,7 +31,7 @@ let sum l =
       | h::t -> let! r = sum t
                 return h + r })
     return result }
-  (sum l) (id)
+  runCont (sum l) id (fun _ -> -1)
 
 [<Test>]
 let ``When summing a list without a 2 via callCC it should return 8``() =
@@ -35,27 +40,3 @@ let ``When summing a list without a 2 via callCC it should return 8``() =
 [<Test>]
 let ``When summing a list containing 2 via callCC it should return 43``() =
   sum [1;2;3] |> should equal 43
-
-/// Call/CC scenario, taken from <see href="http://www.haskell.org/all_about_monads/html/contmonad.html"/>.
-(*
-[<Test>]
-let ``CallCC should allow escapes from continuations``() =
-  let callCCTest n = cont {
-    let str = callCC (fun exit1 -> cont {
-      if n < 10 then return! exit1 (n.ToString()) else
-      let ns = (n/2).ToString().ToCharArray() |> Array.map (fun c -> Convert.ToInt32(c))
-      let n' = callCC (fun exit2 -> cont {
-        if ns.Length < 3 then return! exit2 ns.Length
-        elif ns.Length < 5 then return! exit2 n
-        elif ns.Length < 7 then return! cont {
-          let ns' = ns |> Array.rev |> Array.map (fun i -> Convert.ToChar(i))
-          let result = ns' |> Seq.skipWhile ((=) '0')
-          return! exit1 (result.ToString()) }
-        return Array.sum ns })
-      return "(ns = " + ns.ToString() + ") " + n' })
-    return "Answer: " + str }
-
-  let run n test = runCont (test n) id
-
-  run callCCTest 1 |> should equal "1"
-*)
