4bb2f1039c4a3287a0710faf402763750134f1c9
Ryan Riley <ryan.riley@panesofglass.org>
2011-07-06 21:39:27 -0700

    Add additional operators for working with Async types.

---
+++ src/FSharp.Monad/AsyncOperators.fs
@@ -0,0 +1,14 @@
+﻿module FSharp.Monad.Async.Operators
+
+open FSharp.Monad.Operators
+
+let inline returnM x = returnM async x
+let inline (>>=) m f = bindM async m f
+let inline (<*>) f m = applyM async async f m
+let inline pipe m f = liftM async m f
+let inline pipe2 x y f = returnM f <*> x <*> y
+let inline pipe3 x y z f = returnM f <*> x <*> y <*> z
+let inline (<!>) f m = pipe m f
+let inline ( *>) x y = pipe2 x y (fun _ z -> z)
+let inline ( <*) x y = pipe2 x y (fun z _ -> z)
+let inline (>>.) m f = bindM async m (fun _ -> f)
