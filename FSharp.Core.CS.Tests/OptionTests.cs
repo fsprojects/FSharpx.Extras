eb4ffb23d8e4549525e036a563d4f7f43b02ff47
mausch <mauricioscheffer@gmail.com>
2011-07-25 19:18:30 -0300

    initial commit

---
+++ FSharp.Core.CS.Tests/OptionTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class OptionTests {
+        [Test]
+        public void HasValue() {
+            var a = FSharpOption<int>.Some(5);
+            Assert.IsTrue(a.HasValue());
+        }
+
+        [Test]
+        public void NotHasValue() {
+            var a = FSharpOption<int>.None;
+            Assert.IsFalse(a.HasValue());
+        }
+    }
+}
---
+++ FSharp.Core.CS/Option.fs
@@ -0,0 +1,8 @@
+﻿namespace FSharp.Core.CS
+
+open System.Runtime.CompilerServices
+
+[<Extension>]
+module FSharpOptionExtensions =
+    [<Extension>]
+    let HasValue o = Option.isSome o
