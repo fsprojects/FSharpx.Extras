eabdf0802e753e6f082a072229999caffb90edbe
mausch <mauricioscheffer@gmail.com>
2011-07-25 20:43:39 -0300

    pattern matching on lists

---
+++ FSharp.Core.CS.Tests/ListTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.FSharp.Collections;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class ListTests {
+        [Test]
+        public void Match_empty() {
+            var a = FSharpList<int>.Empty;
+            var s = a.Match(() => "empty", (x, xs) => "non empty");
+            Assert.AreEqual("empty", s);
+        }
+
+        [Test]
+        public void Match_non_empty() {
+            var a = FSharpList<int>.Cons(5, FSharpList<int>.Empty);
+            var s = a.Match(() => "empty", 
+                (x, xs) => "head is " + x + ", tail is " + (xs.IsEmpty ? "empty" : "non-empty"));
+            Assert.AreEqual("head is 5, tail is empty", s);
+        }
+    }
+}
---
+++ FSharp.Core.CS/List.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.Core.CS
+
+open System
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type ListExtensions =
+    [<Extension>]
+    static member Match (l, empty: Func<_>, nonempty: Func<_,_,_>) =
+        match l with
+        | [] -> empty.Invoke()
+        | x::xs -> nonempty.Invoke(x,xs)
