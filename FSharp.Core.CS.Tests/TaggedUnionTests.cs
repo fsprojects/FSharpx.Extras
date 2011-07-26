260bb44eecfcf01ae48d8ac46d72dee2dc622cf4
mausch <mauricioscheffer@gmail.com>
2011-07-25 21:07:02 -0300

    pattern matching on discriminated union

---
+++ FSharp.Core.CS.Tests/TaggedUnionTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class TaggedUnionTests {
+        [Test]
+        public void Match() {
+            var a = FSharpChoice<int, string>.NewChoice1Of2(1);
+            var c = a.Match(i => 0, _ => 1);
+            Assert.AreEqual(0, c);
+        }
+    }
+}
---
+++ FSharp.Core.CS/TaggedUnion.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharp.Core.CS
+
+open System
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type TaggedUnionExtensions =
+    [<Extension>]
+    static member Match (c, f1: Func<_,_>, f2: Func<_,_>) =
+        match c with
+        | Choice1Of2 x -> f1.Invoke x
+        | Choice2Of2 y -> f2.Invoke y
+
