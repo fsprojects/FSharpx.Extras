f83a04e0e7000fce5ce9cc8b2bbd39b1c702611f
mausch <mauricioscheffer@gmail.com>
2011-07-25 22:16:29 -0300

    discriminated union with 3 types

--- FSharp.Core.CS/TaggedUnion.fs
+++ FSharp.Core.CS/TaggedUnion.fs
@@ -11,7 +11,18 @@ type TaggedUnionExtensions =
         | Choice1Of2 x -> f1.Invoke x
         | Choice2Of2 y -> f2.Invoke y
 
+    [<Extension>]
+    static member Match (c, f1: Func<_,_>, f2: Func<_,_>, f3: Func<_,_>) =
+        match c with
+        | Choice1Of3 x -> f1.Invoke x
+        | Choice2Of3 x -> f2.Invoke x
+        | Choice3Of3 x -> f3.Invoke x
+
 type FSharpChoice =
     static member New1Of2<'a,'b> (a: 'a) : Choice<'a,'b> = Choice1Of2 a
     static member New2Of2<'a,'b> (b: 'b) : Choice<'a,'b> = Choice2Of2 b
+
+    static member New1Of3<'a,'b,'c> (a: 'a) : Choice<'a,'b,'c> = Choice1Of3 a
+    static member New2Of3<'a,'b,'c> (a: 'b) : Choice<'a,'b,'c> = Choice2Of3 a
+    static member New3Of3<'a,'b,'c> (a: 'c) : Choice<'a,'b,'c> = Choice3Of3 a
         
