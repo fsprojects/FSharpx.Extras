17db216da0287b03180e0bb0bcdc3b33628967e1
mausch <mauricioscheffer@gmail.com>
2011-07-26 00:17:20 -0300

    integration test / sample

---
+++ FSharp.Core.CS.Tests/IntegrationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Specialized;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class IntegrationTests {
+        private int doSomething(int userID, int id) {
+            return userID + id;
+        }
+
+        private void setError(string e) {}
+
+        [Test]
+        public void Test1_Imperative() {
+            // query string
+            var nvc = new NameValueCollection { { "user_id", "123" } };
+            // web forms textbox
+            const string textboxContent = "999";
+
+            int userID;
+            var userID_ok = int.TryParse(nvc["user_id"], out userID);
+            if (!userID_ok) {
+                setError("Invalid User ID");
+            } else {
+                int id;
+                var id_ok = int.TryParse(textboxContent, out id);
+                if (!id_ok) {
+                    setError("Invalid ID");
+                } else {
+                    Console.WriteLine(doSomething(userID, id));
+                }
+            }
+        }
+
+        [Test]
+        public void Test1_Functional() {
+            // query string
+            var nvc = new NameValueCollection { { "user_id", "123" } };
+            // web forms textbox
+            const string textboxContent = "999";
+
+            var somethingOrError = 
+                from userID in Option.TryParseInt(nvc["user_id"]).ToChoice("Invalid User ID")
+                from id in Option.TryParseInt(textboxContent).ToChoice("Invalid ID")
+                select doSomething(userID, id);
+
+            somethingOrError.Match(Console.WriteLine, setError);
+
+        }
+    }
+}
