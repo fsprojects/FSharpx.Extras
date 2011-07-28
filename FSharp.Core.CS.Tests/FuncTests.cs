6f72073a8ce8ffb7ecaa23352445c0e12a8f9ee4
mausch <mauricioscheffer@gmail.com>
2011-07-28 00:14:50 -0300

    func conversions

---
+++ FSharp.Core.CS.Tests/FuncTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class FuncTests {
+        [Test]
+        public void FromAction() {
+            int a = 0;
+            FSharpFunc<Unit, Unit> ff = FSharpFunc.FromAction(() => a = 1);
+            ff.Invoke(null);
+            Assert.AreEqual(1, a);
+        }
+
+        [Test]
+        public void FromActionEta() {
+            int a = 0;
+            Action action = () => a = 1;
+            Action[] actions = new[] {action,action};
+            var functions = actions.Select(FSharpFunc.FromAction);
+        }
+    }
+}
---
+++ FSharp.Core.CS/Functions.fs
@@ -0,0 +1,31 @@
+﻿namespace Microsoft.FSharp.Core
+
+open System
+open System.Runtime.CompilerServices
+
+/// <summary>
+/// Conversion functions from Action/Func to FSharpFunc
+/// We need these because FuncConvert often makes C# type inference fail.
+/// </summary>
+[<Extension>]
+type FSharpFunc =
+    static member FromAction (f: Action) =
+        fun () -> f.Invoke()
+    
+    static member FromAction (f: Action<_>) =
+        fun x -> f.Invoke x
+
+    static member FromAction (f: Action<_,_>) =
+        fun x y -> f.Invoke(x,y)
+
+    static member FromAction (f: Action<_,_,_>) =
+        fun x y z -> f.Invoke(x,y,z)
+
+    static member FromFunc (f: Func<_>) =
+        fun () -> f.Invoke()
+
+    static member FromFunc (f: Func<_,_>) =
+        fun x -> f.Invoke x
+
+    static member FromFunc (f: Func<_,_,_>) =
+        fun x y -> f.Invoke(x,y)
