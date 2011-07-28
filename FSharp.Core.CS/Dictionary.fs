c5bf6fd4f548b5a4c7e9c1777e6742cb46b4642a
mausch <mauricioscheffer@gmail.com>
2011-07-28 01:12:16 -0300

    dictionary

---
+++ FSharp.Core.CS/Dictionary.fs
@@ -0,0 +1,12 @@
+﻿namespace System.Collections.Generic
+
+open System
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type DictionaryExtensions =
+    [<Extension>]
+    static member TryFind (d: IDictionary<_,_>, key) =
+        match d.TryGetValue key with
+        | true,v -> Some v
+        | _ -> None
