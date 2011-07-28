2e769c421fd2cb15d75dc48189356ac52ce17803
mausch <mauricioscheffer@gmail.com>
2011-07-28 00:43:17 -0300

    set

---
+++ FSharp.Core.CS.Tests/SetTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
+using NUnit.Framework;
+
+namespace FSharp.Core.CS.Tests {
+    [TestFixture]
+    public class SetTests {
+        [Test]
+        public void New() {
+            FSharpSet<int> set = FSharpSet.New(1, 2, 3);
+            FSharpSet<int> set2 = new FSharpSet<int>(new[] { 1, 2, 3 });
+            Assert.AreEqual(set, set2);
+        }
+
+        [Test]
+        public void ToSet() {
+            FSharpSet<int> set = new[] { 1, 2, 3 }.ToFSharpSet();
+
+        }
+    }
+}
---
+++ FSharp.Core.CS/Set.fs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.FSharp.Core
+
+open System
+open System.Runtime.CompilerServices
+
+[<Extension>]
+type FSharpSet =
+    static member New([<ParamArray>] values: 'a array) =
+        set values
+
+    [<Extension>]
+    static member ToFSharpSet values = set values
