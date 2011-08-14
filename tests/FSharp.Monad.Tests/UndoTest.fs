edc367287c10a98d9c39185bfcaeedc96d748afd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-08-14 14:09:37 +0200

    Adding more tests for the UndoMonad.

--- tests/FSharp.Monad.Tests/UndoTest.fs
+++ tests/FSharp.Monad.Tests/UndoTest.fs
@@ -30,11 +30,12 @@ let ``When starting a text editor with "" and adding three strings and undoing t
     let! _ = addText "foo"
     let! _ = addText "bar"
     let! _ = addText "baz"
-    let! _ = undo()
-    let! _ = undo()
-    return () }
-  let actual = exec test (empty "")
-  actual |> should equal "foo"
+    let! firstUndo = undo()
+    let! secondUndo = undo()
+    return firstUndo,secondUndo }
+  let actual = test (empty "")
+  actual |> snd |> current |> should equal "foo"
+  actual |> fst |> should equal (true,true)
 
 [<Test>]
 let ``When starting a text editor with "" and adding three strings and undoing two and redoing two, it should contain all three strings``() =
@@ -44,8 +45,30 @@ let ``When starting a text editor with "" and adding three strings and undoing t
     let! _ = addText "baz"
     let! _ = undo()
     let! _ = undo()
-    let! _ = redo()
-    let! _ = redo()
-    return () }
-  let actual = exec test (empty "")
-  actual |> should equal "foobarbaz"
+    let! firstRedo = redo()
+    let! secondRedo = redo()
+    return firstRedo,secondRedo }
+  let actual = test (empty "")
+  actual |> snd |> current |> should equal "foobarbaz"
+  actual |> fst |> should equal (true,true)
+
+[<Test>]
+let ``When starting a text editor with "" and adding a string, it should not allow two undos``() =
+  let test = undoable {
+    let! _ = addText "foo"    
+    let! firstUndo = undo()
+    let! secondUndo = undo()
+    return firstUndo,secondUndo }
+  let actual = test (empty "")
+  actual |> snd |> current |> should equal ""
+  actual |> fst |> should equal (true,false)
+
+[<Test>]
+let ``When starting a text editor with "" and adding a string, it should not allow a redo without an undo``() =
+  let test = undoable {
+    let! _ = addText "foo"    
+    let! firstRedo = redo()    
+    return firstRedo }
+  let actual = test (empty "")
+  actual |> snd |> current |> should equal "foo"
+  actual |> fst |> should equal false
