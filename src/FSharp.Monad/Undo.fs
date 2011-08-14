040feab45c0c52773fe02c63e81dab7ec74e8a55
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-08-14 12:58:46 +0200

    Adding a first version of the UndoMonad. Ported from http://haskell.org/haskellwiki/New_monads/MonadUndo

---
+++ src/FSharp.Monad/Undo.fs
@@ -0,0 +1,52 @@
+﻿module FSharp.Monad.Undo
+
+// UndoMonad on top of StateMonad
+open FSharp.Monad.State
+
+let undoable = state
+
+type 'a History =  { 
+    Current:'a
+    Undos : 'a list
+    Redos : 'a list }
+
+let empty x = { Current = x; Undos = []; Redos = [] }
+let current history = history.Current
+
+let getHistory = getState
+
+let putToHistory x = undoable {
+    let! history = getState
+    do! putState 
+         { Current = x; 
+           Undos = history.Current :: history.Undos
+           Redos = [] } }
+
+let exec m s = m s |> snd |> current
+
+let getCurrent() = undoable {
+    let! history = getState
+    return current history}
+
+let undo() = undoable {
+    let! history = getState
+    match history.Undos with
+    | [] -> return false
+    | (x::rest) -> 
+        do! putState 
+                { Current = x;
+                  Undos = rest;
+                  Redos = history.Current :: history.Redos }
+        return true}
+
+
+let redo() = undoable {
+    let! history = getState
+    match history.Redos with
+    | [] -> return false
+    | (x::rest) -> 
+        do! putState 
+                { Current = x;
+                  Undos = history.Current :: history.Undos;
+                  Redos = rest }
+        return true}
---
+++ tests/FSharp.Monad.Tests/UndoTest.fs
@@ -0,0 +1,51 @@
+﻿module FSharp.Monad.Tests.UndoTest
+
+open FSharp.Monad.Undo
+open NUnit.Framework
+open FsUnit
+
+// Simple "text editor" example
+let addText newText = undoable {
+    let! text = getCurrent()
+    do! putToHistory (text + newText) 
+    return text}
+
+[<Test>]
+let ``When starting a text editior with empty string, it should have a empty string in history``() =
+  let actual = addText "" (empty "")
+  fst actual |> should equal ""
+
+[<Test>]
+let ``When starting a text editor with "" and adding two strings, it should contain both string``() =
+  let test = undoable {
+    let! _ = addText "foo"
+    let! _ = addText "bar"
+    return () }
+  let actual = exec test (empty "")
+  actual |> should equal "foobar"
+
+[<Test>]
+let ``When starting a text editor with "" and adding three strings and undoing two, it should contain the first string``() =
+  let test = undoable {
+    let! _ = addText "foo"
+    let! _ = addText "bar"
+    let! _ = addText "baz"
+    let! _ = undo()
+    let! _ = undo()
+    return () }
+  let actual = exec test (empty "")
+  actual |> should equal "foo"
+
+[<Test>]
+let ``When starting a text editor with "" and adding three strings and undoing two and redoing two, it should contain all three strings``() =
+  let test = undoable {
+    let! _ = addText "foo"
+    let! _ = addText "bar"
+    let! _ = addText "baz"
+    let! _ = undo()
+    let! _ = undo()
+    let! _ = redo()
+    let! _ = redo()
+    return () }
+  let actual = exec test (empty "")
+  actual |> should equal "foobarbaz"
