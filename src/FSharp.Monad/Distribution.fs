b57dd3698c447a5ac8673ca27b8088a7885d5f3b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-08-15 12:00:11 +0200

    Adding a first version of the DistributionMonad. If we don't want the dependency on FSPowerPack we could easily change the BigRational to float.

---
+++ src/FSharp.Monad/Distribution.fs
@@ -0,0 +1,53 @@
+﻿module FSharp.Monad.Distribution
+
+type 'a Outcome = {
+    Value: 'a
+    Probability : BigRational  }
+
+type 'a Distribution = 'a Outcome seq
+
+// P(A AND B) = P(A | B) * P(B)
+let bindD (dist:'a Distribution) (f: 'a -> 'b Distribution) =
+    dist 
+        |> Seq.map (fun p1 -> 
+                f p1.Value
+                |> Seq.map (fun p2 -> 
+                        { Value = p2.Value; 
+                            Probability = 
+                              p1.Probability * 
+                              p2.Probability}))
+        |> Seq.concat : 'b Distribution
+
+let returnD (value:'a) =   
+    Seq.singleton { Value = value ; Probability = 1N/1N }
+      : 'a Distribution
+
+type DistributionMonadBuilder() =
+    member this.Bind (r, f) = bindD r f
+    member this.Return x = returnD x
+
+let distribution = DistributionMonadBuilder()
+
+// Create some helpers
+let toUniformDistribution seq : 'a Distribution =
+    let l = Seq.length seq
+    seq 
+      |> Seq.map (fun e -> 
+            { Value = e; 
+              Probability = 1N / bignum.FromInt l })
+
+let probability (dist:'a Distribution) = 
+    dist
+      |> Seq.map (fun o -> o.Probability)
+      |> Seq.sum
+
+let fairDice sides = toUniformDistribution [1..sides]
+
+type CoinSide = 
+| Heads 
+| Tails
+
+let fairCoin = toUniformDistribution [Heads; Tails]
+
+let filter predicate (dist:'a Distribution) =
+    dist |> Seq.filter (fun o -> predicate o.Value)
---
+++ tests/FSharp.Monad.Tests/DistributionTest.fs
@@ -0,0 +1,36 @@
+﻿module FSharp.Monad.Tests.DistributionTest
+
+open FSharp.Monad.Distribution
+open NUnit.Framework
+open FsUnit
+
+// Simple examples
+let sumOfTwoFairDices = distribution {
+  let! d1 = fairDice 6
+  let! d2 = fairDice 6
+  return d1 + d2 }
+
+let fairCoinAndDice = distribution {
+  let! d = fairDice 6
+  let! c = fairCoin
+  return d,c }
+
+[<Test>]
+let ``When creating a empty distribution, then the probability should be 1``() =
+  let actual = distribution { return () }
+  probability actual |> should equal (1N/1N)
+
+[<Test>]
+let ``When creating two fair dices, then P(Sum of 2 dices = 7) should be 1/6``() =
+  let actual = sumOfTwoFairDices |> filter ((=) 7)
+  probability actual |> should equal (1N/6N)
+
+[<Test>]
+let ``When creating a fair coin and a fair dice, then P(Heads) should be 1/2``() =
+  let actual = fairCoinAndDice |> filter (fun (_,c) -> c = Heads)
+  probability actual |> should equal (1N/2N)
+
+[<Test>]
+let ``When creating a fair coin and a fair dice, then P(Heads and dice > 3) should be 1/4``() =
+  let actual = fairCoinAndDice |> filter (fun (d,c) -> c = Heads && d > 3)
+  probability actual |> should equal (1N/4N)
