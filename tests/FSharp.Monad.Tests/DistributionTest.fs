4acb66b87d66bd798812f63b430a5a8a5947df78
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-08-15 12:57:35 +0200

    Rearranging distribution tests

--- tests/FSharp.Monad.Tests/DistributionTest.fs
+++ tests/FSharp.Monad.Tests/DistributionTest.fs
@@ -4,26 +4,25 @@ open FSharp.Monad.Distribution
 open NUnit.Framework
 open FsUnit
 
-// Simple examples
-let sumOfTwoFairDices = distribution {
-  let! d1 = fairDice 6
-  let! d2 = fairDice 6
-  return d1 + d2 }
-
-let fairCoinAndDice = distribution {
-  let! d = fairDice 6
-  let! c = fairCoin
-  return d,c }
-
 [<Test>]
 let ``When creating a empty distribution, then the probability should be 1``() =
   let actual = distribution { return () }
   probability actual |> should equal (1N/1N)
 
+let sumOfTwoFairDices = distribution {
+  let! d1 = fairDice 6
+  let! d2 = fairDice 6
+  return d1 + d2 }
+
 [<Test>]
 let ``When creating two fair dices, then P(Sum of 2 dices = 7) should be 1/6``() =
   let actual = sumOfTwoFairDices |> filter ((=) 7)
   probability actual |> should equal (1N/6N)
+
+let fairCoinAndDice = distribution {
+  let! d = fairDice 6
+  let! c = fairCoin
+  return d,c }
 
 [<Test>]
 let ``When creating a fair coin and a fair dice, then P(Heads) should be 1/2``() =
