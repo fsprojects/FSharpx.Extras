7fe8f6a1076c747ed12aae31b35947a7016404d0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-08-16 19:40:44 +0200

    Adding some poker samples for DistributionMonad.

--- src/FSharp.Monad/Distribution.fs
+++ src/FSharp.Monad/Distribution.fs
@@ -34,7 +34,7 @@ let distribution = DistributionMonadBuilder()
 let toUniformDistribution seq : 'a Distribution =
     let l = Seq.length seq
     seq 
-      |> Seq.map (fun e -> 
+      |> Seq.map (fun e ->
             { Value = e; 
               Probability = 1N / bignum.FromInt l })
 
@@ -54,5 +54,32 @@ type CoinSide =
 
 let fairCoin = toUniformDistribution [Heads; Tails]
 
-let filter predicate (dist:'a Distribution) =
+let filter predicate (dist:'a Distribution) : 'a Distribution =
     dist |> Seq.filter (fun o -> predicate o.Value)
+
+let filterInAnyOrder items dist =
+  items
+    |> Seq.fold (fun d item -> filter (Seq.exists ((=) (item))) d) dist
+
+let map f (dist:'a Distribution) : 'b Distribution = 
+    dist 
+      |> Seq.map (fun o -> { Value = f o.Value; Probability = o.Probability })
+
+let selectOne values =
+    [for e in values -> e,values |> Seq.filter ((<>) e)] 
+      |> toUniformDistribution
+
+let rec selectMany n values =
+    match n with 
+    | 0 -> certainly ([],values)
+    | _ -> 
+        distribution {
+             let! (x,c1) = selectOne values
+             let! (xs,c2) = selectMany (n-1) c1
+             return x::xs,c2}
+    
+let select n values = 
+    selectMany n values   
+      |> map (fst >> List.rev)
+
+let remove items = Seq.filter (fun v -> Seq.forall ((<>) v) items)
