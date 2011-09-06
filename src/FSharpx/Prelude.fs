35be3b618e896ce4df8462611c9315f708e01663
mausch <mauricioscheffer@gmail.com>
2011-09-06 03:12:25 -0300

    more prelude

--- src/FSharpx/Prelude.fs
+++ src/FSharpx/Prelude.fs
@@ -3,5 +3,10 @@
 [<AutoOpen>]
 module Prelude =
   let inline flip f a b = f b a
+  let inline curry f a b = f(a,b)
+  let inline uncurry f (a,b) = f a b
+  let inline swap (a,b) = (b,a)
+  let inline konst a _ = a
+  let inline konst2 a _ _ = a
 
 
