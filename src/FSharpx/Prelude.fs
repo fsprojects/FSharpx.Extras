18a40c21f70bbe1e469377570bde54e1c6c42c05
mausch <mauricioscheffer@gmail.com>
2011-09-10 11:25:00 -0300

    moved tuples module to prelude

--- src/FSharpx/Prelude.fs
+++ src/FSharpx/Prelude.fs
@@ -10,3 +10,19 @@ module Prelude =
   let inline konst2 a _ _ = a
 
 
+/// Functions to create tuples
+module Tuples =
+  /// Creates a pair
+  let inline t2 a b = a,b
+  /// Creates a 3-tuple
+  let inline t3 a b c = a,b,c
+  /// Creates a 4-tuple
+  let inline t4 a b c d = a,b,c,d
+  /// Creates a 5-tuple
+  let inline t5 a b c d e = a,b,c,d,e
+  /// Creates a 6-tuple
+  let inline t6 a b c d e f = a,b,c,d,e,f
+
+  let inline fst3 (a,_,_) = a
+  let inline snd3 (_,a,_) = a
+  let inline thr3 (_,_,a) = a
