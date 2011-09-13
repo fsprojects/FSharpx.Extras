89a061ddb82a2aca39e080cd3e615700db912510
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-09-13 09:44:52 +0200

    Starting to import the strings module from FAKE.

---
+++ src/FSharpx/Strings.fs
@@ -0,0 +1,7 @@
+﻿namespace FSharpx
+
+open System
+
+module String =
+  /// Checks wether the given text starts with the given prefix
+  let startsWith prefix (text:string) = text.StartsWith prefix
---
+++ tests/FSharpx.CSharpTests/StringTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace FSharpx.CSharpTests
+{
+    [TestFixture]
+    public class StringTests
+    {
+        [Test]
+        public void WhenDetectingIfStringStartsWithHelloAndItDoes()
+        {
+            Assert.IsTrue(String.startsWith("Hello", "Hello World"));
+        }
+
+        [Test]
+        public void WhenDetectingIfStringStartsWithHelloAndItDoesnt()
+        {
+            Assert.IsFalse(String.startsWith("Hello", "World"));
+        }
+    }
+}
