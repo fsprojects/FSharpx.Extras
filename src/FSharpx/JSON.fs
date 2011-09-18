07c4a301de616b80deb713a87ab62db2bb813343
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-09-18 17:12:43 +0200

    Initial version of a simple JSON parser

---
+++ src/FSharpx/JSON.fs
@@ -0,0 +1,105 @@
+﻿module FSharpx.JSON
+
+// Initial version of the parser from http://blog.efvincent.com/parsing-json-using-f/
+// Simplyfied, added AST and fixed some minor bugs
+
+open System
+
+type Token =
+| OpenBracket | CloseBracket
+| OpenArray | CloseArray
+| Colon | Comma
+| String of string
+| Number of string
+
+let tokenize source=
+    let rec parseString acc = function
+        | '\\' :: '"' :: t -> // escaped quote
+            parseString (acc + "\"") t
+        | '"' :: t -> // closing quote terminates    
+            acc, t
+        | c :: t -> // otherwise accumulate
+            parseString (acc + (c.ToString())) t
+        | _ -> failwith "Malformed string."
+ 
+    let rec token acc = function
+        | (')' :: _) as t -> acc, t // closing paren terminates
+        | ('}' :: _) as t -> acc, t // closing paren terminates
+        | (':' :: _) as t -> acc, t // colon terminates
+        | (',' :: _) as t -> acc, t // comma terminates
+        | w :: t when Char.IsWhiteSpace(w) -> acc, t // whitespace terminates
+        | [] -> acc, [] // end of list terminates
+        | c :: t -> token (acc + (c.ToString())) t // otherwise accumulate chars
+
+    let rec tokenize' acc = function
+        | w :: t when Char.IsWhiteSpace(w) -> tokenize' acc t   // skip whitespace
+        | '{' :: t -> tokenize' (OpenBracket :: acc) t
+        | '}' :: t -> tokenize' (CloseBracket :: acc) t
+        | '[' :: t -> tokenize' (OpenArray :: acc) t
+        | ']' :: t -> tokenize' (CloseArray :: acc) t
+        | ':' :: t -> tokenize' (Colon :: acc) t
+        | ',' :: t -> tokenize' (Comma :: acc) t
+        | '"' :: t -> // start of string
+            let s, t' = parseString "" t
+            tokenize' (Token.String(s) :: acc) t'        
+        | '-' :: d :: t when Char.IsDigit(d) -> // start of negative number
+            let n, t' = token ("-" + d.ToString()) t
+            tokenize' (Token.Number(n) :: acc) t'
+        | '+' :: d :: t 
+        | d :: t when Char.IsDigit(d) -> // start of positive number
+            let n, t' = token (d.ToString()) t
+            tokenize' (Token.Number(n) :: acc) t'
+        | [] -> List.rev acc // end of list terminates
+        | _ -> failwith "Tokinzation error"
+
+    tokenize' [] [for x in source -> x]
+
+
+type JSON =
+| Text of string
+| Number of float
+| JArray of JSON list
+| JObject of Map<string,JSON>
+
+let emptyJObject = JObject Map.empty
+let addProperty key value = function
+| JObject(properties) -> JObject(Map.add key value properties)
+| _ -> failwith "Malformed JSON object" 
+
+
+/// Parses a JSOPN source text and returns an JSON AST
+let parse source =
+    let map = function
+    | Token.Number(n) -> Number (System.Double.Parse n) 
+    | Token.String(x) -> Text x
+    | v -> failwith "Syntax Error, unrecognized token in map()"
+ 
+    let rec parseValue = function
+    | OpenBracket :: t -> parseJObject t
+    | OpenArray :: t ->  parseArray t
+    | h :: t -> map h, t
+    | _ -> failwith "bad value"
+ 
+    and parseArray = function
+    | Comma :: t -> parseArray t
+    | CloseArray :: t -> JArray [], t
+    | t ->        
+        let element, t' = parseValue t
+        match parseArray t' with
+        | JArray(elements),t'' -> JArray (element :: elements),t''
+        | _ -> failwith "Malformed JSON array"
+    and parseJObject = function
+    | Comma :: t -> parseJObject t
+    | Token.String(name) :: Colon :: t -> 
+        let value,t' = parseValue t
+        let jObject,t'' = parseJObject t'
+        addProperty name value jObject,t''
+    | CloseBracket :: t -> emptyJObject, t
+    | _ -> failwith "Malformed JSON object"
+    
+
+    match tokenize source with
+    | OpenBracket :: t ->
+        let result, t' = parseJObject t
+        result
+    | _ -> failwith "JSON did not begin with an object"
---
+++ tests/FSharpx.Tests/JSONParserTests.fs
@@ -0,0 +1,44 @@
+﻿module FSharpx.Tests.JSONParserTests
+
+open NUnit.Framework
+open FSharpx.JSON
+open FsUnit
+
+
+[<Test>] 
+let ``Can parse empty document``() = 
+    parse "{}" 
+    |> should equal emptyJObject
+
+[<Test>] 
+let ``Can parse document with single property``() =
+    parse "{\"firstName\": \"John\"}" 
+    |> should equal (emptyJObject |> addProperty "firstName" (Text "John"))
+
+
+[<Test>] 
+let ``Can parse document with text and numbers``() =
+    parse "{\"firstName\": \"John\", \"lastName\": \"Smith\", \"age\": 25}" 
+    |> should equal 
+        (emptyJObject 
+            |> addProperty "firstName" (Text "John")
+            |> addProperty "lastName" (Text "Smith")
+            |> addProperty "age" (Number 25.))
+
+[<Test>]
+let ``Can parse nested document`` () =
+    "{
+        \"main\": {
+            \"title\": \"example\",
+            \"nested\": {
+                \"nestedTitle\": \"sub\"
+            }
+        }
+    }"
+    |> parse
+    |> should equal 
+            (emptyJObject
+            |> addProperty "main"
+                (emptyJObject
+                    |> addProperty "title" (Text "example")
+                    |> addProperty "nested" (emptyJObject |> addProperty "nestedTitle" (Text "sub"))))
