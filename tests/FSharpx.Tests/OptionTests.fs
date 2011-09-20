81af541caf7048116ea31f9d69475b0ce6c6096c
mausch <mauricioscheffer@gmail.com>
2011-09-20 18:44:42 -0300

    spaces

--- tests/FSharpx.Tests/OptionTests.fs
+++ tests/FSharpx.Tests/OptionTests.fs
@@ -7,19 +7,19 @@ open FSharpx.Option
 
 [<Test>]
 let ``kleisli composition``() =
-  let f x = 
-    if x > 5
-      then Some "hello"
-      else None
-  let g x =
-    if x = "hello"
-      then Some 10
-      else None
+    let f x = 
+        if x > 5
+            then Some "hello"
+            else None
+    let g x =
+        if x = "hello"
+            then Some 10
+            else None
 
-  let h = f >=> g
-  Assert.AreEqual(Some 10, h 8)
-  Assert.AreEqual(None, h 1)
-  ()
+    let h = f >=> g
+    Assert.AreEqual(Some 10, h 8)
+    Assert.AreEqual(None, h 1)
+    ()
 
 [<Test>]
 let ``from bool and value``() =
