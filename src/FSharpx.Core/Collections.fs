b4710d78c8367ab0b6cd41674fc8470f849e1eef
Ryan Riley <ryan.riley@logos.com>
2011-09-21 22:00:15 -0700

    Implement IEnumerable<byte> on ByteString to provide a mechanism for retrieving an IEnumerator<byte>.

--- src/FSharpx.Core/Collections.fs
+++ src/FSharpx.Core/Collections.fs
@@ -9,7 +9,128 @@ open System.Diagnostics.Contracts
 open System.Diagnostics
 #endif
 open System.Runtime.CompilerServices
+            
+module Enumerator = 
+ 
+    let singleton a = 
+        let state = ref 0
+        { new IEnumerator<_> with 
+            member self.Current = if (state.Value = 1) then a else invalidOp "!"
+          interface System.Collections.IEnumerator with
+            member self.Current = if (state.Value = 1) then box a else invalidOp "!"
+            member self.MoveNext() = state := state.Value + 1; state.Value = 1
+            member self.Reset() = state := 0
+          interface System.IDisposable with 
+            member self.Dispose() = () }
+  
+    let bind (f:_ -> IEnumerator<'b>) (a:IEnumerator<'a>) =
+        if a.MoveNext() then f (Some(a.Current)) else f(None) 
+   
+    let combine (a:IEnumerator<_>) b = 
+        let current = ref a
+        let first = ref true
+        { new IEnumerator<_> with 
+            member self.Current = current.Value.Current
+          interface System.Collections.IEnumerator with
+            member self.Current = box current.Value.Current
+            member self.MoveNext() = 
+                if current.Value.MoveNext() then true 
+                elif first.Value then 
+                    current := b
+                    first := false
+                    current.Value.MoveNext()
+                else false
+            member self.Reset() = 
+                a.Reset(); b.Reset()
+                first := true; current := a
+          interface System.IDisposable with 
+            member self.Dispose() = a.Dispose(); b.Dispose() }
+    
+    let empty<'a> : IEnumerator<'a> = 
+        { new IEnumerator<_> with 
+            member self.Current = invalidOp "!"
+          interface System.Collections.IEnumerator with
+            member self.Current = invalidOp "!"
+            member self.MoveNext() = false
+            member self.Reset() = ()
+          interface System.IDisposable with 
+            member self.Dispose() = ()}
+    
+    let delay f = 
+        let en : Lazy<IEnumerator<_>> = lazy f()
+        { new IEnumerator<_> with 
+            member self.Current = en.Value.Current
+          interface System.Collections.IEnumerator with
+            member self.Current = box en.Value.Current
+            member self.MoveNext() = en.Value.MoveNext()
+            member self.Reset() = en.Value.Reset()
+          interface System.IDisposable with 
+            member self.Dispose() = en.Value.Dispose() }
+   
+    let toSeq gen = 
+        { new IEnumerable<'T> with 
+              member x.GetEnumerator() = gen() 
+          interface IEnumerable with 
+              member x.GetEnumerator() = (gen() :> IEnumerator) }
 
+    type EnumerableBuilder() = 
+        member x.Delay(f) = delay f
+        member x.YieldFrom(a) = a
+        member x.Yield(a) = singleton a
+        member x.Bind(a:IEnumerator<'a>, f:_ -> IEnumerator<'b>) = bind f a
+        member x.Combine(a, b) = combine a b
+        member x.Zero() = empty<_>
+    let iter = new EnumerableBuilder()    
+
+    let head (en:IEnumerator<'a>) =
+        if en.MoveNext() then en.Current
+        else invalidOp "!"
+  
+    let length (en:IEnumerator<'a>) =
+        let rec loop acc =
+            if en.MoveNext() then loop (acc+1)
+            else acc
+        loop 0
+  
+    let skip n (en:IEnumerator<'a>) =
+        if n = 0 then en
+        else
+            let rec loop acc =
+                if not (en.MoveNext()) then empty<_>
+                elif n = acc then en
+                else loop (acc+1)
+            loop 1
+  
+    let take n (en:IEnumerator<'a>) =
+      if n = 0 then empty<_>
+      else
+        let rec loop acc = iter {
+          let! v = en
+          match v with
+          | None -> ()
+          | Some v' ->
+            yield v'
+            if acc = n then ()
+            else yield! loop (acc+1) }
+        loop 1
+
+    // Implementing the zip function for enumerators
+    let rec zip xs ys = iter {
+        let! x = xs
+        let! y = ys
+        match x, y with 
+        | Some(x), Some(y) ->
+            yield x, y
+            yield! zip xs ys 
+        | _ -> () }
+   
+    let rec map f en = iter {
+        let! x = en
+        match x with 
+        | Some(x) -> yield f x
+                     yield! map f en
+        | _ -> () }               
+   
 module Seq =
     /// <summary>
     /// Adds an index to a sequence
@@ -72,18 +193,18 @@ type JoinList<'a> =
     | Join of JoinList<'a> * JoinList<'a> * int (* the total length of the JoinList *)
     with
     interface IEnumerable<'a> with
-        member this.GetEnumerator() =
+        member x.GetEnumerator() =
             let enumerable = seq {
-                match this with
+                match x with
                 | EmptyJoinList -> () 
                 | Unit x -> yield x
                 | Join(x,y,_) ->
                     yield! x :> seq<'a>
                     yield! y :> seq<'a> }
             enumerable.GetEnumerator()
-        member this.GetEnumerator() =
+        member x.GetEnumerator() =
             let enumerable = seq {
-                match this with
+                match x with
                 | EmptyJoinList -> () 
                 | Unit x -> yield x
                 | Join(x,y,_) ->
@@ -146,9 +267,7 @@ type JoinList<'a> with
     static member op_Append(left, right) = JoinList.append left right
 
 /// An ArraySegment with structural comparison and equality.
-[<CustomEquality; CustomComparison>]
-[<SerializableAttribute>]
-[<StructAttribute>]
+[<CustomEquality; CustomComparison; SerializableAttribute; StructAttribute>]
 type BS =
     val Array: byte[]
     val Offset: int
@@ -174,6 +293,17 @@ type BS =
             match other with
             | :? BS as other' -> BS.Compare(x, other')
             | _ -> invalidArg "other" "Cannot compare a value of another type."
+    interface System.Collections.Generic.IEnumerable<byte> with
+        /// Gets an enumerator for the bytes stored in the byte string.
+        /// NOTE: The enumerator gets a new reference to the contained data and does not mutate the actual byte string.
+        member x.GetEnumerator() =
+            if x.Count = 0 then Enumerator.empty<_>
+            else (x.Array.[x.Offset..(x.Offset + x.Count-1)] :> seq<byte>).GetEnumerator()
+        /// Gets an enumerator for the bytes stored in the byte string.
+        /// NOTE: The enumerator gets a new reference to the contained data and does not mutate the actual byte string.
+        member x.GetEnumerator() =
+            if x.Count = 0 then Enumerator.empty<_> :> IEnumerator
+            else x.Array.[x.Offset..(x.Offset + x.Count-1)].GetEnumerator()
   
 module ByteString =
     /// An active pattern for conveniently retrieving the properties of a BS.
@@ -186,13 +316,12 @@ module ByteString =
     let ofSeq s = let arr = Array.ofSeq s in BS(arr, 0, arr.Length)
     let ofList l = BS(Array.ofList l, 0, l.Length)
     let ofString (s:string) = s.ToCharArray() |> Array.map byte |> create
-    let toArray (bs:BS) = bs.Array.[bs.Offset..(bs.Count - 1)]
-    let toSeq (bs:BS) =
-        seq { for i in bs.Offset..(bs.Offset + bs.Count - 1) do yield bs.Array.[i] }
-    let toList (bs:BS) =
-        [ for i in bs.Offset..(bs.Offset + bs.Count - 1) -> bs.Array.[i] ]
-    let toString (bs:BS) =
-        System.Text.Encoding.ASCII.GetString(bs.Array, bs.Offset, bs.Count)
+    let toArray (bs:BS) =
+        if bs.Count = 0 then Array.empty<_>
+        else bs.Array.[bs.Offset..(bs.Offset + bs.Count - 1)]
+    let toSeq (bs:BS) = bs :> seq<byte>
+    let toList (bs:BS) = List.ofSeq bs
+    let toString (bs:BS) = System.Text.Encoding.ASCII.GetString(bs.Array, bs.Offset, bs.Count)
     let isEmpty (bs:BS) = 
         #if NET40
         Contract.Requires(bs.Count >= 0)
