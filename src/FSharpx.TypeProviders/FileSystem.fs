29f823aede895828537773b084c69d3413dabe65
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-09-27 12:07:47 +0200

    Improving addXmlDoc by using type constraints.

--- src/FSharpx.TypeProviders/FileSystem.fs
+++ src/FSharpx.TypeProviders/FileSystem.fs
@@ -2,26 +2,25 @@
 
 // originally from https://gist.github.com/1241061
 
-open System.Reflection
-open Microsoft.FSharp.Core.CompilerServices
 open Samples.FSharpPreviewRelease2011.ProvidedTypes
-open System.Text.RegularExpressions
 open FSharpx.TypeProviders.Settings
 open FSharpx.TypeProviders.DSL
 open System.IO
 
-let rec annotateWithSubdirectories (fileSystemInfo:FileSystemInfo) (ownerTy:ProvidedTypeDefinition) =
+let rec annotateAsFileSystemInfo (fileSystemInfo:FileSystemInfo) (ownerTy:ProvidedTypeDefinition) =
     try        
         let annotated =
-            ownerTy.AddXmlDoc <| sprintf "A strongly typed interface to '%s'" fileSystemInfo.FullName
             ownerTy
+                |> addXmlDoc (sprintf "A strongly typed interface to '%s'" fileSystemInfo.FullName)
                 |> hideOldMethods
-                |> addMember (literalField "Path" (sprintf "Full path to '%s'" fileSystemInfo.FullName) fileSystemInfo.FullName)
+                |> addMember (
+                    literalField "Path" fileSystemInfo.FullName
+                      |> addXmlDoc (sprintf "Full path to '%s'" fileSystemInfo.FullName))
         
         match fileSystemInfo with
         | :? DirectoryInfo as dir ->
              dir.EnumerateFileSystemInfos()
-                |> Seq.map (fun info -> runtimeType<obj> info.Name |> annotateWithSubdirectories info)
+                |> Seq.map (fun info -> runtimeType<obj> info.Name |> annotateAsFileSystemInfo info)
                 |> Seq.fold (fun ownerType subdirType -> addMember subdirType ownerType) annotated
         | _ -> annotated    
     with 
@@ -31,4 +30,4 @@ let typedFileSystem =
     erasedType<obj> thisAssembly rootNamespace "FileSystemTyped"
       |> staticParameter "path" (fun typeName path -> 
             erasedType<obj> thisAssembly rootNamespace typeName
-                |> annotateWithSubdirectories (new DirectoryInfo(path)))
+                |> annotateAsFileSystemInfo (new DirectoryInfo(path)))
--- src/FSharpx.TypeProviders/TypeProviderDSL.fs
+++ src/FSharpx.TypeProviders/TypeProviderDSL.fs
@@ -11,16 +11,18 @@ let hideOldMethods (typeDef:ProvidedTypeDefinition) =
     typeDef.HideObjectMethods <- true
     typeDef
 
+let inline addXmlDoc xmlDoc (definition: ^a) = 
+    (^a : (member AddXmlDoc: string -> unit) (definition,xmlDoc))
+    definition
+
 let runtimeType<'a> typeName = 
     ProvidedTypeDefinition(typeName = cleanupTypeName typeName, baseType = Some typeof<'a>)
 
 let erasedType<'a> assemblyName rootNamespace typeName = 
     ProvidedTypeDefinition(assemblyName, rootNamespace, cleanupTypeName typeName, Some typeof<'a>)
 
-let literalField name xmlDoc (value:'a) =
-    let field = ProvidedLiteralField(cleanupTypeName name, typeof<'a>, value)
-    field.AddXmlDoc xmlDoc
-    field
+let literalField name (value:'a) =
+    ProvidedLiteralField(cleanupTypeName name, typeof<'a>, value)
 
 let addMember memberDef (typeDef:ProvidedTypeDefinition) =
     typeDef.AddMember memberDef
