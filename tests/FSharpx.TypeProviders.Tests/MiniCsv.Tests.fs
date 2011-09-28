204fda28cf8b89b419c9c30e4e0d4cd07df539d2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-09-28 15:48:09 +0200

    Added MiniCsv type provider

--- tests/FSharpx.TypeProviders.Tests/MiniCsv.Tests.fs
+++ tests/FSharpx.TypeProviders.Tests/MiniCsv.Tests.fs
@@ -18,4 +18,3 @@ let ``Can create type for empty document``() =
     row1.Distance |> should equal 50.<metre>
     let time = row1.Time // try doing "Go To Definition" on Time property
     time |> should equal 3.7<second>
-    
