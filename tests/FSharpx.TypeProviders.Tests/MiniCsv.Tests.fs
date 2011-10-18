880079e033422783af8c20930ebbd651a59f964a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-10-18 11:40:47 +0200

    Use nchrunch for TypeProviders.Tests

--- tests/FSharpx.TypeProviders.Tests/MiniCsv.Tests.fs
+++ tests/FSharpx.TypeProviders.Tests/MiniCsv.Tests.fs
@@ -18,10 +18,3 @@ let ``Can create type for small document``() =
     row1.Distance |> should equal 50.<metre>
     let time = row1.Time // try doing "Go To Definition" on Time property
     time |> should equal 3.7<second>
-
-[<Test>] 
-let ``Can create type for document with same structure on different path``() = 
-    let csv = smallCsv()
-    let row1 = csv.Data |> Seq.head 
-
-    row1.Distance |> should equal 55.<metre>
