294537d997d3aa61e2781ace2539de9b16f5f6c5
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-10-23 17:09:13 +0200

    Adding a first version of the registry type provider.

---
+++ tests/FSharpx.TypeProviders.Tests/Registry.Tests.fs
@@ -0,0 +1,17 @@
+﻿module FSharp.TypeProviders.Tests.RegistryTests
+
+open FSharpx
+open NUnit.Framework
+open FsUnit
+
+type LocalRegistry = RegistryTyped<""> // Todo: Try to get rid of this useless static parameter
+
+[<Test>] 
+let ``Can create type for HKEY_CURRENT_USER``() = 
+    LocalRegistry.HKEY_CURRENT_USER.Path 
+      |> should equal @"HKEY_CURRENT_USER"
+
+[<Test>] 
+let ``Can create subtype for HKEY_LOCAL_MACHINE``() = 
+    LocalRegistry.HKEY_LOCAL_MACHINE.SOFTWARE.Path 
+      |> should equal @"HKEY_LOCAL_MACHINE\SOFTWARE"
