1363c78d2298ac24f8cc9bd32bf0be5b1876ab64
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-10-24 09:44:57 +0200

    Adding replace function to Strings module

---
+++ tests/FSharpx.CSharpTests/StringsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace FSharpx.CSharpTests
+{
+    [TestFixture]
+    public class StringsTests
+    {
+        [Test]
+        public void WhenDetectingIfStringStartsWithHelloAndItDoes()
+        {
+            Assert.IsTrue(Strings.startsWith("Hello", "Hello World"));
+        }
+
+        [Test]
+        public void WhenDetectingIfStringStartsWithHelloAndItDoesnt()
+        {
+            Assert.IsFalse(Strings.startsWith("Hello", "World"));
+        }
+
+        [Test]
+        public void WhenReplacingSimplePatternInStringWhichContainsThePattern()
+        {
+            Assert.AreEqual(Strings.replace("Hello", "World", "Hello World!"), "World World!");
+        }
+
+        [Test]
+        public void WhenReplacingSimplePatternInStringWhichDoesntContainThePattern()
+        {
+            Assert.AreEqual(Strings.replace("Hello?", "World", "Hello World!"), "Hello World!");
+        }
+    }
+}
