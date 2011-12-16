4f7b784b70dc73cfd464e2c8daa4a450a54247e6
Gustavo Leon <gustavo@perezleon.com.ar>
2011-12-17 00:52:22 +0100

    + State

--- src/FSharpx.Core/Monad.fs
+++ src/FSharpx.Core/Monad.fs
@@ -357,6 +357,23 @@ module Nullable =
 
 module State =
 
+    type State<'s,'a> = State of ('s->('a * 's)) with
+        static member (?<-) (_      , _Functor:Fmap  ,   State m   ) = fun f -> State(fun s -> let (a, s') = m s in (f a, s'))
+
+    let runState (State x) = x
+    type State<'s,'a> with
+        static member (?<-) (_      , _Monad  :Return, _:State<_,_>) = fun a -> State(fun s -> (a, s))
+        static member (?<-) (State m, _Monad  :Bind  , _:State<_,_>) = fun k -> State(fun s -> let (a, s') = m s in runState(k a) s')
+
+    let mapState  f (State m)  = State(f << m)
+    let withState f (State m)  = State(m << f)
+    let evalState (State sa) s = fst(sa s)
+    let execState (State sa) s = snd(sa s)
+    let get   = State (fun s -> (s , s))
+    let put x = State (fun _ -> ((), x))
+
+
+    //
     type State_<'a, 's> = 's -> 'a * 's
     
     let getState = fun s -> (s,s)
