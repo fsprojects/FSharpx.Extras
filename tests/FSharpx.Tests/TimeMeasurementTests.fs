308d8089f60cbd64aa05e40adf3bdd00ac2c394b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-01-18 18:36:28 +0100

    Added TimeMeasurement helpers - making the test less fragile

--- tests/FSharpx.Tests/TimeMeasurementTests.fs
+++ tests/FSharpx.Tests/TimeMeasurementTests.fs
@@ -9,7 +9,7 @@ let ``When using a stopwatch the elapsed time should be measured``() =
     let f = fun () -> Threading.Thread.Sleep(100)
     let runtime = stopTime f |> snd
 
-    Assert.GreaterOrEqual(runtime,100.)
+    Assert.GreaterOrEqual(runtime, 80.)
 
 [<Test>]
 let ``When using a stopwatch the average time should be measured ``() =
