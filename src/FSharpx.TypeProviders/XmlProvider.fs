0aaea5db5efa5331086009f668c3e1a7b61c82ca
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-03-19 10:58:33 +0100

    Starting to use the type provider DSL in the XML provider - generateType extracted

--- src/FSharpx.TypeProviders/XmlProvider.fs
+++ src/FSharpx.TypeProviders/XmlProvider.fs
@@ -13,30 +13,16 @@ open Microsoft.FSharp.Quotations
 open Samples.FSharp.ProvidedTypes
 open System.Xml.Linq
 
-let xmlType (ownerType:TypeProviderForNamespaces) (cfg:TypeProviderConfig) =  
-  erasedType<obj> thisAssembly rootNamespace "StructuredXml"  
-  |> staticParameter "FileName"  // Parameterize the type by the file to use as a template
-      (fun typeName fileName ->
-        let resolvedFileName = findConfigFile cfg.ResolutionFolder fileName
-        let doc = XDocument.Load resolvedFileName
-        watchForChanges ownerType resolvedFileName
+// Generates type for an inferred XML element
+let rec generateType (ownerType:ProvidedTypeDefinition) (ProvidedXElement(name, niceName, children, attributes)) =
+    let ty = runtimeType<TypedXElement> (makeUniqueName niceName)
+    ownerType.AddMember(ty)
 
-        // -------------------------------------------------------------------------------------------
-        // Infer schema from the loaded data and generate type with properties
-
-        let schema = Inference.provideElement doc.Root.Name.LocalName [doc.Root]      
-        let resTy = erasedType<TypedXDocument> thisAssembly rootNamespace typeName
-
-        // Generates type for an inferred XML element
-        let rec generateType (ProvidedXElement(name, niceName, children, attributes)) =
-          let ty = runtimeType<TypedXElement> (makeUniqueName niceName)
-          resTy.AddMember(ty)
-
-          // Generate property for every inferred attribute of the element
-          for ProvidedXAttribute(name, niceName, typ, opt) in attributes do 
-            if opt then
-              // For optional elements, we return Option value
-              ProvidedProperty(niceName, optionType typ, GetterCode = fun [self] ->
+    // Generate property for every inferred attribute of the element
+    for ProvidedXAttribute(name, niceName, typ, opt) in attributes do 
+        if opt then
+            // For optional elements, we return Option value
+            ProvidedProperty(niceName, optionType typ, GetterCode = fun [self] ->
                 let accessExpr = 
                   <@@  (%%self:TypedXElement).Element.
                           Attribute(XName.op_Implicit name).Value @@> 
@@ -52,25 +38,39 @@ let xmlType (ownerType:TypeProviderForNamespaces) (cfg:TypeProviderConfig) =
                     Expr.NewUnionCase(some, [accessExpr]),
                     Expr.NewUnionCase(none, []) ) )
               |> ty.AddMember
-            else
-              ProvidedProperty(niceName, typ, GetterCode = fun [self] ->
+        else
+            ProvidedProperty(niceName, typ, GetterCode = fun [self] ->
                 <@@ (%%self:TypedXElement).Element.
                       Attribute(XName.op_Implicit name).Value @@>
                 |> convertExpr typ)
               |> ty.AddMember
 
 
-          // Iterate over all the XML elements, generate type for them
-          // and add member for accessing them to the parent.
-          for (ProvidedXElement(name, niceName, _, _)) as child in children do
-            let chty = generateType child
+    // Iterate over all the XML elements, generate type for them
+    // and add member for accessing them to the parent.
+    for (ProvidedXElement(name, niceName, _, _)) as child in children do
+        let chty = generateType ownerType child
 
-            ProvidedMethod("Get" + niceName + "Elements", [], seqType chty, InvokeCode = fun [self] ->
+        ProvidedMethod("Get" + niceName + "Elements", [], seqType chty, InvokeCode = fun [self] ->
               <@@ seq { for e in ((%%self:TypedXElement).Element.Elements(XName.op_Implicit name)) -> TypedXElement(e) } @@>)
-            |> ty.AddMember
+          |> ty.AddMember
 
-          ty
+    ty
 
+let xmlType (ownerType:TypeProviderForNamespaces) (cfg:TypeProviderConfig) =  
+  erasedType<obj> thisAssembly rootNamespace "StructuredXml"  
+  |> staticParameter "FileName"  // Parameterize the type by the file to use as a template
+      (fun typeName fileName ->
+        let resolvedFileName = findConfigFile cfg.ResolutionFolder fileName
+        let doc = XDocument.Load resolvedFileName
+        watchForChanges ownerType resolvedFileName
+
+        // -------------------------------------------------------------------------------------------
+        // Infer schema from the loaded data and generate type with properties
+
+        let schema = Inference.provideElement doc.Root.Name.LocalName [doc.Root]      
+        let resTy = erasedType<TypedXDocument> thisAssembly rootNamespace typeName
+       
         // -------------------------------------------------------------------------------------------
         // Generate constructors for loading XML data and add type representing Root node        
         resTy
@@ -84,5 +84,5 @@ let xmlType (ownerType:TypeProviderForNamespaces) (cfg:TypeProviderConfig) =
             |> addXmlDoc "Initializes a XML document from the given path.")
         |+!> provideProperty
                 "Root"
-                (generateType schema)
+                (generateType resTy schema)
                 (fun args -> <@@ TypedXElement((%%args.[0] : TypedXDocument).Document.Root) @@>))
