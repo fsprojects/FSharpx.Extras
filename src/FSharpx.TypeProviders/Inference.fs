717ef1aeb4dd2dc12544bd4361c725dc14b34471
mausch <mauricioscheffer@gmail.com>
2012-03-19 18:48:03 -0300

    typo

--- src/FSharpx.TypeProviders/Inference.fs
+++ src/FSharpx.TypeProviders/Inference.fs
@@ -6,20 +6,20 @@ open FSharpx.TypeProviders.DSL
 open System.Collections.Generic
 open System.Globalization
 
-/// Checks wether the string is a boolean value
+/// Checks whether the string is a boolean value
 let isBool (s:string) =
     let l = s.ToLower()
     l = "true" || l = "false" || l = "yes" || l = "no"
 
-/// Checks wether the string is an int
+/// Checks whether the string is an int
 let isInt (s:string) = Int32.TryParse s |> fst
 
-/// Checks wether the string is a float
+/// Checks whether the string is a float
 let isFloat (s:string) =
       Double.TryParse(s, NumberStyles.Float, CultureInfo.InvariantCulture) 
         |> fst
 
-/// Checks wether all values of the sequence can be inferred to a special type
+/// Checks whether all values of the sequence can be inferred to a special type
 let inferType values =     
     if Seq.forall isBool values then typeof<bool>
     elif Seq.forall isInt values then typeof<int>
