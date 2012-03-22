1e61e5610d9f8d2627936949da403a9d4d00bff9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-03-22 14:09:27 +0100

    Starting a WPF sample app using the XAML type provider

---
+++ samples/WPF/WPFSample/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open System.Windows
+open System.Windows.Controls
+
+let loadWindow() =    
+    let resourceLocator = new Uri("/WPFSample;component/Window.xaml", UriKind.Relative)
+    let window = Application.LoadComponent(resourceLocator) :?> Window
+    (window.FindName("Button1") :?> Button).Click.Add(
+        fun _ -> MessageBox.Show("Hello world!") |> ignore)
+    window
+
+[<STAThread>]
+(new Application()).Run(loadWindow()) |> ignore
