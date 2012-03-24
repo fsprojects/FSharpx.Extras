18a5f53fb1a3e8a476b715dc47cb4aa2beb955ab
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-03-24 17:06:11 +0100

    Cleanup the XAML sample project

--- samples/WPF/WPFSample/Program.fs
+++ samples/WPF/WPFSample/Program.fs
@@ -6,10 +6,12 @@ open FSharpx
 type MainWindow = XAML<"Window.xaml">
 
 let loadWindow() =
-   let window = MainWindow()
-   window.Button1.Click.Add(
-        fun _ -> MessageBox.Show("Hello world!") |> ignore)
-   window.MainWindow
+    let window = MainWindow()
+    window.Button1.Click.Add(fun _ -> 
+        MessageBox.Show("Hello world!") 
+        |> ignore)
+    window.MainWindow
 
 [<STAThread>]
-(new Application()).Run(loadWindow()) |> ignore
+(new Application()).Run(loadWindow()) 
+|> ignore
