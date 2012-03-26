3937f5a189d155f31f9fd5c7b1258a776d7b537a
mausch <mauricioscheffer@gmail.com>
2012-03-26 00:46:50 -0300

    #37 initial task computation builder

--- src/FSharpx.Core/Monad.fs
+++ src/FSharpx.Core/Monad.fs
@@ -112,6 +112,66 @@ module Async =
     let foldM f s = 
         Seq.fold (fun acc t -> acc >>= (flip f) t) (returnM s)
 
+module Task =
+    open System.Threading.Tasks
+
+    let inline fromAsync x =
+        let abegin, aend, acancel = Async.AsBeginEnd (fun () -> x)
+        Task.Factory.FromAsync<_>((fun a b -> abegin((), a, b)), aend, null)
+
+    let bind (f: 'a -> Task<'b>) (m: Task<'a>) = 
+        m.ContinueWith(fun (x: Task<_>) -> f x.Result).Unwrap()
+
+    let inline returnM a = 
+        let s = TaskCompletionSource()
+        s.SetResult a
+        s.Task
+
+    /// Sequentially compose two actions, passing any value produced by the first as an argument to the second.
+    let inline (>>=) m f = bind f m
+    /// Flipped >>=
+    let inline (=<<) f m = bind f m
+    /// Sequentially compose two either actions, discarding any value produced by the first
+    let inline (>>.) m1 m2 = m1 >>= (fun _ -> m2)
+    /// Left-to-right Kleisli composition
+    let inline (>=>) f g = fun x -> f x >>= g
+    /// Right-to-left Kleisli composition
+    let inline (<=<) x = flip (>=>) x
+
+    /// Promote a function to a monad/applicative, scanning the monadic/applicative arguments from left to right.
+    let inline lift2 f a b = 
+        a >>= fun aa -> b >>= fun bb -> f aa bb |> returnM
+
+    /// Sequential application
+    let inline ap x f = lift2 id f x
+
+    /// Sequential application
+    let inline (<*>) f x = ap x f
+
+    /// Transforms a Task's first value by using a specified mapping function.
+    let inline map f (m: Task<'a>) =
+        m.ContinueWith(fun (t: Task<_>) -> f t.Result)
+
+    /// Infix map
+    let inline (<!>) f x = map f x
+
+    /// Sequence actions, discarding the value of the first argument.
+    let inline ( *>) a b = lift2 (fun _ z -> z) a b
+    /// Sequence actions, discarding the value of the second argument.
+    let inline ( <*) a b = lift2 (fun z _ -> z) a b
+    
+    type TaskBuilder() =
+        member this.Return x = returnM x
+        member this.ReturnFrom (a: 'a Task) = a
+        member this.Bind(m, f) = bind f m
+        member this.Combine(comp1, comp2) = 
+            this.Bind(comp1, fun () -> comp2)
+        member this.TryFinally(m, compensation) =
+            try this.ReturnFrom m
+            finally compensation()
+        member this.Using(res:#IDisposable, body) =
+            this.TryFinally(body res, fun () -> match res with null -> () | disp -> disp.Dispose())
+
 module ZipList = 
     let returnM v = Seq.initInfinite (fun _ -> v)
     /// Sequential application
---
+++ tests/FSharpx.Tests/TaskTests.fs
@@ -0,0 +1,45 @@
+﻿module FSharpx.Tests.TaskTests
+
+open System
+open System.IO
+open System.Net
+open NUnit.Framework
+open FSharpx
+open FSharpx.Task
+open System.Threading.Tasks
+
+let task = TaskBuilder()
+
+type WebRequest with
+    member x.GetResponseTask() =
+        Task.Factory.FromAsync((fun a b -> x.BeginGetResponse(a,b)), x.EndGetResponse, null)
+
+type StreamReader with
+    member x.ReadToEndTask() = 
+        fromAsync (x.AsyncReadToEnd())
+
+[<Test>]
+let loadprices() =
+    let url = "http://ichart.finance.yahoo.com/table.csv?s=MSFT&d=9&e=30&f=2008&g=d&a=2&b=13&c=1986&ignore=.csv"
+    let req = WebRequest.Create url
+    let downloadTask =
+        task {
+            let! resp = req.GetResponseTask()
+            let stream = resp.GetResponseStream()
+            let reader = new StreamReader(stream)
+            let! csv = reader.ReadToEndTask()
+            let prices =
+                csv.Split([|'\n'|])
+                |> Seq.skip 1
+                |> Seq.map (fun line -> line.Split([|','|]))
+                |> Seq.filter (fun values -> values |> Seq.length = 7)
+                |> Seq.map (fun values ->
+                    let t = DateTime.parse values.[0] |> Option.get
+                    let p = decimal values.[6]
+                    t,p)
+                |> Seq.toList
+            return prices
+        }
+    let t,p = downloadTask.Result.[0]
+    Assert.AreEqual(DateTime(2008,10,30), t)
+    Assert.AreEqual(20.82m, p)
