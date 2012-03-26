7e434330b3fad9674c100587ef6d1c0a43786b2f
mausch <mauricioscheffer@gmail.com>
2012-03-26 16:37:29 -0300

    minor test changes

--- tests/FSharpx.CSharpTests/SetTests.cs
+++ tests/FSharpx.CSharpTests/SetTests.cs
@@ -6,16 +6,16 @@ namespace FSharpx.CSharpTests {
     [TestFixture]
     public class SetTests {
         [Test]
-        public void New() {
+        public void Create() {
             FSharpSet<int> set1 = FSharpSet.Create(1, 2, 3);
             FSharpSet<int> set2 = new FSharpSet<int>(new[] { 1, 2, 3 });
             Assert.AreEqual(set1, set2);
         }
 
         [Test]
-        public void ToSet() {
+        public void ToFSharpSet() {
             FSharpSet<int> set = new[] { 1, 2, 3 }.ToFSharpSet();
-
+            Assert.AreEqual(3, set.Count);
         }
     }
 }
