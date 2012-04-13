d7aaff7f2ca15ed00fffaa39d49a2ed0a5fc824b
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-04-13 13:52:06 +0200

    Implicit queue taken from http://lepensemoi.free.fr/index.php/2010/02/18/implicit-queue

---
+++ tests/FSharpx.DataStructures.Tests/ImplicitQueueTest.fs
@@ -0,0 +1,11 @@
+﻿module FSharpx.DataStructures.Tests.ImplicitQueueTest
+
+open System
+open FSharpx.DataStructures
+open FSharpx.DataStructures.ImplicitQueue
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty should be Shallow Zero``() =
+    empty |> should equal (Shallow Zero)
