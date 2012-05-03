e03136ff7a1b551ca0ac9e2206e40614e08ade97
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-05-03 19:09:57 +0200

    Removing the dependency to Unquote.Assertions

--- tests/FSharpx.Tests/RegexTests.fs
+++ tests/FSharpx.Tests/RegexTests.fs
@@ -2,53 +2,51 @@ module FSharpx.Tests.RegexTests
 
 open FSharpx
 open NUnit.Framework
-open Swensen.Unquote.Assertions
 open System
 open System.Text.RegularExpressions
+open FsUnit
 
 [<Test>]
 let ``tryMatch success``() =
     let m = "John Smith" |> Regex.tryMatch "(?i)^john .*" |> Option.map (fun m -> { m with MatchValue = m.MatchValue.ToLowerInvariant() })
-    test <@ m.IsSome @>
-    test <@ m.Value.MatchValue = "john smith" @>
+    m.IsSome |> should equal true
+    m.Value.MatchValue |> should equal "john smith"
 
 [<Test>]
 let ``replace with accumulator``() =
     let count, r = 
         Regex "%." 
         |> Regex.replaceWithAcc (fun s _ -> let s = s+1 in s, sprintf "@p%d" s) 0 "values (%d, %s)"
-    test <@ count = 2 && r = "values (@p1, @p2)" @>
+    count |> should equal 2
+    r |> should equal "values (@p1, @p2)"
 
 [<Test>]
 let ``replace with fixed number of replacements``() =
-    let r =
-        Regex "%."
-        |> Regex.replaceWith ["@p1"; "@p2"] "values (%d, %s, %i)"
-    test <@ r = "values (@p1, @p2, %i)" @>
+    Regex "%."
+    |> Regex.replaceWith ["@p1"; "@p2"] "values (%d, %s, %i)"
+    |> should equal "values (@p1, @p2, %i)"
 
 let tryMatchName = Regex.tryMatchWithOptions RegexOptions.None "^(\w*) (\w* )?(\w*)$"
 
 [<Test>]
 let ``match with null input is none`` () =
-    test <@ tryMatchName null |> Option.isNone @>
+    tryMatchName null |> Option.isNone |> should equal true
 
 [<Test>]
 let ``non match is none`` () =
-    test <@ tryMatchName "john" |> Option.isNone @>
+    tryMatchName "john" |> Option.isNone |> should equal true
 
 [<Test>]
 let ``successful match with one optional group unsuccessful`` () =
     let result = tryMatchName "john smith"
-    test <@ result.IsSome @>
+    result.IsSome |> should equal true
     
     let result = result.Value
 
-    test <@ result.Match <> null @>
-    test <@ result.Groups.Length = 3 @>
-    test <@ result.MatchValue = "john smith" @>
-    test <@ result.GroupValues = ["john"; ""; "smith"] @>
-    test <@ result.OptionalGroupValues = [Some "john"; None; Some "smith"] @>
-
+    result.Groups.Length |> should equal 3
+    result.MatchValue |> should equal "john smith"
+    result.GroupValues |> should equal ["john"; ""; "smith"]
+    result.OptionalGroupValues |> should equal [Some "john"; None; Some "smith"]
 
 [<Test>]
 let ``all variations yield same match value result`` () =
@@ -62,17 +60,17 @@ let ``all variations yield same match value result`` () =
     let activeCompiled = Regex.Compiled.(|Match|_|) pattern input
     let activeInterpreted = Regex.Interpreted.(|Match|_|) pattern input
 
-    test <@ withoutOptions.IsSome @>
-    test <@ withoutOptions.Value.MatchValue = input @>
+    withoutOptions.IsSome |> should equal true
+    withoutOptions.Value.MatchValue |> should equal input
 
-    test <@ withOptions.IsSome @>
-    test <@ withOptions.Value.MatchValue = input @>
+    withOptions.IsSome |> should equal true
+    withOptions.Value.MatchValue |> should equal input
 
-    test <@ activeWithOptions.IsSome @>
-    test <@ activeWithOptions.Value.MatchValue = input @>
+    activeWithOptions.IsSome |> should equal true
+    activeWithOptions.Value.MatchValue |> should equal input
 
-    test <@ activeCompiled.IsSome @>
-    test <@ activeCompiled.Value.MatchValue = input @>
+    activeCompiled.IsSome |> should equal true
+    activeCompiled.Value.MatchValue |> should equal input
 
-    test <@ activeInterpreted.IsSome @>
-    test <@ activeInterpreted.Value.MatchValue = input @>
+    activeInterpreted.IsSome |> should equal true
+    activeInterpreted.Value.MatchValue |> should equal input
