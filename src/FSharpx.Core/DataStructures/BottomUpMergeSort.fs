ed28663c7f1f359d5346fb23b068061f469c821e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-05-27 13:18:29 +0200

    Adding BottomUp merge sort from Chris Okasaki’s “Purely functional data structures” - original implementation taken from http://lepensemoi.free.fr/index.php/2010/01/07/bottom-up-merge-sort

---
+++ src/FSharpx.Core/DataStructures/BottomUpMergeSort.fs
@@ -0,0 +1,40 @@
+﻿// BottomUp merge sort from Chris Okasaki’s “Purely functional data structures”
+// original implementation taken from http://lepensemoi.free.fr/index.php/2010/01/07/bottom-up-merge-sort
+module FSharpx.DataStructures.BottomUpMergeSort
+
+open FSharpx
+
+type Sortable<'a> = {
+    Size: int
+    Segments: Lazy<list<list<'a>>> }
+
+let rec merge xs ys =
+    match xs, ys with
+    | [], ys -> ys
+    | xs, [] -> xs
+    | x::tlx, y::tly ->
+        if x <= y then
+            x :: merge tlx ys
+        else
+            y :: merge xs tly
+
+let empty<'a> : Sortable<'a> = { Size = 0; Segments = lazy [] }
+
+let isEmpty x = x.Size = 0
+
+let singleton x = { Size = 1; Segments = lazy [[x]] }
+
+let rec addSeg seg segs size =
+    if size % 2 = 0 then
+        seg::segs
+    else
+        addSeg (merge seg (List.head segs)) (List.tail segs) (size / 2)
+
+let add x y = { Size = y.Size + 1; Segments = lazy addSeg [x] (Lazy.force y.Segments) y.Size }
+
+let rec mergeAll xs ys =
+    match xs, ys with
+    | xs, [] -> xs
+    | xs, seg::segs -> mergeAll (merge xs seg) segs
+
+let sort x = mergeAll [] (Lazy.force x.Segments)
---
+++ tests/FSharpx.DataStructures.Tests/BottomUpMergeSortTest.fs
@@ -0,0 +1,31 @@
+﻿module FSharpx.DataStructures.Tests.BottomUpMergeSortTest
+
+open System
+open FSharpx.DataStructures
+open FSharpx.DataStructures.BottomUpMergeSort
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty list should be empty``() =
+    empty |> isEmpty |> should equal true
+
+[<Test>]
+let ``empty list should be empty after sort``() =
+    sort empty |> should equal []
+
+[<Test>]
+let ``singleton list should be the same after sort``() =
+    sort (singleton 1) |> should equal [1]
+
+[<Test>]
+let ``adding a element to an empty list``() =
+    empty |> add 1 |> sort |> should equal [1]
+
+[<Test>]
+let ``adding multiple elements to an empty list``() =
+    empty |> add 100 |> add 1 |> add 3 |> add 42 |> sort |> should equal [1; 3; 42; 100]
+
+[<Test>]
+let ``adding multiple strings to an empty list``() =
+    empty |> add "100" |> add "1" |> add "3" |> add "42" |> sort |> should equal ["1"; "100"; "3"; "42"]
