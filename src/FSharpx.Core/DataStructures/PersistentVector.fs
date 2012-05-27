0b4c3b0e66d0cd9416d16e921c4181ed367dddf8
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-05-27 18:54:04 +0200

    Starting to port the persistent vector implementation ported from https://github.com/clojure/clojure/blob/master/src/jvm/clojure/lang/PersistentVector.java

---
+++ src/FSharpx.Core/DataStructures/PersistentVector.fs
@@ -0,0 +1,90 @@
+﻿/// A persistent vector implementation ported from https://github.com/clojure/clojure/blob/master/src/jvm/clojure/lang/PersistentVector.java
+module FSharpx.DataStructures.PersistentVector
+
+open FSharpx
+
+type Node(array: obj[])=
+    new() = Node(Array.create 32 null)
+    member this.Array = array
+
+let emptyNode = Node()
+
+type PersistentVector<'a> =  {
+    Count : int
+    Shift : int
+    Root : Node
+    Tail : obj[]
+    TailOff: int }
+
+let inline persistentVector<'a>(count,shift,root,tail) : PersistentVector<'a> = {
+    Count = count
+    Shift = shift
+    Root = root
+    Tail = tail 
+    TailOff = 
+        if count < 32 then 0 else
+        ((count - 1) >>> 5) <<< 5 }
+
+let empty<'a> = persistentVector<'a>(0,5,emptyNode,[||])
+
+let rec newPath(level,node:Node) =
+    if level = 0 then node else
+    let ret = Node()
+    ret.Array.[0] <- newPath(level - 5,node) :> obj
+    ret
+
+let rec pushTail(vector,level,parent:Node,tailnode) =
+    //if parent is leaf, insert node,
+    // else does it map to an existing child? -> nodeToInsert = pushNode one more level
+    // else alloc new path
+    //return  nodeToInsert placed in copy of parent
+    let subidx = ((vector.Count - 1) >>> level) &&& 0x01f
+    let ret = Node(Array.copy parent.Array)
+
+    let nodeToInsert =
+        if level = 5 then tailnode else
+
+        let child = parent.Array.[subidx]
+        if child <> null then
+            pushTail(vector,level-5,child :?> Node,tailnode)
+        else
+            newPath(level-5,tailnode)
+
+    ret.Array.[subidx] <- nodeToInsert :> obj
+    ret
+
+let inline arrayFor<'a> i (vector:PersistentVector<'a>) =
+    if i >= 0 && i < vector.Count then
+        if i >= vector.TailOff then vector.Tail else
+            let node = ref vector.Root
+            let level = ref vector.Shift
+            while !level > 0 do
+                let pos = (i >>> !level) &&& 0x01f
+                node := (!node).Array.[pos] :?> Node
+                level := !level - 5
+
+            (!node).Array
+    else failwith "IndexOutOfbounds"
+
+let nth<'a> i (vector:PersistentVector<'a>) : 'a =
+    let node = arrayFor i vector
+    node.[i &&& 0x01f] :?> 'a
+
+let cons<'a> (x:'a) (vector:PersistentVector<'a>) =
+    if vector.Count - vector.TailOff < 32 then
+        let newTail = Array.append vector.Tail [|x:>obj|]
+        persistentVector<'a>(vector.Count + 1,vector.Shift,vector.Root,newTail)
+    else
+        //full tail, push into tree
+        let tailNode = Node(vector.Tail)
+        let newShift = vector.Shift
+
+        //overflow root?
+        if (vector.Count >>> 5) > (1 <<< vector.Shift) then
+            let newRoot = Node()
+            newRoot.Array.[0] <- vector.Root :> obj
+            newRoot.Array.[1] <- newPath(vector.Shift,tailNode) :> obj
+            persistentVector<'a>(vector.Count + 1,vector.Shift + 5,newRoot,[| x |])
+        else
+            let newRoot = pushTail(vector,vector.Shift,vector.Root,tailNode)
+            persistentVector<'a>(vector.Count + 1,vector.Shift,newRoot,[| x |])
---
+++ tests/FSharpx.DataStructures.Tests/PersistentVectorTest.fs
@@ -0,0 +1,30 @@
+﻿module FSharpx.DataStructures.Tests.PersistentVectorTest
+
+open System
+open FSharpx.DataStructures
+open FSharpx.DataStructures.PersistentVector
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty vector should be empty``() =
+    let x = empty<int>
+    x.Count |> should equal 0
+
+[<Test>]
+let ``cons to an empty vector should create a singleton vector``() =
+    empty |> cons 1 |> nth 0 |> should equal 1
+
+[<Test>]
+let ``multiple cons to an empty vector should create a vector``() =
+    empty |> cons 1 |> cons 4 |> cons 25 |> nth 1 |> should equal 4
+
+[<Test>]
+let ``more than 200 cons to an empty vector should create a vector``() =
+    let xs = [1..256]
+    let vector = ref empty
+    for i in 1..300 do
+        vector := cons i (!vector)
+
+    !vector |> nth 100 |> should equal 101
+    !vector |> nth 200 |> should equal 201
