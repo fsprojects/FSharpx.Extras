b66050018b49ebcaddf5402448bd41afd9a7ddcf
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-05-28 11:43:33 +0200

    Starting with TransientVector

---
+++ src/FSharpx.Core/DataStructures/TransientVector.fs
@@ -0,0 +1,131 @@
+﻿/// A transient vector implementation ported from https://github.com/clojure/clojure/blob/master/src/jvm/clojure/lang/PersistentVector.java
+module FSharpx.DataStructures.TransientVector
+
+open FSharpx
+
+type Node = obj[]
+
+let newNode() = Array.create 32 null    
+
+let emptyNode = newNode()
+
+type TransientVector<'a> =  {
+    Count : int
+    Shift : int
+    Root : Node
+    Tail : obj[]
+    TailOff: int }
+
+let inline count vector = vector.Count
+
+let inline transientVector<'a>(count,shift,root,tail) : TransientVector<'a> = {
+    Count = count
+    Shift = shift
+    Root = root
+    Tail = tail 
+    TailOff = 
+        if count < 32 then 0 else
+        ((count - 1) >>> 5) <<< 5 }
+
+let empty<'a> = transientVector<'a>(0,5,emptyNode,[||])
+
+let rec newPath(level,node:Node) =
+    if level = 0 then node else
+    let ret = newNode()
+    ret.[0] <- newPath(level - 5,node) :> obj
+    ret
+
+let rec pushTail(vector,level,parent:Node,tailnode) =
+    //if parent is leaf, insert node,
+    // else does it map to an existing child? -> nodeToInsert = pushNode one more level
+    // else alloc new path
+    //return  nodeToInsert placed in copy of parent
+    let subidx = ((vector.Count - 1) >>> level) &&& 0x01f
+    let ret = Array.copy parent
+
+    let nodeToInsert =
+        if level = 5 then tailnode else
+
+        let child = parent.[subidx]
+        if child <> null then
+            pushTail(vector,level-5,child :?> Node,tailnode)
+        else
+            newPath(level-5,tailnode)
+
+    ret.[subidx] <- nodeToInsert :> obj
+    ret
+
+let inline arrayFor<'a> i (vector:TransientVector<'a>) =
+    if i >= 0 && i < vector.Count then
+        if i >= vector.TailOff then vector.Tail else
+            let node = ref vector.Root
+            let level = ref vector.Shift
+            while !level > 0 do
+                let pos = (i >>> !level) &&& 0x01f
+                node := (!node).[pos] :?> Node
+                level := !level - 5
+
+            !node
+    else raise Exceptions.OutOfBounds
+
+let nth<'a> i (vector:TransientVector<'a>) : 'a =
+    let node = arrayFor i vector
+    node.[i &&& 0x01f] :?> 'a
+
+let cons<'a> (x:'a) (vector:TransientVector<'a>) =
+    if vector.Count - vector.TailOff < 32 then
+        let newTail = Array.append vector.Tail [|x:>obj|]
+        transientVector<'a>(vector.Count + 1,vector.Shift,vector.Root,newTail)
+    else
+        //full tail, push into tree
+        let tailNode = vector.Tail
+        let newShift = vector.Shift
+
+        //overflow root?
+        if (vector.Count >>> 5) > (1 <<< vector.Shift) then
+            let newRoot = newNode()
+            newRoot.[0] <- vector.Root :> obj
+            newRoot.[1] <- newPath(vector.Shift,tailNode) :> obj
+            transientVector<'a>(vector.Count + 1,vector.Shift + 5,newRoot,[| x |])
+        else
+            let newRoot = pushTail(vector,vector.Shift,vector.Root,tailNode)
+            transientVector<'a>(vector.Count + 1,vector.Shift,newRoot,[| x |])
+
+
+let rec doAssoc(level,node:Node,i,x) =
+    let ret = Array.copy node
+    if level = 0 then 
+        ret.[i &&& 0x01f] <- x :> obj 
+    else
+        let subidx = (i >>> level) &&& 0x01f
+        ret.[subidx] <- doAssoc(level - 5, node.[subidx] :?> Node, i, x) :> obj
+    ret
+
+let assocN<'a> i (x:'a) (vector:TransientVector<'a>) : TransientVector<'a> =
+    if i >= 0 && i < vector.Count then
+        if i >= vector.TailOff then
+            let newTail = Array.copy vector.Tail
+            newTail.[i &&& 0x01f] <- x :> obj
+            transientVector(vector.Count, vector.Shift, vector.Root, newTail)
+        else
+            transientVector(vector.Count, vector.Shift, doAssoc(vector.Shift, vector.Root, i, x), vector.Tail)
+    elif i = vector.Count then
+        cons x vector
+    else raise Exceptions.OutOfBounds
+
+let rangedIterator<'a> startIndex endIndex (vector:TransientVector<'a>) : 'a seq =
+    let i = ref startIndex
+    let b = ref (!i - (!i % 32))
+    let array = if startIndex < vector.Count then ref (arrayFor !i vector) else ref null
+
+    seq {
+        while !i < endIndex do
+            if !i - !b = 32 then
+                array := arrayFor !i vector
+                b := !b + 32
+
+            yield (!array).[!i &&& 0x01f] :?> 'a
+            i := !i + 1 
+       }
+
+let toSeq vector = rangedIterator 0 vector.Count vector
---
+++ tests/FSharpx.DataStructures.Tests/TransientVectorTest.fs
@@ -0,0 +1,52 @@
+﻿module FSharpx.DataStructures.Tests.TransientVectorTest
+
+open System
+open FSharpx.DataStructures
+open FSharpx.DataStructures.TransientVector
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty vector should be empty``() =
+    let x = empty<int>
+    x |> count |> should equal 0
+
+[<Test>]
+let ``multiple cons to an empty vector should increase the count``() =
+    empty |> cons 1 |> cons 4 |> cons 25 |> count |> should equal 3
+
+[<Test>]
+let ``cons to an empty vector should create a singleton vector``() =
+    empty |> cons 1 |> nth 0 |> should equal 1
+
+[<Test>]
+let ``multiple cons to an empty vector should create a vector``() =
+    empty |> cons 1 |> cons 4 |> cons 25 |> nth 1 |> should equal 4
+
+[<Test>]
+let ``300 cons to an empty vector should create a vector``() =
+    let vector = ref empty
+    for i in 1..300 do
+        vector := cons i (!vector)
+
+    !vector |> nth 100 |> should equal 101
+    !vector |> nth 200 |> should equal 201
+
+[<Test>]
+let ``assoc an element to a nonempty vector should not change the original vector``() =
+    let v = empty |> cons "1" |> cons "4" |> cons "25" 
+
+    v |> assocN 2 "5" |> nth 2 |> should equal "5"
+    v |> nth 2 |> should equal "25"
+
+[<Test>]
+let ``vector should should be convertable to a seq``() =
+    empty |> cons 1 |> cons 4 |> cons 25 |> toSeq |> Seq.toList |> should equal [1;4;25]
+
+[<Test>]
+let ``vector with more 300 elements should be convertable to a seq``() =
+    let vector = ref empty
+    for i in 1..300 do
+        vector := cons i (!vector)
+
+    !vector |> toSeq |> Seq.toList |> should equal [1..300]
