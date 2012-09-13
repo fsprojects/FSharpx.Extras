4bdae4ce1537605967162e121c5fa762a4c09010
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-09-13 20:34:28 +0200

    Adding a BorderGatewayProtocol sample for the graph type provider.

---
+++ tests/FSharpx.TypeProviders.Tests/BorderGatewayProtocolGraph.Tests.fs
@@ -0,0 +1,23 @@
+﻿module FSharpx.TypeProviders.Tests.BorderGatewayProtocolGraphTests
+
+open NUnit.Framework
+open FSharpx
+open FSharpx.TypeProviders
+open FsUnit
+
+type BGP = Graph<"BorderGatewayProtocol.dgml", "Idle"> 
+
+[<Test>]
+let ``Initially everything is idle``() =   
+    let state = BGP.InitialState
+    Assert.AreEqual(state.Name,"Idle")
+
+[<Test>]
+let ``Connection can be established``() =   
+    let state = 
+        BGP.InitialState
+          .TransitToConnect()
+          .TransitToOpenSent()
+          .TransitToOpenConfirm()
+          .TransitToEstablished()
+    Assert.AreEqual(state.Name,"Established")
