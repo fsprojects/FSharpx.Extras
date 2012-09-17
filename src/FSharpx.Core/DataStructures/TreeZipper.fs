54ca8eee844243ab691eebebca325e193694e162
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-09-17 14:53:50 +0200

    Little cleanup of the tree zipper

--- src/FSharpx.Core/DataStructures/TreeZipper.fs
+++ src/FSharpx.Core/DataStructures/TreeZipper.fs
@@ -9,7 +9,6 @@ type 'a Tree=
    | Branch of 'a * 'a Tree * 'a Tree
 
 type TreeDirection =  Left | Right
-type ZipperDirection = Up | Left | Right 
 
 /// The tree zipper datastructure
 type 'a TreeZipper  = { 
@@ -21,10 +20,8 @@ open FSharpx
 /// Moves the zipper one level up
 let up z =
     match z.Path with
-    | (d,v,other)::ep ->  
-        match d with 
-        | TreeDirection.Left  -> { Focus = Branch(v,z.Focus,other); Path = ep }
-        | TreeDirection.Right -> { Focus = Branch(v,other,z.Focus); Path = ep }
+    | (Left,v,other)::ep ->  { Focus = Branch(v,z.Focus,other); Path = ep }
+    | (Right,v,other)::ep -> { Focus = Branch(v,other,z.Focus); Path = ep }
     | [] -> failwith "can't go up" // because ep only goes down and is empty 
 
 /// Moves the zipper to the top
@@ -36,14 +33,19 @@ let rec top z  =
 /// Moves the zipper to the left
 let left z = 
     match z.Focus with
-    | (Branch(v, explored, other)) -> { Focus = explored; Path = (TreeDirection.Left, v, other)::z.Path }
+    | (Branch(v, explored, other)) -> { Focus = explored; Path = (Left, v, other) :: z.Path }
     | Leaf -> failwith "can't go down on leaf"
 
 /// Moves the zipper to the right
 let right z = 
     match z.Focus with
-    | (Branch(v, other, explored)) -> { Focus = explored; Path= (TreeDirection.Right, v, other)::z.Path }
+    | (Branch(v, other, explored)) -> { Focus = explored; Path = (Right, v, other) :: z.Path }
     | Leaf -> failwith "can't go down on leaf"
+
+/// Creates a zipper from a tree
+let zipper t = { Focus = t; Path = [] } 
+
+type TreeZipperDirection = Up | Left | Right 
 
 let inline getMove direction =
     match direction with
@@ -55,7 +57,4 @@ let inline getMove direction =
 let rec move directions (z:'a TreeZipper) =
     directions
       |> Seq.map getMove
-      |> Seq.fold (|>) z
-        
-/// Creates a zipper from a tree
-let zipper t = { Focus = t; Path = [] } 
+      |> Seq.fold (|>) z       
