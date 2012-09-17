4cd21ba14509517095dfc64660e38bdfb555d6b0
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-09-17 12:22:32 +0200

    Little cleanup of the tree zipper

--- src/FSharpx.Core/DataStructures/TreeZipper.fs
+++ src/FSharpx.Core/DataStructures/TreeZipper.fs
@@ -3,40 +3,59 @@
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module FSharpx.DataStructures.TreeZipper
 
+/// A simple binarys tree
 type 'a Tree=
    | Leaf
-   | Branch of  'a * 'a Tree * 'a Tree
+   | Branch of 'a * 'a Tree * 'a Tree
 
-type TDirection =  Left | Right
-type ZDirection = Up | Left | Right 
+type TreeDirection =  Left | Right
+type ZipperDirection = Up | Left | Right 
 
-type 'a Zipper  = { focus:'a Tree;  path: (TDirection * 'a * 'a Tree) list}
+/// The tree zipper datastructure
+type 'a TreeZipper  = { 
+  Focus:'a Tree
+  Path: (TreeDirection * 'a * 'a Tree) list }
 
 open FSharpx
 
-let up z =  match z.path with
-            | (d,v,other)::ep ->  match d with 
-                                    | TDirection.Left  -> {focus=Branch(v,z.focus,other); path=ep}
-                                    | TDirection.Right -> {focus=Branch(v,other,z.focus); path=ep}
-            | []        -> failwith "can't go up" // because ep only goes down and is empty 
+/// Moves the zipper one level up
+let up z =
+    match z.Path with
+    | (d,v,other)::ep ->  
+        match d with 
+        | TreeDirection.Left  -> { Focus = Branch(v,z.Focus,other); Path = ep }
+        | TreeDirection.Right -> { Focus = Branch(v,other,z.Focus); Path = ep }
+    | [] -> failwith "can't go up" // because ep only goes down and is empty 
 
-let rec top z  =  match z.path with [] -> z | _  -> top (up z)
+/// Moves the zipper to the top
+let rec top z  = 
+    match z.Path with 
+    | [] -> z 
+    | _  -> top (up z)
 
-let left z  =   match z.focus with
-                        | (Branch(v, l, r)) ->  let v, explored, other = v, l, r 
-                                                {focus=explored; path=((TDirection.Left, v, other)::z.path)}
-                        | Leaf -> failwith "can't go down on leaf"
+/// Moves the zipper to the left
+let left z = 
+    match z.Focus with
+    | (Branch(v, explored, other)) -> { Focus = explored; Path = (TreeDirection.Left, v, other)::z.Path }
+    | Leaf -> failwith "can't go down on leaf"
 
-let right z =  match z.focus with
-                        | (Branch(v, l, r)) ->  let v, explored, other = v, r, l 
-                                                {focus=explored; path=((TDirection.Right, v, other)::z.path)}
-                        | Leaf -> failwith "can't go down on leaf"
+/// Moves the zipper to the right
+let right z = 
+    match z.Focus with
+    | (Branch(v, other, explored)) -> { Focus = explored; Path= (TreeDirection.Right, v, other)::z.Path }
+    | Leaf -> failwith "can't go down on leaf"
 
-let rec move (p:ZDirection list) (z:'a Zipper) =
-    match p with 
-    | []    -> z 
-    | d::xs -> match d with 
-                | Up    -> move xs (up z)
-                | Left  -> move xs (left z)
-                | Right -> move xs (right z)
-let fromTree t = {focus = t; path = []} 
+let inline getMove direction =
+    match direction with
+    | Up -> up
+    | Left -> left
+    | Right -> right
+    
+/// Moves the zipper in the directions of the given list
+let rec move directions (z:'a TreeZipper) =
+    directions
+      |> Seq.map getMove
+      |> Seq.fold (|>) z
+        
+/// Creates a zipper from a tree
+let zipper t = { Focus = t; Path = [] } 
--- tests/FSharpx.DataStructures.Tests/TreeZipperTest.fs
+++ tests/FSharpx.DataStructures.Tests/TreeZipperTest.fs
@@ -7,35 +7,34 @@ open FSharpx.DataStructures.TreeZipper
 open NUnit.Framework
 open FsUnit
 
-
 let tree  = Branch("a", Branch("b", Leaf, Branch("c", Leaf, Leaf)), Branch("d", Leaf, Leaf))
 
 [<Test>]
 let ``Can create zipper from tree``() =       
-   let z1 = tree |> fromTree
-   Assert.AreEqual(z1.focus,tree)
+   let z1 = tree |> zipper
+   Assert.AreEqual(z1.Focus,tree)
 
 [<Test>]
 let ``Can move down to the left inside the zipper``() =       
-   let z1 = tree |> fromTree |> move [Left]
-   Assert.AreEqual(z1.focus,Branch("b", Leaf, Branch("c", Leaf, Leaf)))
+   let z1 = tree |> zipper |> left
+   Assert.AreEqual(z1.Focus,Branch("b", Leaf, Branch("c", Leaf, Leaf)))
 
 [<Test>]
 let ``Can move down to the right inside the zipper``() =       
-   let z1 = tree |> fromTree |> move [Right]
-   Assert.AreEqual(z1.focus,Branch("d", Leaf, Leaf))
+   let z1 = tree |> zipper |> right
+   Assert.AreEqual(z1.Focus,Branch("d", Leaf, Leaf))
 
 [<Test>]
 let ``Can move down to the left and the right inside the zipper``() =       
-   let z1 = tree |> fromTree |> move [Left;Right]
-   Assert.AreEqual(z1.focus,Branch("c", Leaf, Leaf))
+   let z1 = tree |> zipper |> move [Left;Right]
+   Assert.AreEqual(z1.Focus,Branch("c", Leaf, Leaf))
 
 [<Test>]
 let ``Can move up inside the zipper``() =       
-   let z1 = tree |> fromTree |> move [Left;Right;Right;Up;Up;Up]
-   Assert.AreEqual(z1.focus,tree)
+   let z1 = tree |> zipper |> move [Left;Right;Right;Up;Up;Up]
+   Assert.AreEqual(z1.Focus,tree)
 
 [<Test>]
 let ``Can move to the top from inside the zipper``() =       
-   let z1 = tree |> fromTree |> move [Left;Right;Right] |> top
-   Assert.AreEqual(z1.focus,tree)
+   let z1 = tree |> zipper |> move [Left;Right;Right] |> top
+   Assert.AreEqual(z1.Focus,tree)
