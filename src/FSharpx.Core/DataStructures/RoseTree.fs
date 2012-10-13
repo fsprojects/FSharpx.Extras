ed5966bd49e8ef47889945fb55a3736b6d718aec
mausch <mauricioscheffer@gmail.com>
2012-10-13 13:27:45 -0300

    Start RoseTree

---
+++ src/FSharpx.Core/DataStructures/RoseTree.fs
@@ -0,0 +1,54 @@
+﻿namespace FSharpx.DataStructures
+
+open System
+open System.Linq
+
+/// Multi-way tree, also known as rose tree.
+// Ported from http://hackage.haskell.org/packages/archive/containers/latest/doc/html/src/Data-Tree.html
+type 'a RoseTree = { Root: 'a; Children: 'a RoseTree list }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module RoseTree =
+    let inline create root children = { Root = root; Children = children }
+
+    let inline singleton x = create x []
+
+    let rec map f (x: _ RoseTree) = 
+        { RoseTree.Root = f x.Root
+          Children = List.map (map f) x.Children }
+
+    let rec ap f x =
+        { RoseTree.Root = f.Root x.Root
+          Children = 
+            let a = List.map (map f.Root) x.Children
+            let b = List.map (fun c -> ap c x) f.Children
+            List.append a b }
+
+    let rec bind f x =
+        let a = f x.Root
+        { RoseTree.Root = a.Root
+          Children = List.append a.Children (List.map (bind f) x.Children) }
+
+    let rec dfsPre (x: _ RoseTree) =
+        seq {
+            yield x.Root
+            yield! Seq.collect dfsPre x.Children
+        }
+
+    let rec dfsPost (x: _ RoseTree) =
+        seq {
+            yield! Seq.collect dfsPost x.Children
+            yield x.Root
+        }
+
+    let rec unfold f seed =
+        let root, bs = f seed
+        create root (unfoldForest f bs)
+
+    and unfoldForest f =
+        List.map (unfold f)
+
+// TODO: 
+// sequence / mapM / filterM
+// zipper: http://hackage.haskell.org/package/rosezipper-0.2
+// Using a LazyList kills equality. Is it worth it to define a custom equality?
---
+++ tests/FSharpx.DataStructures.Tests/RoseTreeTest.fs
@@ -0,0 +1,59 @@
+﻿module FSharpx.DataStructures.Tests.RoseTreeTest
+
+open FSharpx
+open FSharpx.DataStructures
+open NUnit.Framework
+
+let tree = RoseTree.create
+
+let atree = tree 1 [tree 2 [tree 3 []]; tree 4 [tree 5 [tree 6 []]]]
+let ctree = tree "f" [tree "b" [tree "a" []; tree "d" [tree "c" []; tree "e" []]]; tree "g" [tree "i" [tree "h" []]]]
+
+// examples borrowed from http://en.wikipedia.org/wiki/Tree_traversal#Example
+
+[<Test>]
+let ``dfs pre``() =
+    let actual = RoseTree.dfsPre ctree |> Seq.toList
+    Assert.AreEqual(["f";"b";"a";"d";"c";"e";"g";"i";"h"], actual)
+
+[<Test>]
+let ``dfs post``() =
+    let actual = RoseTree.dfsPost ctree |> Seq.toList
+    Assert.AreEqual(["a";"c";"e";"d";"b";"h";"i";"g";"f"], actual)
+    
+[<Test>]
+let map() =
+    let actual = RoseTree.map ((+) 1) atree
+    let expected = tree 2 [tree 3 [tree 4 []]; tree 5 [tree 6 [tree 7 []]]]
+    Assert.AreEqual(expected, actual)
+
+
+
+// not the best example, as text nodes cannot have children
+
+type HtmlElement = { TagName: string; Attributes: (string * string) list }
+
+type HtmlNode =
+| Element of HtmlElement
+| Text of string
+
+let elemA tag attr = HtmlNode.Element { TagName = tag; Attributes = attr }
+let elem tag = elemA tag []
+let text t = tree (HtmlNode.Text t) []
+type Html = HtmlNode RoseTree
+
+let htmldoc = 
+    tree (elem "body") [tree (elem "div") [text "hello world"]]
+
+[<Test>]
+let bind() =
+    let wrapText =
+        function
+        | Element _ as x -> RoseTree.singleton x
+        | Text t -> tree (elem "span") [text t]
+    let newDoc = htmldoc |> RoseTree.bind wrapText
+    let expected = tree (elem "body") [tree (elem "div") [tree (elem "span") [text "hello world"]]]
+    Assert.AreEqual(expected, newDoc)
+    printfn "%A" newDoc
+
+// TODO port example from http://blog.moertel.com/articles/2007/03/07/directory-tree-printing-in-haskell-part-two-refactoring
