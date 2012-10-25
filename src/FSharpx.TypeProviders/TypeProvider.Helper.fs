c37f17df186fdb10cc0c091bbba3e86dfa06136d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-10-25 19:58:37 +0200

    As discussed with @tpetricek on twitter. For a lot of the users it would be easier to contribute to the TypeProviders if they would not use these custom operators. This is the first attempt of a clean type provider.

---
+++ src/FSharpx.TypeProviders/TypeProvider.Helper.fs
@@ -0,0 +1,26 @@
+﻿/// Starting to implement some helpers on top of ProvidedTypes API
+module internal FSharpx.TypeProviders.Helper
+
+open System.IO
+
+let findConfigFile resolutionFolder configFileName =
+    if Path.IsPathRooted configFileName then 
+        configFileName 
+    else 
+        Path.Combine(resolutionFolder, configFileName)
+
+open Samples.FSharp.ProvidedTypes
+
+let eraseType assemblyName rootNamespace typeName toType = 
+    ProvidedTypeDefinition(assemblyName, rootNamespace, typeName, Some toType)
+
+let erasedType<'a> assemblyName rootNamespace typeName = 
+    eraseType assemblyName rootNamespace typeName typeof<'a>
+
+open FSharpx.Strings
+
+let literalField name (value:'a) = ProvidedLiteralField(niceName name, typeof<'a>, value)
+
+// Get the assembly and namespace used to house the provided types
+let thisAssembly = System.Reflection.Assembly.GetExecutingAssembly()
+let rootNamespace = "FSharpx"
