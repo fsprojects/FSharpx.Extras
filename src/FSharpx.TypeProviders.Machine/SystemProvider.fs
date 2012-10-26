07e6779d481122e3e1f5e7787da82f0287014406
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-10-26 08:34:58 +0200

    Moving back to the plain old type provider API in the registry provider.

--- src/FSharpx.TypeProviders.Machine/SystemProvider.fs
+++ src/FSharpx.TypeProviders.Machine/SystemProvider.fs
@@ -4,15 +4,16 @@ open System.Reflection
 open Microsoft.FSharp.Core.CompilerServices
 open Samples.FSharp.ProvidedTypes
 open System.Text.RegularExpressions
+open FSharpx.TypeProviders.Helper
 
 [<TypeProvider>]
 type public SystemProvider(cfg:TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces()
 
     do this.AddNamespace(
-        DSL.rootNamespace, 
+        rootNamespace, 
         [FilesTypeProvider.createTypedFileSystem()
-         RegistryProvider.typedRegistry])
+         RegistryProvider.createTypedRegistry()])
 
 [<TypeProviderAssembly>]
 do ()
