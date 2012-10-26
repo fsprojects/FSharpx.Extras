de229f7cad652968e207c65ec0e8b0367a8d0b3b
pocketberserker <pocketberserker@gmail.com>
2012-10-26 17:16:46 +0900

    Start BKTree

---
+++ src/FSharpx.Core/DataStructures/BKTree.fs
@@ -0,0 +1,195 @@
+﻿namespace FSharpx.DataStructures
+
+open System.Collections.Generic
+
+// Ported from http://hackage.haskell.org/packages/archive/bktrees/latest/doc/html/src/Data-Set-BKTree.html
+
+type 'a BKTree =
+  | Node of 'a * int * IDictionary<int,'a BKTree>
+  | Empty
+
+module BKTree =
+
+    open FSharpx
+
+    let isEmpty = function
+        | Empty -> true
+        | Node(_,_,_) -> false
+
+    let empty = Empty
+
+    let size = function
+        | Node(_, s, _) -> s
+        | Empty -> 0
+
+    let singleton a = Node(a, 1, Dictionary())
+
+    let rec insert distance a = function
+        | Empty -> Node(a, 1 , Dictionary())
+        | Node(b, size, dict) ->
+            let recurse _ tree = insert distance a tree
+            let d = distance a b
+            let dict = Dictionary.insertWith recurse d (Node(a, 1, Dictionary())) dict
+            Node(b, size + 1, dict)
+
+    let rec exists distance a = function
+        | Empty -> false
+        | Node(b, _, dict) ->
+            let d = distance a b
+            if d = 0 then true
+            else
+                match Dictionary.tryFind d dict with
+                | None -> false
+                | Some tree -> exists distance a tree
+
+    let private subTree d n dict =
+        let (_, rightTree) = dict |> Dictionary.splitWithKey ((>) (d-n-1))
+        let (centerTree, _) = rightTree |> Dictionary.splitWithKey ((>=) (d+n+1))
+        centerTree
+
+    let rec existsDistance distance n a = function
+        | Empty -> false
+        | Node(b, _, dict) ->
+            let d = distance a b
+            if d <= n then true
+            else
+                dict
+                |> subTree d n
+                |> Dictionary.valueList
+                |> List.exists (existsDistance distance n a)
+
+    let rec elems = function
+        | Empty -> []
+        | Node(a, _ , d) ->
+            a :: (d |> Dictionary.valueList |> List.collect elems)
+
+    let rec elemsDistance distance n a = function
+        | Empty -> []
+        | Node(b, _, dict) ->
+            let d = distance a b
+            dict
+            |> subTree d n
+            |> Dictionary.valueList
+            |> List.collect (elemsDistance distance n a)
+            |> if d <= n then List.cons b else id
+
+    let fromList distance xs = xs |> List.fold (flip (insert distance)) empty
+
+    let unions distance xs = xs |> List.collect elems |> fromList distance
+
+    let rec delete distance a = function
+        | Empty -> Empty
+        | Node(b, _, dict) ->
+            let d = distance a b
+            if d = 0 then unions distance (Dictionary.valueList dict)
+            else
+                let subtree = Dictionary.updateWith (Some << (delete distance a)) d dict
+                let size = subtree |> Dictionary.valueList |> List.map size |> List.sum |> (+) 1
+                Node(b, size, subtree)
+
+    module Int =
+
+        let distance i j = abs(i - j)
+
+        let insert a tree = insert distance a tree
+
+        let exists a tree = exists distance a tree
+  
+        let existsDistance n a tree = existsDistance distance n a tree
+
+        let elemsDistance n a tree = elemsDistance distance n a tree
+
+        let fromList xs = fromList distance xs
+
+        let unions xs = unions distance xs
+
+        let union t1 t2 = unions [t1; t2]
+
+        let delete a tree = delete distance a tree
+
+    module Char =
+
+        let distance(i:char) (j:char) = abs((int i) - (int j))
+
+        let insert a tree = insert distance a tree
+
+        let exists a tree = exists distance a tree
+
+        let existsDistance n a tree = existsDistance distance n a tree
+
+        let elemsDistance n a tree = elemsDistance distance n a tree
+
+        let fromList xs = fromList distance xs
+
+        let unions xs = unions distance xs
+
+        let union t1 t2 = unions [t1; t2]
+
+        let delete a tree = delete distance a tree
+
+    module List =
+
+        let private hirschberg xs = function
+            | [] -> List.length xs
+            | ys ->
+                let lys = List.length ys
+                let startArr = [1..lys] |> List.map (fun x -> (x,x))
+                xs
+                |> Seq.zip (Seq.unfold (fun i -> Some(i+1, i+1)) 0)
+                |> Seq.toList
+                |> List.fold (fun arr (i,xi) ->
+                    ys
+                    |> List.zip (List.sortBy fst arr)
+                    |> List.mapAccum (fun (s,c) ((j,el),yj) ->
+                        let nc = List.min [s + (if xi = yj then 0 else 1); el + 1; c + 1]
+                        ((el,nc),(j,nc))
+                    ) (i - 1, i)
+                    |> snd
+                ) startArr
+                |> List.find (fst >> (=) lys)
+                |> snd
+
+        let distance = hirschberg
+
+        let insert a tree = insert distance a tree
+
+        let exists a tree = exists distance a tree
+
+        let existsDistance n a tree = existsDistance distance n a tree
+
+        let elemsDistance n a tree = elemsDistance distance n a tree
+
+        let fromList xs = fromList distance xs
+
+        let unions xs = unions distance xs
+
+        let union t1 t2 = unions [t1; t2]
+
+        let delete a tree = delete distance a tree
+
+    module ByteString =
+
+        open ByteString
+
+        let distance xs = function
+            | ys when ByteString.isEmpty ys -> ByteString.length xs
+            | ys ->
+                let xs = ByteString.toList xs
+                let ys = ByteString.toList ys
+                List.distance xs ys
+
+        let insert a tree = insert distance a tree
+
+        let exists a tree = exists distance a tree
+
+        let existsDistance n a tree = existsDistance distance n a tree
+
+        let elemsDistance n a tree = elemsDistance distance n a tree
+
+        let fromList xs = fromList distance xs
+
+        let unions xs = unions distance xs
+
+        let union t1 t2 = unions [t1; t2]
+
+        let delete a tree = delete distance a tree
---
+++ tests/FSharpx.DataStructures.Tests/BKTreeTest.fs
@@ -0,0 +1,168 @@
+﻿module FSharpx.DataStructures.Tests.BKTreeTest
+
+open System.Collections.Generic
+open FSharpx
+open FSharpx.DataStructures
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+
+let fsCheck t = fsCheck "" t
+
+let sem tree = tree |> BKTree.elems |> List.sort
+
+let trans f xs = xs |> BKTree.Int.fromList |> f |> sem
+
+let rec inv dict = function
+    | Empty -> true
+    | Node(a, _, dictionary) ->
+        List.forall (fun (d,b) -> BKTree.Int.distance a b = d) dict &&
+            List.forall (fun (d,t) -> inv ((d,a)::dict) t) (Dictionary.toList dictionary)
+
+let invariant t = inv [] t
+
+[<Test>]
+let ``native empty``() =
+    fsCheck <| fun (xs :int list) ->
+        BKTree.List.distance [] xs = List.length xs && BKTree.List.distance xs [] = List.length xs
+
+[<Test>]
+let ``native cons``() =
+    fsCheck <| fun (x:int) xs ys ->
+        BKTree.List.distance (x::xs) (x::ys) = BKTree.List.distance xs ys
+
+[<Test>]
+let ``native diff``() =
+    fsCheck <| fun (x:int) y xs ys ->
+        x <> y ==>
+            (BKTree.List.distance (x::xs) (y::ys) =
+              1 + List.min [BKTree.List.distance (x::xs) ys; BKTree.List.distance (x::xs) (x::ys); BKTree.List.distance xs (y::ys)])
+
+[<Test>]
+let empty() =
+    fsCheck (fun n -> not <| BKTree.Int.exists n BKTree.empty)
+
+[<Test>]
+let isEmpty() =
+    fsCheck (fun xs -> BKTree.isEmpty (BKTree.Int.fromList xs) = List.isEmpty xs)
+
+[<Test>]
+let singleton() =
+    fsCheck (fun n -> BKTree.elems (BKTree.Int.fromList [n]) = [n])
+
+[<Test>]
+let fromList() =
+    fsCheck (fun xs -> sem (BKTree.Int.fromList xs) = List.sort xs)
+
+[<Test>]
+let ``fromList inv``() =
+    fsCheck (fun xs -> invariant (BKTree.Int.fromList xs))
+
+[<Test>]
+let insert() =
+    fsCheck (fun n xs -> trans (BKTree.Int.insert n) xs = List.sort (n::xs))
+
+[<Test>]
+let ``insert inv``() =
+    fsCheck (fun n xs -> invariant (BKTree.Int.insert n (BKTree.Int.fromList xs)))
+
+[<Test>]
+let exists() =
+    fsCheck (fun n xs -> BKTree.Int.exists n (BKTree.Int.fromList xs) = List.exists ((=) n) xs)
+
+[<Test>]
+let existsDistance() =
+    fsCheck <| fun dist n xs ->
+        let d = dist % 5
+        let reference = List.exists (fun e -> BKTree.Int.distance n e <= d) xs
+        Prop.collect reference
+            (BKTree.Int.existsDistance d n (BKTree.Int.fromList xs) = List.exists (fun e -> BKTree.Int.distance n e <= d) xs)
+
+[<Test>]
+let delete() =
+    let rec removeFirst n = function
+        | [] -> []
+        | x::xs when x = n -> xs
+        | x::xs -> x :: removeFirst n xs
+    fsCheck (fun n xs -> trans (BKTree.Int.delete n) xs = List.sort (removeFirst n xs))
+     
+[<Test>]
+let ``delete inv``() =
+    fsCheck (fun n xs -> invariant (BKTree.Int.delete n (BKTree.Int.fromList xs)))
+
+[<Test>]
+let elems() =
+    fsCheck (fun xs -> List.sort (BKTree.elems (BKTree.Int.fromList xs)) = List.sort xs)
+
+[<Test>]
+let elemsDistance() =
+    fsCheck <| fun dist n xs ->
+        let d = dist % 5
+        List.sort (BKTree.Int.elemsDistance d n (BKTree.Int.fromList xs)) =
+            List.sort (List.filter (fun e -> BKTree.Int.distance n e <= d) xs)
+
+[<Test>]
+let unions() =
+    fsCheck (fun xss -> sem (BKTree.Int.unions (List.map BKTree.Int.fromList xss)) = List.sort (List.concat xss))
+
+[<Test>]
+let ``unions inv``() =
+    fsCheck (fun xss -> invariant (BKTree.Int.unions (List.map BKTree.Int.fromList xss)))
+
+[<Test>]
+let union() =
+    fsCheck <| fun xs ys ->
+        sem (BKTree.Int.union (BKTree.Int.fromList xs) (BKTree.Int.fromList ys)) = List.sort (List.append xs ys)
+
+[<Test>]
+let ``union inv``() =
+    fsCheck (fun xs ys -> invariant (BKTree.Int.union (BKTree.Int.fromList xs) (BKTree.Int.fromList ys)))
+
+[<Test>]
+let ``delete . insert = id``() =
+    fsCheck (fun n xs -> trans ((BKTree.Int.delete n) << (BKTree.Int.insert n)) xs = List.sort xs)
+
+[<Test>]
+let ``The size of an empty BKTree is 0``() =
+    fsCheck (fun _ -> BKTree.size BKTree.empty = 0)
+
+[<Test>]
+let ``fromList and size``() =
+    fsCheck (fun xs -> BKTree.size (BKTree.Int.fromList xs) = List.length xs)
+
+[<Test>]
+let ``insert . size = size + 1``() =
+    fsCheck <| fun n xs ->
+        let tree = BKTree.Int.fromList xs
+        BKTree.size (BKTree.Int.insert n tree) = BKTree.size tree + 1
+
+[<Test>]
+let ``delete . size = size - 1``() =
+    fsCheck <| fun n xs ->
+        let tree = BKTree.Int.fromList xs
+        BKTree.size (BKTree.Int.delete n tree) = BKTree.size tree - if BKTree.Int.exists n tree then 1 else 0
+
+[<Test>]
+let ``union and size``() =
+    fsCheck <| fun xs ys ->
+        let treeXs = BKTree.Int.fromList xs
+        let treeYs = BKTree.Int.fromList ys
+        BKTree.size (BKTree.Int.union treeXs treeYs) = BKTree.size treeXs + BKTree.size treeYs
+
+[<Test>]
+let ``unions and size``() =
+    fsCheck <| fun xss ->
+        let trees = List.map BKTree.Int.fromList xss
+        BKTree.size (BKTree.Int.unions trees) = List.sum (List.map BKTree.size trees)
+
+[<Test>]
+let ``unions and exists``() =
+    fsCheck <| fun xss ->
+        let tree = BKTree.Int.unions (List.map BKTree.Int.fromList xss)
+        List.forall (fun x -> BKTree.Int.exists x tree) (List.concat xss)
+
+[<Test>]
+let ``fromList and exists``() =
+    fsCheck <| fun xs ->
+        let tree = BKTree.Int.fromList xs
+        List.forall (fun x -> BKTree.Int.exists x tree) xs
