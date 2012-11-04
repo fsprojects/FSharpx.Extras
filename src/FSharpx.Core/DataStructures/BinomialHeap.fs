7be9ab0b7334b91638da345462d5b853b582f955
Jack Fox <jackfoxy@gmail.com>
2012-11-04 14:31:28 -0800

    added BinomialHeap data structure

---
+++ src/FSharpx.Core/DataStructures/BinomialHeap.fs
@@ -0,0 +1,227 @@
+﻿// originally published by Julien
+// original implementation taken http://lepensemoi.free.fr/index.php/2009/12/10/binomial-heap
+
+//J.F. modified
+
+namespace FSharpx.DataStructures
+
+open System.Collections
+open System.Collections.Generic
+
+type BinomialTree<'a> = Node of (int * 'a * list<BinomialTree<'a>>)
+
+type BinomialHeap<'a when 'a : comparison> (isMaximalist : bool, heap : list<BinomialTree<'a>>) =
+
+
+
+    member private this.heap = heap
+
+    ///returns true if the heap has max element at head
+    member this.IsMaximalist = isMaximalist
+
+    ///returns the count of elememts, O(log n)
+    member this.Length() = List.fold (fun acc (Node(r, _, _)) -> acc + int (2.0**(float r))) 0 this.heap
+
+    static member internal  empty isMaximalist = BinomialHeap<'a>(isMaximalist, [])
+
+    static member private  isEmpty : BinomialHeap<'a> -> bool = function 
+        | h when h.heap.IsEmpty -> true 
+        | _ -> false
+
+    static member private  rank ((Node(r, _, _)) : BinomialTree<'a>) = r
+
+    static member private  root ((Node(_, x, _)) : BinomialTree<'a>) = x
+
+    static member private  link isMaximalist (Node(r, x1, xs1) as n1) (Node(_, x2, xs2) as n2) : BinomialTree<'a> =
+        if x1 <= x2 then
+            if isMaximalist then Node(r+1, x2, n1 :: xs2)
+            else Node(r+1, x1, n2 :: xs1)
+        else
+            if isMaximalist then Node(r+1, x1, n2 :: xs1)
+            else Node(r+1, x2, n1 :: xs2)
+
+    static member private insertTree (isMaximalist : bool) (t : BinomialTree<'a>) (h : list<BinomialTree<'a>>) : list<BinomialTree<'a>> = 
+        match h with
+        | [] -> [t]
+        | hd::tl as t' ->
+            if BinomialHeap.rank t < BinomialHeap.rank hd then t::t'
+            else BinomialHeap.insertTree isMaximalist (BinomialHeap.link isMaximalist t hd) tl
+
+    static member private  insert isMaximalist x (h : list<BinomialTree<'a>>) =
+        BinomialHeap.insertTree isMaximalist (Node(0, x, [])) h
+
+    static member private merge isMaximalist (h1  : list<BinomialTree<'a>>) (h2  : list<BinomialTree<'a>>)  : list<BinomialTree<'a>> =
+        match h1, h2 with
+        | [], x -> x
+        | x, [] -> x
+        | (hd1::tl1), (hd2::tl2) ->
+          if BinomialHeap.rank hd1 < BinomialHeap.rank hd2 
+          then hd1 :: BinomialHeap.merge isMaximalist tl1 h2
+          elif BinomialHeap.rank hd1 > BinomialHeap.rank hd2 then hd2 :: BinomialHeap.merge isMaximalist h1 tl2
+          else BinomialHeap.insertTree isMaximalist (BinomialHeap.link isMaximalist hd1 hd2) (BinomialHeap.merge isMaximalist ( tl1) ( tl2))
+
+    static member private removeHeadTree : bool * list<BinomialTree<'a>> -> (BinomialTree<'a> * list<BinomialTree<'a>>) = function
+        | _, [] -> raise Exceptions.Empty
+        | _, [t] -> t, []
+        | isMaximalist, hd::tl ->
+            let hd', tl'= BinomialHeap.removeHeadTree (isMaximalist, tl)
+            if isMaximalist then
+                if BinomialHeap.root hd > BinomialHeap.root hd' 
+                then hd, tl 
+                else hd', hd::tl'
+            else
+                if BinomialHeap.root hd <= BinomialHeap.root hd' 
+                then hd, tl 
+                else hd', hd::tl'
+
+    static member private head isMaximalist (h : list<BinomialTree<'a>>) : 'a =
+        let (Node(_, x, xs1), _) = BinomialHeap.removeHeadTree (isMaximalist, h)
+        x
+
+    static member internal ofSeq (maximalist: bool) (s:seq<'a>) : BinomialHeap<'a> = 
+        if Seq.isEmpty s then BinomialHeap.empty maximalist
+        else
+            let x = 
+                Seq.fold (fun acc elem -> [Node(0, elem, [])]::acc) [] s
+    
+            let pairWiseMerge (l: list<list<BinomialTree<'a>>>) =
+                let rec loop (acc: list<list<BinomialTree<'a>>>) : list<list<BinomialTree<'a>>> -> list<list<BinomialTree<'a>>> = function
+                    | h1::h2::tl -> loop ((BinomialHeap.merge maximalist h1 h2)::acc) tl
+                    | h1::[] -> h1::acc
+                    | [] -> acc
+
+                loop [] l
+
+            let rec loop : list<list<BinomialTree<'a>>> -> list<BinomialTree<'a>> = function
+                | h::[] -> h
+                | x -> loop (pairWiseMerge x)
+                
+            BinomialHeap<'a>(maximalist, (loop x)) 
+
+    static member private tail isMaximalist (h : list<BinomialTree<'a>>) =
+        let Node(_, x, xs1), xs2 = BinomialHeap.removeHeadTree (isMaximalist, h)
+        BinomialHeap.merge isMaximalist (List.rev xs1) xs2
+
+    static member private uncons isMaximalist (h : list<BinomialTree<'a>>) : 'a * BinomialHeap<'a> =
+        let Node(_, x, xs1), xs2 = BinomialHeap.removeHeadTree (isMaximalist, h)
+        x, BinomialHeap<'a>(isMaximalist, (BinomialHeap.merge isMaximalist (List.rev xs1) xs2))
+    
+    static member private foldHeap nodeF leafV (h : list<BinomialTree<'a>>) = 
+
+        let rec loop (h : list<BinomialTree<'a>>) cont =
+            match h with
+            | Node(_, a, h')::tl -> loop h'  (fun lacc ->  
+                                    loop tl (fun racc -> 
+                                    cont (nodeF a lacc racc))) 
+            | _ -> cont leafV
+           
+        loop h (fun x -> x)
+
+    static member private inOrder (h : list<BinomialTree<'a>>) = (BinomialHeap.foldHeap (fun x l r acc -> l (x :: (r acc))) (fun acc -> acc) h) [] 
+
+    ///returns the min or max element
+    member this.Head = BinomialHeap.head this.IsMaximalist this.heap
+
+    ///returns option first min or max element
+    member this.TryGetHead = 
+        if this.heap.IsEmpty then None
+        else Some(BinomialHeap.head this.IsMaximalist this.heap)
+
+    ///returns a new heap with the element inserted
+    member this.Insert x  = BinomialHeap<'a>(this.IsMaximalist, (BinomialHeap.insert this.IsMaximalist x this.heap))
+
+    ///returns true if the heap has no elements
+    member this.IsEmpty = BinomialHeap.isEmpty this
+
+    ///returns heap from merging two heaps, both must have same isMaximalist
+    member this.Merge (xs : BinomialHeap<'a>) : BinomialHeap<'a> = 
+        if (this.IsMaximalist = xs.IsMaximalist) then
+            BinomialHeap<'a>(this.IsMaximalist, (BinomialHeap.merge this.IsMaximalist this.heap xs.heap))
+        else failwith "not same max or min"
+
+    ///returns heap option from merging two heaps
+    member this.TryMerge (xs : BinomialHeap<'a>) = 
+        if (this.IsMaximalist <> xs.IsMaximalist) then None
+        else Some(BinomialHeap<'a>(this.IsMaximalist, (BinomialHeap.merge this.IsMaximalist this.heap xs.heap)))
+
+    ///returns a new heap of the elements trailing the head
+    member this.Tail = 
+        BinomialHeap<'a>(this.IsMaximalist, (BinomialHeap.tail this.IsMaximalist this.heap))
+       
+    ///returns option heap of the elements trailing the head
+    member this.TryGetTail = 
+        if this.heap.IsEmpty then None
+        else Some(BinomialHeap<'a>(this.IsMaximalist, (BinomialHeap.tail this.IsMaximalist this.heap)))
+
+    ///returns the head element and tail
+    member this.Uncons = BinomialHeap.uncons this.IsMaximalist this.heap
+
+    ///returns option head element and tail
+    member this.TryUncons = 
+        if this.heap.IsEmpty then None
+        else Some(BinomialHeap.uncons this.IsMaximalist this.heap)
+
+    interface IEnumerable<'a>
+
+        with 
+        member this.GetEnumerator() = 
+            let e = 
+                if this.IsMaximalist
+                then BinomialHeap.inOrder this.heap |> List.sort |> List.rev |> List.toSeq
+                else BinomialHeap.inOrder this.heap |> List.sort |> List.toSeq
+
+            e.GetEnumerator()
+
+        member this.GetEnumerator() = 
+            let e = 
+                if this.IsMaximalist
+                then BinomialHeap.inOrder this.heap |> List.sort |> List.rev |> List.toSeq
+                else BinomialHeap.inOrder this.heap |> List.sort |> List.toSeq
+
+            e.GetEnumerator() :> IEnumerator 
+
+module BinomialHeap = 
+    //pattern discriminator
+    let (|Cons|Nil|) (h: BinomialHeap<'a>) = match h.TryUncons with Some(a,b) -> Cons(a,b) | None -> Nil
+  
+    ///returns a empty heap
+    let empty (maximalist: bool) = BinomialHeap.empty maximalist
+
+    ///returns the min or max element
+    let inline head (xs: BinomialHeap<'a>)  = xs.Head
+
+    ///returns option first min or max element
+    let inline tryGetHead (xs: BinomialHeap<'a>)  = xs.TryGetHead
+
+    ///returns a new heap with the element inserted
+    let inline insert x (xs: BinomialHeap<'a>) = xs.Insert x   
+
+    ///returns true if the heap has no elements
+    let inline isEmpty (xs: BinomialHeap<'a>) = xs.IsEmpty
+
+    ///returns true if the heap has max element at head
+    let inline isMaximalist (xs: BinomialHeap<'a>) = xs.IsMaximalist
+
+    ///returns the count of elememts
+    let inline length (xs: BinomialHeap<'a>) = xs.Length() 
+
+    ///returns heap from merging two heaps, both must have same isMaximalist
+    let inline merge (xs: BinomialHeap<'a>) (ys: BinomialHeap<'a>) = xs.Merge ys
+
+    ///returns heap option from merging two heaps
+    let inline tryMerge (xs: BinomialHeap<'a>) (ys: BinomialHeap<'a>) = xs.TryMerge ys
+
+    ///returns heap from the sequence
+    let ofSeq maximalist s = BinomialHeap.ofSeq maximalist s
+
+    ///returns a new heap of the elements trailing the head
+    let inline tail (xs: BinomialHeap<'a>) = xs.Tail
+
+    ///returns option heap of the elements trailing the head
+    let inline tryGetTail (xs: BinomialHeap<'a>) = xs.TryGetTail
+
+    ///returns the head element and tail
+    let inline uncons (xs: BinomialHeap<'a>) = xs.Uncons
+
+    ///returns option head element and tail
+    let inline tryUncons (xs: BinomialHeap<'a>) = xs.TryUncons
---
+++ tests/FSharpx.DataStructures.Tests/BinomialHeapTest.fs
@@ -0,0 +1,226 @@
+﻿module FSharpx.DataStructures.Tests.BinomialHeapTest
+
+open FSharpx
+open FSharpx.DataStructures
+open FSharpx.DataStructures.BinomialHeap
+open FSharpx.Tests.Properties
+open NUnit.Framework
+open FsCheck
+open FsCheck.NUnit
+open FsUnit
+
+//only going up to 5 elements is probably sufficient to test all edge cases
+
+(*
+Could not get IHeap<'c, 'a when 'c :> IHeap<'c, 'a> and 'a : comparison> interface working smoothly between shared code,
+NUnit TestCaseSource(), FsCheck, and trying to pass around the tuple of heap generator and list. So need individual test
+file for each heap type, unlike IQueue.
+
+Even restricting only to this type, never got generic element type 'a to work. Need separate tests for int and string.
+*)
+
+let insertThruList l h  =
+    let rec loop (h' : BinomialHeap<'a>) (l' : 'a list) = 
+        match l' with
+        | hd :: [] -> h'.Insert hd
+        | hd :: tl -> loop (h'.Insert hd) tl
+        | [] -> h' 
+        
+    loop h l
+
+(* BinomialHeap Gens *)
+let maxBinomialHeapIntGen =
+        gen { let! n = Gen.length2thru12
+              let! n2 = Gen.length1thru12
+              let! x =  Gen.listInt n
+              let! y =  Gen.listInt n2
+              return ( (BinomialHeap.ofSeq true x |> insertThruList y), ((x @ y) |> List.sort |> List.rev) ) }
+
+let maxBinomialHeapIntOfSeqGen =
+        gen { let! n = Gen.length1thru12
+              let! x =  Gen.listInt n
+              return ( (BinomialHeap.ofSeq true x), (x |> List.sort |> List.rev) ) }
+
+let maxBinomialHeapIntInsertGen =
+        gen { let! n = Gen.length1thru12
+              let! x =  Gen.listInt n
+              return ( (BinomialHeap.empty true |> insertThruList x), (x |> List.sort |> List.rev) ) }
+
+let maxBinomialHeapStringGen =
+        gen { let! n = Gen.length1thru12
+              let! n2 = Gen.length2thru12
+              let! x =  Gen.listString n
+              let! y =  Gen.listString n2
+              return ( (BinomialHeap.ofSeq true x |> insertThruList y), ((x @ y) |> List.sort |> List.rev) ) }
+
+let minBinomialHeapIntGen =
+        gen { let! n = Gen.length2thru12
+              let! n2 = Gen.length1thru12
+              let! x =  Gen.listInt n
+              let! y =  Gen.listInt n2
+              return ( (BinomialHeap.ofSeq false x |> insertThruList y), ((x @ y) |> List.sort) ) }
+
+let minBinomialHeapIntOfSeqGen =
+        gen { let! n = Gen.length1thru12
+              let! x =  Gen.listInt n
+              return ( (BinomialHeap.ofSeq false x), (x |> List.sort |> List.rev) ) }
+
+let minBinomialHeapIntInsertGen =
+        gen { let! n = Gen.length1thru12
+              let! x =  Gen.listInt n
+              return ( (BinomialHeap.empty false |> insertThruList x), (x |> List.sort |> List.rev) ) }
+
+let minBinomialHeapStringGen =
+        gen { let! n = Gen.length1thru12
+              let! n2 = Gen.length2thru12
+              let! x =  Gen.listString n
+              let! y =  Gen.listString n2
+              return ( (BinomialHeap.ofSeq false x |> insertThruList y), ((x @ y) |> List.sort) ) }
+
+// NUnit TestCaseSource does not understand array of tuples at runtime
+let intGens start =
+    let v = Array.create 6 (box (maxBinomialHeapIntGen, "max BinomialHeap int"))
+    v.[1] <- box ((maxBinomialHeapIntOfSeqGen  |> Gen.suchThat (fun (q, l) -> l.Length >= start)), "max BinomialHeap OfSeq")
+    v.[2] <- box ((maxBinomialHeapIntInsertGen  |> Gen.suchThat (fun (q, l) -> l.Length >= start)), "max BinomialHeap from Insert")
+    v.[3] <- box (maxBinomialHeapIntGen , "max BinomialHeap int")
+    v.[4] <- box ((maxBinomialHeapIntOfSeqGen  |> Gen.suchThat (fun (q, l) -> l.Length >= start)), "max BinomialHeap OfSeq")
+    v.[5] <- box ((maxBinomialHeapIntInsertGen  |> Gen.suchThat (fun (q, l) -> l.Length >= start)), "max BinomialHeap from Insert")
+    v
+
+let stringGens =
+    let v = Array.create 2 (box (maxBinomialHeapStringGen, "max BinomialHeap string"))
+    v.[1] <- box (maxBinomialHeapStringGen, "max BinomialHeap string")
+    v
+
+let intGensStart1 =
+    intGens 1  //this will accept all
+
+let intGensStart2 =
+    intGens 2 // this will accept 11 out of 12
+
+[<Test>]
+let ``cons pattern discriminator``() =
+    let h = ofSeq true ["f";"e";"d";"c";"b";"a"]
+    let h1, t1 = uncons h 
+
+    let h2, t2 = 
+        match t1 with
+        | Cons(h, t) -> h, t
+        | _ ->  "x", t1
+
+    ((h2 = "e") && ((length t2) = 4)) |> should equal true
+
+[<Test>]
+let ``cons pattern discriminator 2``() =
+    let h = ofSeq true ["f";"e";"d";"c";"b";"a"]
+
+    let t2 = 
+        match h with
+        | Cons("f", Cons(_, t)) -> t
+        | _ ->  h
+
+    let h1, t3 = uncons t2 
+
+    ((h1 = "d") && ((length t2) = 4)) |> should equal true
+
+[<Test>]
+let ``empty list should be empty``() = 
+    (BinomialHeap.empty true).IsEmpty |> should equal true
+
+[<Test>]
+[<TestCaseSource("intGensStart2")>]
+let ``head should return``(x : obj) =
+    let genAndName = unbox x 
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun ((h : BinomialHeap<int>), (l : int list)) ->    
+                                                                            (h.Head = l.Head)     
+                                                                            |> classifyCollect h (h.Length())))
+[<Test>]
+let ``insert works``() =
+    (((BinomialHeap.empty true).Insert 1).Insert 2).IsEmpty |> should equal false
+
+[<Test>]
+let ``seq enumerate matches build list``() =
+
+    fsCheck "maxBinomialHeap" (Prop.forAll (Arb.fromGen maxBinomialHeapIntGen) 
+        (fun (h, l) -> h |> List.ofSeq = l |> classifyCollect h (h.Length())))
+
+    fsCheck "minBinomialHeap" (Prop.forAll (Arb.fromGen minBinomialHeapIntGen) 
+        (fun (h, l) -> h |> List.ofSeq = l |> classifyCollect h (h.Length())))
+
+[<Test>]
+let ``length of empty is 0``() =
+    (BinomialHeap.empty true).Length() |> should equal 0
+
+[<Test>]
+[<TestCaseSource("intGensStart1")>]
+let ``seq enumerate matches build list int``(x : obj) =
+    let genAndName = unbox x
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun (h : BinomialHeap<int>, l) -> h |> Seq.toList = l |> classifyCollect h (h.Length())))
+
+[<Test>]
+[<TestCaseSource("stringGens")>]
+let ``seq enumerate matches build list string``(x : obj) =
+    let genAndName = unbox x
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun (h : BinomialHeap<string>, l) -> h |> Seq.toList = l |> classifyCollect h (h.Length())))
+
+[<Test>]
+[<TestCaseSource("intGensStart2")>]
+let ``tail should return``(x : obj) =
+    let genAndName = unbox x 
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun ((h : BinomialHeap<int>), (l : int list)) ->    
+                                                                            let tl = h.Tail
+                                                                            let tlHead =
+                                                                                if (tl.Length() > 0) then (tl.Head = l.Item(1))
+                                                                                else true
+                                                                            (tlHead && (tl.Length() = (l.Length - 1)))     
+                                                                            |> classifyCollect h (h.Length())))
+
+[<Test>]
+let ``tryGetHead on empty should return None``() =
+    (BinomialHeap.empty true).TryGetHead |> should equal None
+
+[<Test>]
+[<TestCaseSource("intGensStart2")>]
+let ``tryGetHead should return``(x : obj) =
+    let genAndName = unbox x 
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun ((h : BinomialHeap<int>), (l : int list)) ->    
+                                                                            (h.TryGetHead.Value = l.Head)     
+                                                                            |> classifyCollect h (h.Length())))
+
+[<Test>]
+let ``tryGetTail on empty should return None``() =
+    (BinomialHeap.empty true).TryGetTail |> should equal None
+
+[<Test>]
+let ``tryGetTail on len 1 should return Some empty``() =
+    let h = BinomialHeap.empty true |> insert 1 |> tryGetTail
+    h.Value |> isEmpty |> should equal true
+
+[<Test>]
+let ``tryMerge max and mis should be None``() =
+    let h1 = ofSeq true ["f";"e";"d";"c";"b";"a"]
+    let h2 = ofSeq false ["t";"u";"v";"w";"x";"y";"z"]
+
+    tryMerge h1 h2 |> should equal None
+
+[<Test>]
+[<TestCaseSource("intGensStart2")>]
+let ``tryUncons 1 element``(x : obj) =
+    let genAndName = unbox x 
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun ((h : BinomialHeap<int>), (l : int list)) ->    
+                                                                            let x, tl = h.TryUncons.Value
+                                                                            ((x = l.Head) && (tl.Length() = (l.Length - 1)))     
+                                                                            |> classifyCollect h (h.Length())))
+
+[<Test>]
+let ``tryUncons empty``() =
+    (BinomialHeap.empty true).TryUncons |> should equal None
+
+[<Test>]
+[<TestCaseSource("intGensStart2")>]
+let ``uncons 1 element``(x : obj) =
+    let genAndName = unbox x 
+    fsCheck (snd genAndName) (Prop.forAll (Arb.fromGen (fst genAndName)) (fun ((h : BinomialHeap<int>), (l : int list)) ->    
+                                                                            let x, tl = h.Uncons
+                                                                            ((x = l.Head) && (tl.Length() = (l.Length - 1)))     
+                                                                            |> classifyCollect h (h.Length())))
