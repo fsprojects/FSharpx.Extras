063af0e78bbe6c2b9821b9f9de28f111ae7c5861
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-11-08 06:17:35 +0100

    Adding the RingBuffer from https://bitbucket.org/colinbul/fsharpent

---
+++ src/FSharpx.Core/DataStructures/RingBuffer.fs
@@ -0,0 +1,64 @@
+﻿/// Originally from https://bitbucket.org/colinbul/fsharpent
+namespace FSharpx.DataStructures
+
+open System
+open System.Collections
+open System.Collections.Generic
+open FSharpx
+
+type RingBuffer<'a>(position : int, vals : seq<'a>) = 
+    
+    let buffer = vals |> Seq.toArray
+    let size = buffer.Length
+    let position = ref position
+    let next curr = 
+        (curr + 1) % size
+
+    new(size : int) = 
+        RingBuffer(0, Array.zeroCreate size)
+
+    new(vals : seq<'a>) = 
+        RingBuffer(0, vals)
+
+    member internal x.MoveNext() = 
+        position := next !position
+
+    member x.Length 
+        with get() = size
+
+    member x.Position 
+        with get() = !position
+
+    member x.Values = 
+         [|
+            let pos = !position
+            let count = ref -1
+            while !count < (size - 1) do
+                yield buffer.[next (pos + !count)]
+                incr(count)
+         |]
+
+    member x.Insert(offset : int, vals : seq<'a>) =
+         let values = Seq.toArray vals
+         if values.Length > 0 && offset < size
+         then
+            let count = ref -1
+            let pos = !position + offset
+            let maxItems = min values.Length (size - offset)
+            for v in values.[0..maxItems-1] do
+                buffer.[next (pos + !count)] <- values.[!count + 1]
+                incr count
+    
+    member x.Advance(offset : int) = 
+        for indx in [1..offset] do
+            buffer.[!position] <- Unchecked.defaultof<'a>
+            x.MoveNext()
+
+    member x.Normalize() = 
+        buffer.[0..size - 1] <- x.Values 
+        position := 0
+
+    member x.Clone() = RingBuffer<'a>(x.Position, x.Values)
+
+module RingBuffer =
+    let create (seq: 'a seq) = new RingBuffer<'a>(seq)
---
+++ tests/FSharpx.DataStructures.Tests/RingBufferTest.fs
@@ -0,0 +1,67 @@
+﻿module FSharpx.DataStructures.Tests.RingBufferTest
+
+open System
+open FSharpx.DataStructures
+open FSharpx.DataStructures.RingBuffer
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``empty RingBuffer should not contain any elements``() =
+    let rb = create []
+    rb.Length |> should equal 0
+    rb.Values |> should equal []
+
+[<Test>]
+let ``Creating a RingBuffer with a seq should init the RingBuffer``() =
+    let rb = create [1..100]
+    rb.Length |> should equal 100
+    rb.Values |> should equal [1..100]
+
+[<Test>]
+let ``Creating a ring buffer with a given size``() = 
+    let actual = new RingBuffer<int>(10)
+    let expected = Array.init 10 (fun _ -> 0)
+    actual.Values |> should equal expected
+
+[<Test>]
+let ``Creating a ring buffer with a given set of values``() = 
+    let actual = new RingBuffer<int>([|1;2;3;4;5;6;7;8;9;10|])
+    let expected = Array.init 10 (fun i -> i + 1)
+    actual.Values |> should equal expected
+
+[<Test>]
+let ``Inserting values at 0 offset should prepend them``() = 
+    let buffer = new RingBuffer<int>(10)
+    buffer.Insert(0, [|1;2;3;4;5|])
+    buffer.Values |> should equal [|1;2;3;4;5;0;0;0;0;0|]
+
+[<Test>]
+let ``When inserting more values than the size of the buffer it should take only the first``() =
+    let buffer = new RingBuffer<int>(10)
+    buffer.Insert(0, [|1;2;3;4;5;6;7;8;9;10;11|])
+    buffer.Values |> should equal [|1;2;3;4;5;6;7;8;9;10|]
+
+[<Test>]
+let ``Insert should not fail if an empty sequence is inserted``() = 
+    let actual = new RingBuffer<int>(10)
+    actual.Insert(0, [||])
+    let expected = Array.init 10 (fun _ -> 0)
+    actual.Values |> should equal expected
+
+[<Test>]
+let ``Advancing past the end of the buffer should cause it to wrap``() =
+    let buffer = new RingBuffer<int>([|1;2;3;4;5;6;7;8;9;10|])
+    buffer.Advance(11)
+    buffer.Values |> should equal [|0;0;0;0;0;0;0;0;0;0|] 
+    buffer.Position |> should equal 1      
+
+[<Test>]
+let ``Cloning a ring buffer should give a new copy``() = 
+    let buffer = new RingBuffer<int>([|1;2;3;4;5;6;7;8;9;10|])
+    let clone = buffer.Clone()
+    buffer.Values |> should equal clone.Values
+    clone.Advance(2)
+    Assert.AreNotEqual(buffer.Values,clone.Values)
+    buffer.Position |> should equal 0
+    clone.Position |> should equal 2
