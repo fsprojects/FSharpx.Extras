56f57cc8a1d45c50f61eb6958b287a9bcbe793df
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-11-22 17:24:58 +0100

    Editdistance test created

--- tests/FSharpx.DataStructures.Tests/BKTreeTest.fs
+++ tests/FSharpx.DataStructures.Tests/BKTreeTest.fs
@@ -4,21 +4,6 @@ open FSharpx
 open FSharpx.DataStructures
 open NUnit.Framework
 open FsUnit
-
-type BS = ByteString
-
-[<Test>]
-let ``distance example``() =
-    BKTree.ByteString.distance (BS"kitten"B) (BS"sitting"B)
-    |> should equal 3
-
-[<Test>]
-let ``toListDistance example``() =
-    [BS"kitten"B; BS"setting"B; BS"getting"B]
-    |> BKTree.ByteString.ofList
-    |> BKTree.ByteString.toListDistance 2 (BS"sitting"B)
-    |> should equal [BS"setting"B; BS"getting"B]
-
 open FsCheck
 open FsCheck.NUnit
 
---
+++ tests/FSharpx.DataStructures.Tests/EditDistanceTest.fs
@@ -0,0 +1,20 @@
+﻿module FSharpx.DataStructures.Tests.EditDistanceTest
+
+open FSharpx
+open FSharpx.DataStructures
+open NUnit.Framework
+open FsUnit
+
+type BS = ByteString
+
+[<Test>]
+let ``distance example``() =
+    BKTree.ByteString.distance (BS "kitten"B) (BS "sitting"B)
+    |> should equal 3
+
+[<Test>]
+let ``toListDistance example``() =
+    [BS "kitten"B; BS "setting"B; BS "getting"B]
+    |> BKTree.ByteString.ofList
+    |> BKTree.ByteString.toListDistance 2 (BS "sitting"B)
+    |> should equal [BS "setting"B; BS "getting"B]
