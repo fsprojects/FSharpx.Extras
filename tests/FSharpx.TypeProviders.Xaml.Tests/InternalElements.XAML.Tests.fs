d31ecfe0e0240bcfeedc6051ed434900323342a9
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-11-26 10:35:35 +0100

    Use internal (custom) type in Xaml type provider test - relates to #159

---
+++ tests/FSharpx.TypeProviders.Xaml.Tests/InternalElements.XAML.Tests.fs
@@ -0,0 +1,24 @@
+﻿namespace FSharpx.TypeProviders.InternalElements
+
+type MyButton() =
+   inherit System.Windows.Controls.Button()
+
+open NUnit.Framework
+open FSharpx
+open FsUnit
+
+type Inlined = 
+    XAML<Schema =
+        """<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+                    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+                    xmlns:internalE="clr-namespace:FSharpx.TypeProviders.InternalElements">
+                <Grid>
+                    <internalE:MyButton x:Name="CustomComponent">This explodes</internalE:MyButton>
+                </Grid>
+            </Window>""">
+
+module Tests =
+    [<Test>][<RequiresSTA>]
+    let ``It should parse xaml with internal elements``() =
+       let window = Inlined()
+       window.CustomComponent |> should equal "MainWindow"
