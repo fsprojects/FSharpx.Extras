a2636596562f72b4ed516e218ff9711319dd4b7d
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-11-30 20:53:47 +0100

    Explicitly building for .net 3.5 and .net 4.0 now

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export MONO_IOMAP=all
-exec mono $MONO_OPTIONS packages/FAKE.1.64.18.0/tools/FAKE.exe build.fsx
+exec mono $MONO_OPTIONS packages/FAKE.1.64.18.0/tools/FAKE.exe build.fsx v35 v40
