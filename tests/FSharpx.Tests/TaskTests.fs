2bfa8e90566b115181f20a86b5472a63abf45db4
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-11-30 14:36:55 +0100

    Testing the taskbuilder with let! and return!

--- tests/FSharpx.Tests/TaskTests.fs
+++ tests/FSharpx.Tests/TaskTests.fs
@@ -52,19 +52,32 @@ let loadprices() =
     Assert.True !started
 
 [<Test>]
-let ``waiting for a task``() =
-    use cts = new CancellationTokenSource()
-    let task = Task.TaskBuilder(cancellationToken = cts.Token)
+let ``task should return the right value after let!``() =
+    let task = Task.TaskBuilder()
     let t() = 
         task {
-            let! v = Task.Factory.StartNew(fun () -> 0)
-            return ()
+            let! v = Task.Factory.StartNew(fun () -> 100)
+            return v
         }
-    cts.Cancel()
+
     match Task.run t with
-    | Task.Canceled -> ()
-    | Task.Error e -> Assert.Fail("Task should have been canceled, but errored with exception {0}", e)
-    | Task.Successful a -> Assert.Fail("Task should have been canceled, but succeeded with result {0}", a)
+    | Task.Canceled -> Assert.Fail("Task should have been successful, but was canceled")
+    | Task.Error e -> Assert.Fail("Task should have been successful, but errored with exception {0}", e)
+    | Task.Successful a -> Assert.AreEqual(100,a)
+
+
+[<Test>]
+let ``task should return the right value after return!``() =
+    let task = Task.TaskBuilder()
+    let t() = 
+        task {
+            return! Task.Factory.StartNew(fun () -> "hello world")
+        }
+
+    match Task.run t with
+    | Task.Canceled -> Assert.Fail("Task should have been successful, but was canceled")
+    | Task.Error e -> Assert.Fail("Task should have been successful, but errored with exception {0}", e)
+    | Task.Successful a -> Assert.AreEqual("hello world",a)
 
 
 [<Test>]
