184ccaefe806a0e563eba80482495edd9e3c9d8e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-12-05 20:21:06 +0100

    Refactor the zipper for easier map usage  - references issue #177

--- src/FSharpx.Core/JSON.Zipper.fs
+++ src/FSharpx.Core/JSON.Zipper.fs
@@ -2,44 +2,73 @@
 
 /// A zipper for JsonValues
 /// inspired by https://gist.github.com/868042 
-type JsonZipper = { Focus : JsonValue; Parent : JsonZipper option; Lefts : JsonValue list; Rights : JsonValue list } 
+
+type JsonZipper = 
+| ListZipper of JsonValue * JsonZipper option * JsonValue list * JsonValue list
+| MapZipper of JsonValue * JsonZipper option * string * Map<string,JsonValue>
 
 /// Returns the JsonValue under focus
-let focus zipper = zipper.Focus     
+let focus zipper = 
+    match zipper with
+    | ListZipper(f,_,_,_) -> f
+    | MapZipper(f,_,_,_) -> f
+
+let parent zipper = 
+    match zipper with
+    | ListZipper(_,p,_,_) -> p
+    | MapZipper(_,p,_,_) -> p
 
 /// Changes the element under the focus
-let modifyText newValue zipper = { zipper with Focus = JsonValue.String newValue } 
+let modifyText newValue zipper = 
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.String newValue,p,ls,rs)
+    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.String newValue,p,n,m)
 
 /// Changes the element under the focus
-let modifyDecimal newValue zipper = { zipper with Focus = JsonValue.NumDecimal newValue } 
+let modifyDecimal newValue zipper = 
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.NumDecimal newValue,p,ls,rs)
+    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.NumDecimal newValue,p,n,m)
 
 /// Changes the element under the focus
-let modifyBool newValue zipper = { zipper with Focus = JsonValue.Bool newValue } 
+let modifyBool newValue zipper = 
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.Bool newValue,p,ls,rs)
+    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.Bool newValue,p,n,m)
 
 
 let left zipper = 
-    match zipper.Lefts with
-    | x::xs -> { Focus = x ; Lefts = xs; Parent = zipper.Parent; Rights = zipper.Focus::zipper.Rights }
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> 
+        match ls with
+        | x::xs -> ListZipper(x,p,xs,f::rs)    
 
 let right zipper = 
-    match zipper.Rights with
-    | x::xs -> { Focus = x ; Lefts = zipper.Focus::zipper.Lefts; Parent = zipper.Parent; Rights = xs }
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> 
+        match rs with
+        | x::xs -> ListZipper(x,p,f::ls,xs)
 
 let down zipper =
-    match zipper.Focus with
-    | JsonValue.Obj map ->
-        match [for kv in map -> kv.Value] with
-        | x::xs -> { Parent = Some zipper; Lefts = [] ; Focus = x; Rights = xs}
-    | JsonValue.Array (x::xs) -> { Parent = Some zipper; Lefts = [] ; Focus = x; Rights = xs}
+    match zipper with
+    | ListZipper(f,p,ls,rs) -> 
+        match f with
+        | JsonValue.Obj map ->
+            let e = Seq.head map
+            MapZipper(e.Value,Some zipper,e.Key,Map.remove e.Key map)
+        | JsonValue.Array (x::xs) -> ListZipper(x,Some zipper,[],xs)
+    | MapZipper(f,p,n,m) -> 
+        match f with
+        | JsonValue.Obj map ->
+            let e = Seq.head map
+            MapZipper(e.Value,Some zipper,e.Key,Map.remove e.Key map)
+        | JsonValue.Array (x::xs) -> ListZipper(x,Some zipper,[],xs)
 
 let downToProperty name zipper =
-    match zipper.Focus with
-    | JsonValue.Obj map ->
-        let rec loop rest position =
-            match rest with
-            | x::xs -> if x = name then position else loop xs (right position)
-        
-        zipper |> down |> loop [for kv in map -> kv.Key]        
+    match zipper with
+    | MapZipper(f,p,n,m) -> 
+        match f with
+        | JsonValue.Obj map ->  MapZipper(Map.find name map,Some zipper,name,Map.remove name map)
 
 let replaceChildren jsonValue children =
     match jsonValue with
@@ -51,14 +80,21 @@ let replaceChildren jsonValue children =
 
 /// Moves the zipper upwards
 let up zipper =
-    match zipper.Parent with
-    | Some parent ->  { parent with Focus = replaceChildren parent.Focus ((List.rev zipper.Lefts) @ zipper.Focus :: zipper.Rights) }
+    match zipper with
+    | ListZipper(f,p,ls,rs) ->        
+        match p with
+        | Some (ListZipper(p_f,p_p,p_ls,p_rs)) -> ListZipper(replaceChildren p_f ((List.rev ls) @ f :: rs),p_p,p_ls,p_rs)
+        | Some (MapZipper(p_f,p_p,p_n,p_m)) -> MapZipper(replaceChildren p_f ((List.rev ls) @ f :: rs),p_p,p_n,p_m)
+    | MapZipper(f,p,n,m) -> 
+        match p with
+        | Some (ListZipper(p_f,p_p,p_ls,p_rs)) -> ListZipper(JsonValue.Obj(Map.add n f m),p_p,p_ls,p_rs)
+        | Some (MapZipper(p_f,p_p,p_n,p_m)) -> MapZipper(JsonValue.Obj(Map.add n f m),p_p,p_n,p_m)
 
 /// Creates a Json zipper
-let zipper jsonValue =  { Focus = jsonValue; Parent = None; Lefts = []; Rights = []}
+let zipper jsonValue =  ListZipper(jsonValue,None,[],[])
 
 /// Returns the whole Json document from the zipper
 let rec getJson zipper = 
-    match zipper.Parent with
-    | None -> zipper.Focus
+    match parent zipper with
+    | None -> focus zipper
     | Some parent -> up zipper |> getJson
