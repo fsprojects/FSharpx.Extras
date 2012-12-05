747d97e39d4e5fa37b132a4ee79aa9b909d142fd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2012-12-05 22:35:32 +0100

    Refactor the zipper for easier usage - references issue #177

--- src/FSharpx.Core/JSON.Zipper.fs
+++ src/FSharpx.Core/JSON.Zipper.fs
@@ -4,14 +4,14 @@
 /// inspired by https://gist.github.com/868042 
 
 type JsonZipper = 
-| ListZipper of JsonValue * JsonZipper option * JsonValue list * JsonValue list
-| MapZipper of JsonValue * JsonZipper option * string * Map<string,JsonValue>
+| ListZipper of JsonValue option * JsonZipper option * JsonValue list * JsonValue list
+| MapZipper of JsonValue option * JsonZipper option * string * Map<string,JsonValue>
 
 /// Returns the JsonValue under focus
 let inline focus zipper = 
     match zipper with
-    | ListZipper(f,_,_,_) -> f
-    | MapZipper(f,_,_,_) -> f
+    | ListZipper(Some f,_,_,_) -> f
+    | MapZipper(Some f,_,_,_) -> f
 
 /// Returns the parent of the current zipper
 let inline parent zipper = 
@@ -20,77 +20,103 @@ let inline parent zipper =
     | MapZipper(_,p,_,_) -> p
 
 /// Changes the element under the focus
-let modifyText newValue zipper = 
+let inline update jsonValue zipper = 
     match zipper with
-    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.String newValue,p,ls,rs)
-    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.String newValue,p,n,m)
+    | ListZipper(Some f,p,ls,rs) -> ListZipper(Some jsonValue,p,ls,rs)
+    | MapZipper(Some f,p,n,m) -> MapZipper(Some jsonValue,p,n,m)
 
-/// Changes the element under the focus
-let modifyDecimal newValue zipper = 
+/// Inserts an element at the current position
+let insert element zipper =
     match zipper with
-    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.NumDecimal newValue,p,ls,rs)
-    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.NumDecimal newValue,p,n,m)
+    | ListZipper(None,p,ls,rs) -> ListZipper(Some element,p,ls,rs)
+    | ListZipper(Some f,p,ls,rs) -> ListZipper(Some element,p,ls,f::rs)
 
-/// Changes the element under the focus
-let modifyBool newValue zipper = 
+/// Inserts a property with the given name and value into the current focus
+let addProperty name element zipper =
     match zipper with
-    | ListZipper(f,p,ls,rs) -> ListZipper(JsonValue.Bool newValue,p,ls,rs)
-    | MapZipper(f,p,n,m) -> MapZipper(JsonValue.Bool newValue,p,n,m)
+    | MapZipper(None,p,n,m) -> MapZipper(Some element,p,name,m)
+    | MapZipper(Some f,p,n,m) -> MapZipper(Some element,p,name,Map.add n f m)
 
 /// Moves the zipper to the left
 let left zipper = 
     match zipper with
-    | ListZipper(f,p,ls,rs) -> 
+    | ListZipper(Some f,p,ls,rs) -> 
         match ls with
-        | x::xs -> ListZipper(x,p,xs,f::rs)    
+        | x::xs -> ListZipper(Some x,p,xs,f::rs)    
 
 /// Moves the zipper to the right
 let right zipper = 
     match zipper with
-    | ListZipper(f,p,ls,rs) -> 
+    | ListZipper(Some f,p,ls,rs) -> 
         match rs with
-        | x::xs -> ListZipper(x,p,f::ls,xs)
+        | x::xs -> ListZipper(Some x,p,f::ls,xs)
+        | _ -> ListZipper(None,p,f::ls,[])
 
 /// Moves the zipper down
 let down zipper =
     match focus zipper with
     | JsonValue.Obj map ->
+        if Map.isEmpty map then MapZipper(None,Some zipper,"",map) else
         let e = Seq.head map
-        MapZipper(e.Value,Some zipper,e.Key,Map.remove e.Key map)
-    | JsonValue.Array (x::xs) -> ListZipper(x,Some zipper,[],xs)    
+        MapZipper(Some e.Value,Some zipper,e.Key,Map.remove e.Key map)
+    | JsonValue.Array (x::xs) -> ListZipper(Some x,Some zipper,[],xs)    
 
 /// Moves the zipper to the property with the given name on the same level
 let toProperty name zipper =
     match zipper with
-    | MapZipper(f,p,n,m) ->
+    | MapZipper(Some f,p,n,m) ->
         let map = Map.add n f m
-        MapZipper(Map.find name map,p,name,Map.remove name map)
+        MapZipper(Map.tryFind name map,p,name,Map.remove name map)
+    | MapZipper(None,p,n,m) ->
+        MapZipper(Map.tryFind name m,p,name,Map.remove name m)
 
-let replaceChildren jsonValue children =
-    match jsonValue with
-    | JsonValue.Array _ -> JsonValue.Array children
-    | JsonValue.Obj map ->
-        let keys = [ for kv in map -> kv.Key ]
-        let zipped = List.zip keys children
-        JsonValue.Obj (Map.ofList zipped)
+/// Removes the element or property from the current level
+let remove zipper =
+    match zipper with
+    | MapZipper(_,p,n,m) ->  
+        if m = Map.empty then MapZipper(None,p,"",m) else
+        let kv = Seq.head m
+        MapZipper(Some kv.Value,p,kv.Key,Map.remove kv.Key m)
 
 /// Moves the zipper upwards
 let up zipper =
     match zipper with
-    | ListZipper(f,p,ls,rs) ->
+    | ListZipper(Some f,p,ls,rs) ->
         match p with
-        | Some (ListZipper(p_f,p_p,p_ls,p_rs)) -> ListZipper(replaceChildren p_f ((List.rev ls) @ f :: rs),p_p,p_ls,p_rs)
-        | Some (MapZipper(p_f,p_p,p_n,p_m)) -> MapZipper(replaceChildren p_f ((List.rev ls) @ f :: rs),p_p,p_n,p_m)
-    | MapZipper(f,p,n,m) -> 
+        | Some (ListZipper(_,p_p,p_ls,p_rs)) -> ListZipper(Some(JsonValue.Array ((List.rev ls) @ f :: rs)),p_p,p_ls,p_rs)
+        | Some (MapZipper(Some p_f,p_p,p_n,p_m)) -> MapZipper(Some(JsonValue.Array((List.rev ls) @ f :: rs)),p_p,p_n,p_m)
+    | ListZipper(None,p,ls,rs) ->
         match p with
-        | Some (ListZipper(p_f,p_p,p_ls,p_rs)) -> ListZipper(JsonValue.Obj(Map.add n f m),p_p,p_ls,p_rs)
-        | Some (MapZipper(p_f,p_p,p_n,p_m)) -> MapZipper(JsonValue.Obj(Map.add n f m),p_p,p_n,p_m)
+        | Some (ListZipper(_,p_p,p_ls,p_rs)) -> ListZipper(Some(JsonValue.Array ((List.rev ls) @ rs)),p_p,p_ls,p_rs)
+        | Some (MapZipper(Some p_f,p_p,p_n,p_m)) -> MapZipper(Some(JsonValue.Array((List.rev ls) @ rs)),p_p,p_n,p_m)
+    | MapZipper(Some f,p,n,m) -> 
+        match p with
+        | Some (ListZipper(_,p_p,p_ls,p_rs)) -> ListZipper(Some(JsonValue.Obj(Map.add n f m)),p_p,p_ls,p_rs)
+        | Some (MapZipper(Some p_f,p_p,p_n,p_m)) -> MapZipper(Some(JsonValue.Obj(Map.add n f m)),p_p,p_n,p_m)
+    | MapZipper(None,p,n,m) -> 
+        match p with
+        | Some (ListZipper(_,p_p,p_ls,p_rs)) -> ListZipper(Some(JsonValue.Obj m),p_p,p_ls,p_rs)
+        | Some (MapZipper(Some p_f,p_p,p_n,p_m)) -> MapZipper(Some(JsonValue.Obj m),p_p,p_n,p_m)
 
 /// Creates a Json zipper
-let zipper jsonValue =  ListZipper(jsonValue,None,[],[])
+let toZipper jsonValue =  
+    match jsonValue with
+    | JsonValue.Array a -> 
+        match a with
+        | [] -> ListZipper(None,None,[],[])
+        | x::xs -> ListZipper(Some x,None,[],xs)
+    | JsonValue.Obj map -> 
+        if Map.isEmpty map then MapZipper(None,None,"",map) else
+        let e = Seq.head map
+        MapZipper(Some e.Value,None,e.Key,Map.remove e.Key map)
 
 /// Returns the whole Json document from the zipper
-let rec getJson zipper = 
+let rec fromZipper zipper = 
     match parent zipper with
-    | None -> focus zipper
-    | Some parent -> up zipper |> getJson
+    | None ->         
+        match zipper with
+        | MapZipper(Some f,p,n,m) -> JsonValue.Obj (Map.add n f m)
+        | MapZipper(None,p,n,m) -> JsonValue.Obj m
+        | ListZipper(Some f,_,ls,rs) -> JsonValue.Array((List.rev ls) @ f :: rs)
+        | ListZipper(None,_,_,_) -> JsonValue.Array []
+    | Some parent -> up zipper |> fromZipper
--- tests/FSharpx.Tests/Json.Zipper.Tests.fs
+++ tests/FSharpx.Tests/Json.Zipper.Tests.fs
@@ -6,80 +6,176 @@ open FSharpx.JSON.Zipper
 open FsUnit
 
 [<Test>]
+let ``Can create zipper from an empty document``() = 
+    parse "{ }"
+    |> toZipper 
+    |> fromZipper
+    |> serialize 
+    |> should equal "{}"
+
+[<Test>]
+let ``Can create zipper from an empty arry``() = 
+    parse "[]"
+    |> toZipper 
+    |> fromZipper
+    |> serialize 
+    |> should equal "[]"
+
+[<Test>]
+let ``Can create zipper from a simple document``() = 
+    parse "{\"id\": \"Open\"}"
+    |> toZipper 
+    |> fromZipper
+    |> serialize 
+    |> should equal "{\"id\":\"Open\"}"
+
+[<Test>]
+let ``Can create zipper from a simple array``() = 
+    parse "[\"Test\",3]"
+    |> toZipper 
+    |> fromZipper
+    |> serialize 
+    |> should equal "[\"Test\",3]"
+
+[<Test>]
 let ``Can modify the first property in a simple document``() = 
     parse "{\"age\":25,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
-    |> zipper 
-    |> down
-    |> modifyDecimal (decimal 26)
-    |> getJson
+    |> toZipper 
+    |> update (JsonValue.NumDecimal (decimal 26))
+    |> fromZipper
     |> serialize 
     |> should equal "{\"age\":26,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
 
 [<Test>]
 let ``Can modify a text property in a simple document``() = 
     parse "{\"age\":25,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
-    |> zipper 
-    |> down
+    |> toZipper 
     |> toProperty "firstName"
-    |> modifyText "Johnny"
-    |> getJson
+    |> update (JsonValue.String "Johnny")
+    |> fromZipper
     |> serialize 
     |> should equal "{\"age\":25,\"firstName\":\"Johnny\",\"lastName\":\"Smith\"}"
 
 [<Test>]
 let ``Can modify a bool property in a simple document``() = 
     parse "{\"age\":25,\"firstName\":\"John\",\"lastName\":\"Smith\",\"isCool\":true}"
-    |> zipper 
-    |> down
+    |> toZipper 
     |> toProperty "isCool"
-    |> modifyBool false
-    |> getJson
+    |> update (JsonValue.Bool false)
+    |> fromZipper
     |> serialize 
     |> should equal "{\"age\":25,\"firstName\":\"John\",\"isCool\":false,\"lastName\":\"Smith\"}"
 
 [<Test>]
 let ``Can modify a decimal property in a simple document``() = 
     parse "{\"age\":25,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
-    |> zipper 
-    |> down
+    |> toZipper 
     |> toProperty "age"
-    |> modifyDecimal (decimal 26)
-    |> getJson
+    |> update (JsonValue.NumDecimal (decimal 26))
+    |> fromZipper
     |> serialize 
     |> should equal "{\"age\":26,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
 
 [<Test>]
 let ``Can modify a couple of properties in a simple document``() = 
     parse "{\"age\":25,\"firstName\":\"John\",\"lastName\":\"Smith\"}"
-    |> zipper 
-    |> down
+    |> toZipper 
     |> toProperty "age"
-    |> modifyDecimal (decimal 26)
+    |> update (JsonValue.NumDecimal (decimal 26))
     |> toProperty "lastName"
-    |> modifyText "Forkmann"
-    |> getJson
+    |> update (JsonValue.String "Forkmann")
+    |> fromZipper
     |> serialize 
     |> should equal "{\"age\":26,\"firstName\":\"John\",\"lastName\":\"Forkmann\"}"
 
 [<Test>]
 let ``Can modify an array element a simple document``() = 
     parse "[1, 2, 3]"
-    |> zipper 
-    |> down
+    |> toZipper 
     |> right
-    |> modifyDecimal (decimal 26)
-    |> getJson
+    |> update (JsonValue.NumDecimal (decimal 26))
+    |> fromZipper
     |> serialize 
     |> should equal "[1,26,3]"
 
 [<Test>]
+let ``Can insert an element to an array at the front``() = 
+    parse "[1, 2, 3]"
+    |> toZipper 
+    |> insert (JsonValue.String "blub")
+    |> fromZipper
+    |> serialize 
+    |> should equal "[\"blub\",1,2,3]"
+
+[<Test>]
+let ``Can insert an element to an array in the middle``() = 
+    parse "[1, 2, 3]"
+    |> toZipper 
+    |> right
+    |> right
+    |> insert (JsonValue.String "bla")
+    |> fromZipper
+    |> serialize 
+    |> should equal "[1,2,\"bla\",3]"
+
+[<Test>]
 let ``Can modify an array element a nested array``() = 
     parse "[ [\"Adam\", \"Eve\"], [\"Bonnie\", \"Clyde\"], [\"Donald\", \"Daisy\"], [\"Han\", \"Leia\"] ]" 
-    |> zipper 
-    |> down
+    |> toZipper 
     |> right
     |> down
-    |> modifyText "Steffen"
-    |> getJson
+    |> update (JsonValue.String "Steffen")
+    |> fromZipper
     |> serialize 
     |> should equal "[[\"Adam\",\"Eve\"],[\"Steffen\",\"Clyde\"],[\"Donald\",\"Daisy\"],[\"Han\",\"Leia\"]]" 
+ 
+[<Test>]
+let ``Can modify a property inside a nested document``() = 
+    parse "[{\"id\": \"Open\"}, null, {\"id\": \"Pause\"}]"
+    |> toZipper 
+    |> right
+    |> right
+    |> down
+    |> toProperty "id"
+    |> update (JsonValue.String "Stopped")
+    |> fromZipper
+    |> serialize 
+    |> should equal "[{\"id\":\"Open\"},null,{\"id\":\"Stopped\"}]"
+
+[<Test>]
+let ``Can create a new object in a nested document``() = 
+    parse "[{\"id\": \"Open\"}, null, {\"id\": \"Pause\"}]"
+    |> toZipper 
+    |> right
+    |> update (JsonValue.Obj(Map.add "id" (JsonValue.String "Tataa") Map.empty))
+    |> fromZipper
+    |> serialize 
+    |> should equal "[{\"id\":\"Open\"},{\"id\":\"Tataa\"},{\"id\":\"Pause\"}]"
+
+[<Test>]
+let ``Can create a new property in a simple document``() = 
+    parse "{\"id\": \"Open\"}"
+    |> toZipper 
+    |> addProperty "id2" (JsonValue.String "Tataa")
+    |> fromZipper
+    |> serialize 
+    |> should equal "{\"id\":\"Open\",\"id2\":\"Tataa\"}"
+
+[<Test>]
+let ``Can remove a property in a simple document``() = 
+    parse "{\"id\": \"Open\",\"id2\":\"Tataa\",\"id3\":\"Tataa\"}"
+    |> toZipper 
+    |> toProperty "id2"
+    |> remove
+    |> fromZipper
+    |> serialize 
+    |> should equal "{\"id\":\"Open\",\"id3\":\"Tataa\"}"
+
+[<Test>]
+let ``Can remove all properties in a simple document``() = 
+    parse "{\"id\": \"Open\"}"
+    |> toZipper
+    |> remove
+    |> fromZipper
+    |> serialize 
+    |> should equal "{}"
