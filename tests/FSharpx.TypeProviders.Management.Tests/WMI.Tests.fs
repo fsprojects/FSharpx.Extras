f3681d1343ac900ce3c2a9f08f9be687b5caf278
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-17 14:51:57 +0100

    Porting the WMI type provider from then F# Sample pack - Adding Test project

---
+++ tests/FSharpx.TypeProviders.Management.Tests/WMI.Tests.fs
@@ -0,0 +1,10 @@
+﻿module FSharpx.TypeProviders.Management.Tests
+
+open System
+open System.Management
+open FSharpx.TypeProviders.Management
+
+type Local = WmiProvider<"localhost">
+let data = Local.GetDataContext()
+
+[ for b in data.Win32_DiskDrive -> b.Name, b.Description]
