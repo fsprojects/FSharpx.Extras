e9115297e96ab7115558b46ab4f2870499435538
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-01-17 15:15:49 +0100

    Adding WMI provider sample which shows a german property

--- tests/FSharpx.TypeProviders.Management.Tests/WMI.Tests.fs
+++ tests/FSharpx.TypeProviders.Management.Tests/WMI.Tests.fs
@@ -7,4 +7,11 @@ open FSharpx.TypeProviders.Management
 type Local = WmiProvider<"localhost">
 let data = Local.GetDataContext()
 
+// Add a handler to watch WMI queries getting executed
+data.QueryExecuted.Add(printfn "Query executed: %s")
+
 [ for b in data.Win32_DiskDrive -> b.Name, b.Description]
+
+//// Access some WMI data from the data connection
+//[for dd in data.CIM_DiskDrive -> 
+//        [for c in dd.Capabilities -> c.``Is_SMART-Meldung``]]
