642ca0cc055aec37e46344d2b88934cef7e2233e
Carsten König <Carsten@gettingsharper.de>
2013-06-04 06:57:56 +0200

    added test for AwaitObservable

---
+++ FSharpx.Core.Tests/AwaitObservableTests.fs
@@ -0,0 +1,86 @@
+﻿namespace FSharpx.Core.Tests
+
+open System
+open System.Threading
+open System.Threading.Tasks
+
+open FSharp.Control.Observable
+
+open FsUnit
+open NUnit.Framework
+
+[<TestFixture>]
+type ``AwaitObservable Tests``() = 
+
+    [<Test>]
+    member test.``AwaitObservable yields a value from the sources Next``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Next("DONE")
+        source.Completed()
+        let result = awaiter(TimeSpan.FromMilliseconds(100.0))
+        result |> should equal (Result "DONE")
+
+    [<Test>]
+    member test.``AwaitObservable yields the first value from the sources Next``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Next("ONE")
+        source.Next("TWO")
+        source.Completed()
+        let result = awaiter(TimeSpan.FromMilliseconds(100.0))
+        result |> should equal (Result "ONE")
+
+    [<Test>]
+    member test.``AwaitObservable is canceled if the source completes without a single result``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Completed()
+        let result = awaiter(TimeSpan.FromMilliseconds(100.0))
+        result |> should equal AwaiterResult<string>.Canceled    
+        
+    [<Test>]
+    member test.``AwaitObservable is unsubscribed from the source after a value was received``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Next("Done")
+        source.AssertUnsubscribe(TimeSpan.FromMilliseconds(100.0))
+
+    [<Test>]
+    member test.``AwaitObservable is unsubscribed from the source after the source completes without a result``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Completed()
+        source.AssertUnsubscribe(TimeSpan.FromMilliseconds(100.0))
+
+    [<Test>]
+    member test.``AwaitObservable is unsubscribed from the source after OnError was called``() =
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiter wf
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        source.Error(exn "test-error")
+        source.AssertUnsubscribe(TimeSpan.FromMilliseconds(100.0))
+
+    [<Test>]
+    member test.``AwaitObservable is unsubscribed from the source if it's resulting async-workflow gets cancelled``() =
+        let cts = new CancellationTokenSource()
+        let source = new ObservableMock<string>()
+        let wf = Async.AwaitObservable source
+        let awaiter = startAsAwaiterWithCancellation (wf, Some cts.Token)
+        source.AssertSubscribtion(TimeSpan.FromMilliseconds(100.0))
+        cts.Cancel()
+        let result = awaiter (TimeSpan.FromMilliseconds(100.0)) 
+        result |> should equal AwaiterResult<string>.Canceled
+        source.AssertUnsubscribe(TimeSpan.FromMilliseconds(100.0))
+        
---
+++ FSharpx.Core.Tests/ObservableMock.fs
@@ -0,0 +1,41 @@
+﻿namespace FSharpx.Core.Tests
+
+open System
+open System.Threading
+open NUnit.Framework
+
+type ObservableMock<'a>() =
+
+    let _waitForSubscribtion = new ManualResetEventSlim(initialState = false)
+    let _waitForUnsubscribe = new ManualResetEventSlim(initialState = false)
+    let _connectedObserver : 'a IObserver option ref = ref None
+
+    let callObserver action =
+        match !_connectedObserver with
+        | Some obs -> action obs
+        | None     -> ()
+
+    member this.WaitForSubscribtionHandle = _waitForSubscribtion.WaitHandle
+    member this.WaitForSubscribtion(timeout : TimeSpan) = _waitForSubscribtion.Wait(timeout)
+    member this.AssertSubscribtion(timeout : TimeSpan) =
+        if not <| this.WaitForSubscribtion(timeout) then Assert.Fail("no subscribtion requested")
+
+    member this.WaitForUnsubscribeHandle = _waitForUnsubscribe.WaitHandle
+    member this.WaitForUnsubscribe(timeout : TimeSpan) = _waitForUnsubscribe.Wait(timeout)
+    member this.AssertUnsubscribe(timeout : TimeSpan) =
+        if not <| this.WaitForUnsubscribe(timeout) then Assert.Fail("a observer-subscription was not disposed")
+
+    member this.Next(value : 'a) = callObserver (fun obs -> obs.OnNext(value))
+    member this.Completed() = callObserver (fun obs -> obs.OnCompleted())
+    member this.Error(error : exn) = callObserver (fun obs -> obs.OnError(error))
+
+    interface IObservable<'a> with
+        member i.Subscribe(observer : IObserver<'a>) =
+            _connectedObserver := Some observer
+            _waitForSubscribtion.Set()
+            _waitForUnsubscribe.Reset()
+            { new IDisposable with 
+                member i.Dispose() = 
+                    _connectedObserver := None 
+                    _waitForUnsubscribe.Set()
+            }
