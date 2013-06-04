fc55e6f8c51ffc1f18e8dd7e3a346d5dc292c747
rsdpisuy <rsdpisuy@gmail.com>
2013-06-04 10:50:15 +0400

    AssemblyInfo.fs is added.

---
+++ src/FSharpx.Text.StructuredFormat/AssemblyInfo.fs
@@ -0,0 +1,35 @@
+﻿module FSharpx.Text.StructuredFormat.AssemblyInfo
+#nowarn "49" // uppercase argument names
+#nowarn "67" // this type test or downcast will always hold
+#nowarn "66" // tis upast is unnecessary - the types are identical
+#nowarn "58" // possible incorrect indentation..
+#nowarn "57" // do not use create_DelegateEvent
+#nowarn "51" // address-of operator can occur in the code
+open System
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+exception ReturnException183c26a427ae489c8fd92ec21a0c9a59 of obj
+exception ReturnNoneException183c26a427ae489c8fd92ec21a0c9a59
+
+[<assembly: ComVisible (false)>]
+
+[<assembly: CLSCompliant (false)>]
+
+[<assembly: Guid ("60F3BB81-5449-45DD-A217-B6045327680C")>]
+
+[<assembly: AssemblyTitle ("FSharpx.Text.StructuredFormat")>]
+
+[<assembly: AssemblyDescription ("FSharpx is a library for the .NET platform implementing general functional constructs on top of the F# core library. Its main target is F# but it aims to be compatible with all .NET languages wherever possible.
+
+This library provides data structures and functoins for pretty printers.")>]
+
+[<assembly: AssemblyProduct ("FSharpx.Text.StructuredFormat")>]
+
+[<assembly: AssemblyVersion ("1.7.8")>]
+
+[<assembly: AssemblyFileVersion ("1.7.8")>]
+
+[<assembly: AssemblyDelaySign (false)>]
+
+()
