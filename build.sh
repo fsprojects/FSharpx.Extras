b5dc555f5c81248dba264dd1b5cf63f9a66a66ae
Don Syme <donsyme@fastmail.fm>
2013-06-28 00:38:26 +0100

    add sync of mozroots to build on mono

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mozroots --import --sync
 mono "./lib/NuGet/NuGet.exe" "install" "FAKE" "-OutputDirectory" "lib" "-ExcludeVersion" "-Prerelease"
 export EnableNuGetPackageRestore=true
 export NugetKey=6cfcf909-7b5a-417d-a706-d1a1ab9b4b9e
