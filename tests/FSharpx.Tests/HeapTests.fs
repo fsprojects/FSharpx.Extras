24ff2050645f9d183e91832ef1dcd9b00f28e38e
Andrew Browne <brownie@brownie.com.au>
2014-07-12 16:28:51 +1000

    FsCheck tests for Heap
    Using List.sort as a reference for correctness

--- tests/FSharpx.Tests/HeapTests.fs
+++ tests/FSharpx.Tests/HeapTests.fs
@@ -15,3 +15,51 @@ let ``Stack does not overflow when calling tail on a large heap`` () =
         |> Heap.ofSeq false
     
     Heap.tail h |> ignore
+
+open FsCheck
+open FsCheck.NUnit
+
+[<Test>]
+let ``compare to list``() =
+    fsCheck "head" <| 
+        fun (xs : list<int>) ->
+            xs |> List.isEmpty |> not ==>
+                (fun () ->
+                    let listHead = xs |> List.sort |> List.head
+                    let heapHead = xs |> Heap.ofSeq false |> Heap.head
+                    listHead = heapHead)
+
+    fsCheck "tryHead" <| 
+        fun (xs : list<int>) ->
+            let listHead = xs |> List.sort |> Seq.tryHead
+            let heapHead = xs |> Heap.ofSeq false |> Heap.tryHead
+            listHead = heapHead
+
+    fsCheck "tail" <| 
+        fun (xs : list<int>) ->
+            xs |> List.isEmpty |> not ==>
+                (fun () ->
+                    let listTail = xs |> List.sort |> List.tail
+                    let heapTail = xs |> Heap.ofSeq false |> Heap.tail
+                    listTail = (heapTail |> List.ofSeq))
+
+    fsCheck "tryTail" <| 
+        fun (xs : list<int>) ->
+            let heapTail = xs |> Heap.ofSeq false |> Heap.tryTail
+            match xs |> List.length with
+            | 0 ->
+                heapTail = None
+            | _ ->
+                let listTail = xs |> List.sort |> List.tail
+                Some listTail = (heapTail |> Option.map List.ofSeq)
+
+    fsCheck "insert" <| 
+        fun (xs : list<int>) ->
+            let listSorted = xs |> List.sort
+            let heap = xs |> List.fold (flip Heap.insert) (Heap.empty false)
+            listSorted = (heap |> List.ofSeq)
+
+    fsCheck "length" <| 
+        fun (xs : list<int>) ->
+            let heap = xs |> Heap.ofSeq false
+            (xs |> List.length) = (heap |> Heap.length)
