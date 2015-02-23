cd47b7af7f25ffb312de1ded6026b6b0214cad5a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-23 15:06:09 +0100

    Async stuff moved over to http://fsprojects.github.io/FSharpx.Async/

--- src/FSharpx.Core/Agents/Agent.fs
+++
@@ -1,8 +0,0 @@
-﻿// ----------------------------------------------------------------------------
-// F# async extensions (Agent.fs)
-// (c) Tomas Petricek, 2011, Available under Apache 2.0 license.
-// ----------------------------------------------------------------------------
-namespace FSharpx.Control
-
-/// Type alias for F# mailbox processor type
-type Agent<'T> = MailboxProcessor<'T>
