add6b8be6334acca4ccca9328c4bc9efdef5d466
Bryan Edds <bryanedds@gmail.com>
2015-08-29 20:29:09 -0400

    Added documentation comments, new functions, and an operator

--- src/FSharpx.Extras/Prelude.fs
+++ src/FSharpx.Extras/Prelude.fs
@@ -2,30 +2,75 @@
 
 open System
 open System.Globalization
+open System.Diagnostics
 
 [<AutoOpen>]
 module Prelude =
+
+    /// Transforms a function by flipping the order of its arguments.
     let inline flip f a b = f b a
+
+    /// Transforms a function by flipping the order of its arguments.
+    let inline flip3 f a b c = f c a b
+
+    /// Transforms a function by flipping the order of its arguments.
+    let inline flip4 f a b c d = f d a b c
+    
+    /// Transforms an uncurried function to a curried function.
     let inline curry f a b = f(a,b)
+
+    /// Transforms an uncurried function to a curried function.
     let inline uncurry f (a,b) = f a b
+
+    /// Transforms an uncurried function to a triple-curried function.
+    let inline  curry3 f a b c = f (a, b, c)
+
+    /// Swap the elements of a pair.
     let inline swap (a,b) = (b,a)
+
+    /// Given a value, creates a function with one ignored argument which returns the value.
     let inline konst a _ = a
+
+    /// Given a value, creates a function with two ignored arguments which returns the value.
     let inline konst2 a _ _ = a
-  
+
     /// Creates a pair
     let inline tuple2 a b = a,b
+    
     /// Creates a 3-tuple
     let inline tuple3 a b c = a,b,c
+    
     /// Creates a 4-tuple
     let inline tuple4 a b c d = a,b,c,d
+    
     /// Creates a 5-tuple
     let inline tuple5 a b c d e = a,b,c,d,e
+    
     /// Creates a 6-tuple
     let inline tuple6 a b c d e f = a,b,c,d,e,f
 
+    /// Fixed point combinator.
     let rec fix f x = f (fix f) x
+
+    /// Fixed point combinator.
     let rec fix2 f x y = f (fix2 f) x y
+
+    /// Fixed point combinator.
     let rec fix3 f x y z = f (fix3 f) x y z
+
+    /// Fail with an unexpected match failure.
+    /// Useful to intelligently handle missing match cases instead of taking on an inexhaustiveness warning.
+    let failwithumf () =
+        let stackTrace = StackTrace ()
+        let frame = stackTrace.GetFrame 1
+        let meth = frame.GetMethod ()
+        let line = frame.GetFileLineNumber ()
+        let fileName = frame.GetFileName ()
+        failwithf "Unexpected match failure in '%s' on line %i in file %s." meth.Name line fileName
+
+    /// Sequencing operator like Haskell's ($). Has better precedence than (<|) due to the
+    /// first character used in the symbol.
+    let (^) = (<|)
 
     type Boolean with
         static member parse x =
