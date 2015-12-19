05d4dbc7c2b454a45c22903f746692d9811f0975
Ondra Pelech <ondra.pelech@gmail.com>
2015-12-19 17:13:38 +0100

    remove old nuget.exe related code and files

--- build.fsx
+++ build.fsx
@@ -6,21 +6,6 @@ open Fake.Git
 open Fake.ReleaseNotesHelper
 open System.IO
 
-let nugetPath = ".nuget/NuGet.exe"
-let RestorePackage() =
-    !! "./**/packages.config"
-    |> Seq.iter (RestorePackage (fun p -> { p with ToolPath = nugetPath }))
-
-RestorePackage()
-
-// properties
-let currentDate = System.DateTime.UtcNow
-
-let coreSummary = "FSharpx.Extras is a library for the .NET platform implementing general functional constructs on top of the F# core library."
-let projectSummary = "FSharpx.Extras is a library for the .NET platform implementing general functional constructs on top of the F# core library."
-let authors = ["Steffen Forkmann"; "Daniel Mohl"; "Tomas Petricek"; "Ryan Riley"; "Mauricio Scheffer"; "Phil Trelford" ]
-let mail = "ryan.riley@panesofglass.org"
-let homepage = "http://github.com/fsprojects/FSharpx.Extras"
 
 // .NET Frameworks
 let net40 = "v4.0"
@@ -29,14 +14,6 @@ let net40 = "v4.0"
 let buildDir = "./bin"
 let buildDirVer fxVersion = buildDir @@ fxVersion
 let packagesDir = "./packages"
-
-let targetPlatformDir = getTargetPlatformDir "v4.0.30319"
-
-let nugDir = "./nuget"
-let nugetDir package = nugDir @@ package
-let nugetLibDir package = nugetDir package @@ "lib"
-
-let packages = ["FSharpx.Extras"; "FSharpx.Text.StructuredFormat"] 
 
 let projectDesc = "FSharpx.Extras implements general functional constructs on top of the F# core library. Its main target is F# but it aims to be compatible with all .NET languages wherever possible."
 
@@ -56,9 +33,6 @@ let gitHome = "https://github.com/fsprojects"
 // The name of the project on GitHub
 let gitName = "FSharpx.Extras"
 
-// The url for the raw files hosted
-let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/fsprojects"
-
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 let release = parseReleaseNotes (File.ReadAllLines "RELEASE_NOTES.md")
 
@@ -76,13 +50,12 @@ let buildLibParams fxVersion =
      ]
 
 // tools
-let nunitVersion = GetPackageVersion packagesDir "NUnit.Runners"
-let nunitPath = packagesDir  @@ sprintf "NUnit.Runners.%s/tools" nunitVersion
+let nunitPath = packagesDir  @@ "NUnit.Runners/tools"
 
 
 // targets
 Target "Clean" (fun _ ->       
-    CleanDirs [buildDir; nugDir]
+    CleanDirs [buildDir]
 )
 
 
@@ -129,40 +102,6 @@ Target "Test" (fun _ ->
             DisableShadowCopy = true
             OutputFile = buildDirVer fxVersion @@ sprintf "TestResults.%s.xml" fxVersion }))
 
-Target "PrepareNuGet" (fun _ ->
-    for fxVersion in fxVersions do
-      for package in packages do
-        let frameworkSubDir = nugetLibDir package @@ normalizeFrameworkVersion fxVersion
-        CleanDir frameworkSubDir
-
-        [for ending in ["dll";"pdb";"xml"] do
-            yield buildDirVer fxVersion @@ sprintf "%s.%s" package ending]
-        |> Seq.filter File.Exists
-        |> CopyTo frameworkSubDir)
-
-
-Target "NuGet" (fun _ ->
-
-  for package in packages do 
-    tracefn "Generating nuget target for package %s" package
-    [ "LICENSE.md" ] |> CopyTo (nugetDir package)
-    NuGet (fun p -> 
-        {p with               
-            Authors = authors
-            Project = package
-            WorkingDir = nugetDir package
-            Description = getPackageDesc package
-            Version = release.AssemblyVersion
-            OutputPath = nugetDir package
-            AccessKey = getBuildParamOrDefault "nugetkey" ""
-            Publish = hasBuildParam "nugetkey"
-            Dependencies =
-                [ yield ("FSharpx.Async", NormalizeVersion (Fake.NuGetHelper.GetPackageVersion "packages" "FSharpx.Async"));
-                  yield ("FSharpx.Collections", NormalizeVersion (Fake.NuGetHelper.GetPackageVersion "packages" "FSharpx.Collections"));
-                  if package <> "FSharpx.Extras" then                   
-                     yield ("FSharpx.Extras", RequireExactly (NormalizeVersion release.AssemblyVersion)) ] })
-        "FSharpx.Extras.nuspec")
-            
 
 Target "PaketPack" (fun _ ->
     Paket.Pack (fun p ->
@@ -211,8 +150,6 @@ Target "CI" DoNothing
   ==> "Test" 
 
 "Build"
-  ==> "PrepareNuGet"
-  ==> "NuGet"
   ==> "PaketPack"
 
 "Test" 
@@ -223,12 +160,8 @@ Target "CI" DoNothing
   ==> "ReleaseDocs"
   ==> "Release"
 
-"NuGet"
-  ==> "Release"
-
 "Test"
   ==> "GenerateDocs"
-  ==> "NuGet"
   ==> "PaketPack"
   ==> "CI"
 
