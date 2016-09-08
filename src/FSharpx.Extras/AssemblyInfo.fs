d22d8cba091f8041d7ca6176e3ac6421237986d1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-09-08 10:46:35 +0200

    Bump version to 2.1.2

--- src/FSharpx.Extras/AssemblyInfo.fs
+++ src/FSharpx.Extras/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpx.Extras")>]
 [<assembly: AssemblyProductAttribute("FSharpx.Extras")>]
 [<assembly: AssemblyDescriptionAttribute("FSharpx.Extras implements general functional constructs on top of the F# core library. Its main target is F# but it aims to be compatible with all .NET languages wherever possible.")>]
-[<assembly: AssemblyVersionAttribute("2.1.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.1.1")>]
+[<assembly: AssemblyVersionAttribute("2.1.2")>]
+[<assembly: AssemblyFileVersionAttribute("2.1.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.1.1"
-    let [<Literal>] InformationalVersion = "2.1.1"
+    let [<Literal>] Version = "2.1.2"
+    let [<Literal>] InformationalVersion = "2.1.2"
