7525861f0598bd52281a2543b48eb0e36e11416d
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-09-09 08:19:21 +0200

    Bump version to 2.1.3

--- src/FSharpx.Text.StructuredFormat/AssemblyInfo.fs
+++ src/FSharpx.Text.StructuredFormat/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpx.Text.StructuredFormat")>]
 [<assembly: AssemblyProductAttribute("FSharpx.Extras")>]
 [<assembly: AssemblyDescriptionAttribute("FSharpx.Extras implements general functional constructs on top of the F# core library. Its main target is F# but it aims to be compatible with all .NET languages wherever possible.")>]
-[<assembly: AssemblyVersionAttribute("2.1.2")>]
-[<assembly: AssemblyFileVersionAttribute("2.1.2")>]
+[<assembly: AssemblyVersionAttribute("2.1.3")>]
+[<assembly: AssemblyFileVersionAttribute("2.1.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.1.2"
-    let [<Literal>] InformationalVersion = "2.1.2"
+    let [<Literal>] Version = "2.1.3"
+    let [<Literal>] InformationalVersion = "2.1.3"
