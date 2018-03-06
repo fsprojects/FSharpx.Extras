0ab35c8412d0a1d507ab841b1421d6be41d3bf1c
Christian Steinert <chr.steinert@outlook.com>
2018-03-06 14:15:07 +0100

    Fix additional args for build.

--- build.fsx
+++ build.fsx
@@ -122,7 +122,7 @@ Target "Clean" (fun _ ->
 // Build library & test project
 
 Target "Build" (fun _ ->
-    DotNetCli.Build (fun c -> { c with AdditionalArgs = [ "/p:SourceLinkCreate:true" ] })
+    DotNetCli.Build (fun c -> { c with AdditionalArgs = [ "/p:SourceLinkCreate=true" ] })
 )
 
 // --------------------------------------------------------------------------------------
