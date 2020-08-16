b9225f0b423a781e0aa527821a07ba8cd97f9d9c
gdziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2020-08-16 12:25:07 +0200

    Remove TaskBuilder.

--- src/FSharpx.Extras/ComputationExpressions/Task.fs
+++ src/FSharpx.Extras/ComputationExpressions/Task.fs
@@ -7,27 +7,6 @@ module Task =
     open System.Threading.Tasks
     open FSharpx.Collections
 
-    /// Task result
-    type Result<'T> = 
-        /// Task was canceled
-        | Canceled
-        /// Unhandled exception in task
-        | Error of exn 
-        /// Task completed successfully
-        | Successful of 'T
-
-    let run (t: unit -> Task<'a>) : Result<'a> = 
-        try
-            let task = t()
-            task.Result |> Result<_>.Successful
-        with 
-        | :? OperationCanceledException -> Result<_>.Canceled
-        | :? AggregateException as e ->
-            match e.InnerException with
-            | :? TaskCanceledException -> Result<_>.Canceled
-            | _ -> Result<_>.Error e
-        | e -> Result<_>.Error e
-
     let toAsync (t: Task<'T>): Async<'T> =
         let abegin (cb: AsyncCallback, state: obj) : IAsyncResult = 
             match cb with
@@ -101,145 +80,7 @@ module Task =
 
     let inline mapM f x = sequence (List.map f x)
 
-    [<Obsolete("Prefer NuGet package TaskBuilder.fs version FSharp.Control.Tasks.V2.TaskBuilder")>]
-    type TaskBuilder(?continuationOptions, ?scheduler, ?cancellationToken) =
-        let contOptions = defaultArg continuationOptions TaskContinuationOptions.None
-        let scheduler = defaultArg scheduler TaskScheduler.Default
-        let cancellationToken = defaultArg cancellationToken CancellationToken.None
-
-        member this.Return x = returnM x
-
-        member this.Bind(m, f) = bindWithOptions cancellationToken contOptions scheduler f m
-
-        member this.Zero() : Task<unit> = this.Return ()
-
-        member this.ReturnFrom (a: Task<'a>) = a
-
-        member this.Run (body : unit -> Task<'a>) = body()
-
-        member this.Delay (body : unit -> Task<'a>) : unit -> Task<'a> = fun () -> this.Bind(this.Return(), body)
-
-        member this.Combine(t1:Task<unit>, t2 : unit -> Task<'b>) : Task<'b> = this.Bind(t1, t2)
-
-        member this.While(guard, body : unit -> Task<unit>) : Task<unit> =
-            if not(guard())
-            then this.Zero()
-            else this.Bind(body(), fun () -> this.While(guard, body))
-
-        member this.TryWith(body : unit -> Task<'a>, catchFn:exn -> Task<'a>) : Task<'a> =
-            let continuation (t:Task<'a>) : Task<'a> =
-                if t.IsFaulted
-                then catchFn(t.Exception.GetBaseException())
-                else this.Return(t.Result)
-
-            try body().ContinueWith(continuation).Unwrap()
-            with e -> catchFn(e)
-
-        member this.TryFinally(body : unit -> Task<'a>, compensation) : Task<'a> =
-            let wrapOk (x:'a) : Task<'a> =
-                compensation()
-                this.Return x
-
-            let wrapCrash (e:exn) : Task<'a> =
-                compensation()
-                reraise' e
-
-            this.Bind(this.TryWith(body, wrapCrash), wrapOk)
-
-        member this.Using(res:#IDisposable, body : #IDisposable -> Task<'a>) : Task<'a> =
-            this.TryFinally((fun () -> body res), fun () -> if not (isNull (box res)) then res.Dispose())
-
-        member this.For(sequence:seq<'a>, body : 'a -> Task<unit>) : Task<unit> =
-            this.Using( sequence.GetEnumerator()
-                      , fun enum -> this.While( enum.MoveNext
-                                              , fun () -> body enum.Current
-                                              )
-                      )
-
-    [<Obsolete("Prefer NuGet package TaskBuilder.fs version FSharp.Control.Tasks.V2.task")>]
-    let task = TaskBuilder()
-
     type TokenToTask<'a> = CancellationToken -> Task<'a>
-    type TaskBuilderWithToken(?continuationOptions, ?scheduler) =
-        let contOptions = defaultArg continuationOptions TaskContinuationOptions.None
-        let scheduler = defaultArg scheduler TaskScheduler.Default
-
-        let lift (t: Task<_>) = fun (_: CancellationToken) -> t
-
-        let bind (t:TokenToTask<'a>) (f : 'a -> TokenToTask<'b>) =
-            fun (token: CancellationToken) ->
-                (t token).ContinueWith( fun (x: Task<_>) -> f x.Result token
-                                      , token
-                                      , contOptions
-                                      , scheduler
-                                      )
-                         .Unwrap()
-        
-        member this.Return x = lift (returnM x)
-
-        member this.Bind(t, f) = bind t f
-
-        member this.Bind(t, f) = bind (lift t) f
-
-        member this.ReturnFrom t = lift t
-
-        member this.ReturnFrom (t:TokenToTask<'a>) = t
-
-        member this.Zero() : TokenToTask<unit> = this.Return ()
-
-        member this.Run (body : unit -> TokenToTask<'a>) = body()
-
-        member this.Delay (body : unit -> TokenToTask<'a>) : unit -> TokenToTask<'a> = fun () -> this.Bind(this.Return(), body)
-
-        member this.Combine(t1 : TokenToTask<unit>, t2 : unit -> TokenToTask<'b>) : TokenToTask<'b> = this.Bind(t1, t2)
-
-        member this.While(guard, body : unit -> TokenToTask<unit>) : TokenToTask<unit> =
-            if not(guard())
-            then this.Zero()
-            else this.Bind(body(), fun () -> this.While(guard, body))
-
-        member this.TryWith(body : unit -> TokenToTask<'a>, catchFn : exn -> TokenToTask<'a>) : TokenToTask<'a> = fun token ->
-            let continuation (t:Task<'a>) : Task<'a> =
-                if t.IsFaulted
-                then catchFn(t.Exception.GetBaseException())
-                else this.Return(t.Result)
-                <| token
-
-            try (body() token).ContinueWith(continuation).Unwrap()
-            with e -> catchFn(e) token
-
-        member this.TryFinally(body : unit -> TokenToTask<'a>, compensation) : TokenToTask<'a> =
-            let wrapOk (x:'a) : TokenToTask<'a> =
-                compensation()
-                this.Return x
-
-            let wrapCrash (e:exn) : TokenToTask<'a> =
-                compensation()
-                reraise' e
-
-            this.Bind(this.TryWith(body, wrapCrash), wrapOk)
-
-        member this.Using(res:#IDisposable, body : #IDisposable -> TokenToTask<'a>) : TokenToTask<'a> =
-            let compensation() =
-                match res with
-                | null -> ()
-                | disp -> disp.Dispose()
-
-            this.TryFinally((fun () -> body res), compensation)
-
-        member this.For(sequence:seq<'a>, body : 'a -> TokenToTask<unit>) : TokenToTask<unit> =
-            this.Using( sequence.GetEnumerator()
-                      , fun enum -> this.While( enum.MoveNext
-                                              , fun () -> body enum.Current
-                                              )
-                      )
-
-    /// Creates a single Task<unit> that will complete when all of the Task<unit> objects in an enumerable collection have completed.
-    let inline WhenAllUnits (units:seq<Task<unit>>) : Task<unit> =
-        task {
-            let! (_:unit[]) = Task.WhenAll units
-            return ()
-        }
 
     /// Converts a Task into Task<unit>
     let inline ToTaskUnit (t:Task) =
@@ -258,58 +99,8 @@ module Task =
             |> Some
         | _ -> None
 
-    /// Creates a task that executes a specified task.
-    /// If this task completes successfully, then this function returns Choice1Of2 with the returned value.
-    /// If this task raises an exception before it completes then return Choice2Of2 with the raised exception.
-    let Catch (t:Task<'a>) : Task<Choice<'a, exn>> =
-        task {
-            try let! r = t
-                return Choice1Of2 r
-            with e ->
-                let e' = match e with
-                         | AggregateExn [inner] -> inner
-                         | x                    -> x
-                return Choice2Of2 e'
-        }
-
     /// Creates a task that executes all the given tasks.
     let Parallel (tasks : seq<unit -> Task<'a>>) : (Task<'a[]>) =
         tasks
         |> Seq.map (fun t -> t())
         |> Task.WhenAll
-
-    /// Creates a task that executes all the given tasks.
-    /// This function doesn't throw exceptions, but instead returns an array of Choices.
-    let ParallelCatch (tasks : seq<unit -> Task<'a>>) : (Task<Choice<'a, exn>[]>) =
-        let catch t () =
-            Catch <| t()
-        tasks
-        |> Seq.map catch
-        |> Parallel
-
-    /// common code for ParallelCatchWithThrottle and ParallelWithThrottle
-    let private ParallelWithThrottleCustom transformResult throttle (tasks : seq<unit -> Task<'a>>) : (Task<'b[]>) =
-        task {
-            use semaphore = new SemaphoreSlim(throttle)
-            let throttleTask (t:unit->Task<'a>) () : Task<'b> =
-                task {
-                    do! semaphore.WaitAsync() |> ToTaskUnit
-                    let! result = Catch <| t()
-                    semaphore.Release() |> ignore
-                    return transformResult result
-                }
-            return! tasks
-                    |> Seq.map throttleTask
-                    |> Parallel
-        }
-
-    /// Creates a task that executes all the given tasks.
-    /// This function doesn't throw exceptions, but instead returns an array of Choices.
-    /// The paralelism is throttled, so that at most `throttle` tasks run at one time.
-    let ParallelCatchWithThrottle throttle (tasks : seq<unit -> Task<'a>>) : (Task<Choice<'a, exn>[]>) =
-        ParallelWithThrottleCustom id throttle tasks
-
-    /// Creates a task that executes all the given tasks.
-    /// The paralelism is throttled, so that at most `throttle` tasks run at one time.
-    let ParallelWithThrottle throttle (tasks : seq<unit -> Task<'a>>) : (Task<'a[]>) =
-        ParallelWithThrottleCustom Choice.getOrReraise throttle tasks
