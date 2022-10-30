3957ba33d00390e452adc10c4039c6608d6883b9
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2022-10-31 00:51:56 +0100

    Add Monoid CE example.

---
+++ docs/MonoidCESample.fsx
@@ -0,0 +1,21 @@
+﻿#r @"../bin/FSharpx.Extras.dll"
+#r @"../bin/FSharpx.Collections.dll"
+
+open FSharpx
+
+// Sample monoid implementation
+let addition = Monoid.sum()
+
+// Using monoids computation expression capabilities
+let x : float =
+    addition {
+        yield 1.0
+        yield 5.0
+        yield 6.0
+    }
+
+// Getting the zero
+let zero : float =
+    addition {
+        ()
+    }
